9861887a804b2a7042fc56e1ea49d3adf08e8933
jayaranigarg <jagarg@microsoft.com>
2017-01-30 02:22:48 -0800

    Building vsmanproj in build script (#38)
    * Building vsmanproj in build script
    
    * Removing MicroBuildOverridePluginDirectory switch
    
    * PR comments
    
    * Removing vsmanprojs from full build

--- scripts/Build.ps1
+++ scripts/Build.ps1
@@ -8,7 +8,11 @@ Param(
   [Alias("c")]
   [string] $configuration = "Debug",
   
-  [switch] $clearPackageCache
+  [switch] $clearPackageCache,
+  [switch] $templates,
+  [switch] $wizards,
+  [switch] $vsmanprojects,
+  [switch] $full
 )
 
 $ErrorActionPreference = "Stop"
@@ -16,24 +20,65 @@ $ErrorActionPreference = "Stop"
 . $PSScriptRoot\common.lib.ps1
 
 $solutions = @("TestFx.sln","Templates\MSTestTemplates.sln","WizardExtensions\WizardExtensions.sln")
+$vsmanprojs =@(
+"setup\Templates\Desktop\Microsoft.VisualStudio.Templates.CS.MSTestv2.Desktop.UnitTest.vsmanproj",
+"setup\Templates\UWP\Microsoft.VisualStudio.Templates.CS.MSTestv2.UWP.UnitTest.vsmanproj",
+"setup\WizardExtensions\MSTestv2IntelliTestExtension\Microsoft.VisualStudio.TestTools.MSTestV2.WizardExtension.IntelliTest.vsmanproj",
+"setup\WizardExtensions\MSTestv2UnitTestExtension\Microsoft.VisualStudio.TestTools.MSTestV2.WizardExtension.UnitTest.vsmanproj"
+)
+
+function Invoke-Build([string] $solution)
+{
+    $msbuild = Locate-MSBuild
+	$solutionPath = Locate-Solution -relativePath $solution
+
+	Write-Host -object "Starting $solution build..."
+	Write-Host -object "$msbuild /t:$target /p:Configuration=$configuration /tv:$msbuildVersion /m $solutionPath"
+	& $msbuild /t:$target /p:Configuration=$configuration /tv:$msbuildVersion /m $solutionPath
+  
+	if ($lastExitCode -ne 0) {
+		throw "Build failed with an exit code of '$lastExitCode'."
+	}
+}
+
+function Build-vsmanprojs
+{
+  $msbuild = Locate-MSBuild
+  
+  foreach($vsmanproj in $vsmanprojs)
+  {
+	$vsmanprojPath = Locate-Solution -relativePath $vsmanproj
+	
+	Write-Host -object "Starting $vsmanproj build..."
+	Write-Host -object "$msbuild /t:$target /p:Configuration=$configuration /tv:$msbuildVersion /m /p:TargetExt=.vsman $vsmanprojPath"
+	& $msbuild /t:$target /p:Configuration=$configuration /tv:$msbuildVersion /m /p:TargetExt=.vsman $vsmanprojPath
+	
+	if ($lastExitCode -ne 0) {
+		throw "VSManProj build failed with an exit code of '$lastExitCode'."
+	}
+  }
+}
 
 function Perform-Build {
   Write-Host -object ""
 
-  $msbuild = Locate-MSBuild 
+  Invoke-Build -solution "TestFx.sln"
   
-  foreach($solution in $solutions)
+  if($templates -or $full)
   {
-	$solutionPath = Locate-Solution -relativePath $solution
-
-	Write-Host -object "Starting $solution build..."
-	& $msbuild /t:$target /p:Configuration=$configuration /tv:$msbuildVersion /m $solutionPath
-  
-	if ($lastExitCode -ne 0) {
-		throw "Solution build failed with an exit code of '$lastExitCode'."
-	}
+	Invoke-Build -solution "Templates\MSTestTemplates.sln"
   }
-
+  
+  if($wizards -or $full)
+  {
+	Invoke-Build -solution "WizardExtensions\WizardExtensions.sln"	
+  }
+  
+  if($vsmanprojects)
+  {
+	Build-vsmanprojs
+  }
+  
   Write-Host -object "The build completed successfully." -foregroundColor Green
 }
 
@@ -50,6 +95,7 @@ function Perform-Restore {
   }
 
   Write-Host -object "Starting toolset restore..."
+  Write-Host -object "$nuget restore -msbuildVersion $msbuildVersion -verbosity quiet -nonInteractive -configFile $nugetConfig $toolset"
   & $nuget restore -msbuildVersion $msbuildVersion -verbosity quiet -nonInteractive -configFile $nugetConfig $toolset
   
   if ($lastExitCode -ne 0) {
@@ -64,6 +110,7 @@ function Perform-Restore {
   {
 	$solutionPath = Locate-Solution -relativePath $solution
 
+	Write-Host -object "$nuget restore -msbuildPath $msbuildPath -verbosity quiet -nonInteractive -configFile $nugetConfig $solutionPath"
 	& $nuget restore -msbuildPath $msbuildPath -verbosity quiet -nonInteractive -configFile $nugetConfig $solutionPath
   }
 
@@ -84,9 +131,14 @@ function Print-Help {
   Write-Host -object ""
   Write-Host -object "  Help                          - [Switch] - Prints this help message."
   Write-Host -object "  ClearPackageCache             - [Switch] - Indicates local package cache should be cleared before restore."
+  Write-Host -object "  Templates                     - [Switch] - Indicates Templates should also be build."
+  Write-Host -object "  Wizards                       - [Switch] - Indicates WizardExtensions should also be build."
+  Write-Host -object "  VSManProjects                 - [Switch] - Indiactes VSManProjes should also be build."
+  Write-Host -object "  Full                          - [Switch] - Indicates to perform a full build which includes Adapter,Framework,Templates,Wizards, and vsmanprojs."
   Write-Host -object ""
   Write-Host -object "  Configuration                 - [String] - Specifies the build configuration. Defaults to 'Debug'."
   Write-Host -object "  Target                        - [String] - Specifies the build target. Defaults to 'Build'."
+
   Write-Host -object ""
   Exit 0
 }
