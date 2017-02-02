850067b9f38756c19daf6f3dfee691c26d580443
Abhitej John <abhitej@outlook.com>
2017-02-03 01:43:44 +0530

    Updating PR and CI to run tests. (#48)
    * Updating PR and CI to run tests.
    
    * Just running UTs

--- netci.groovy
+++ netci.groovy
@@ -16,12 +16,14 @@ def branch = GithubBranchName
         def newJobName = InternalUtilities.getFullJobName(project, configuration, isPR)    
 
         // Define your build/test strings here
-        def buildString = """call build.cmd -c ${configuration}"""
-
+        def buildString = """call build.cmd -c ${configuration} -full"""
+        def testString = """call test.cmd -c ${configuration} -pl"""
+        
         // Create a new job for windows build
         def newJob = job(newJobName) {
             steps {
                 batchFile(buildString)
+                batchFile(testString)
             }
         }
 
