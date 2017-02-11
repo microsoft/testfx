7abd65e5bf4d50a99d91ef4d412a93b1c525745f
Abhitej Anoop John Bandi <aajohn@microsoft.com>
2017-02-11 21:32:44 +0530

    Copyright headers everywhere.

--- test/UnitTests/PlatformServices.Shared.Unit.Tests/AssemblyAttributes.cs
+++ test/UnitTests/PlatformServices.Shared.Unit.Tests/AssemblyAttributes.cs
@@ -1,4 +1,6 @@
-﻿
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using MSTestAdapter.PlatformServices.CoreSystem.Tests;
 
 [assembly: CoreReflectionOperationsTests.DummyAAttribute("a1")]
