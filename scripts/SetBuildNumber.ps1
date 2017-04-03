0c8e7fd8173e08cf7bbd0bea4613ec5250f090a8
Abhitej Anoop John Bandi <aajohn@microsoft.com>
2017-04-04 02:01:06 +0530

    Taking the build number as input.

--- scripts/SetBuildNumber.ps1
+++ scripts/SetBuildNumber.ps1
@@ -1,16 +1,20 @@
 [CmdletBinding(PositionalBinding=$false)]
 Param(  
   [Parameter(Mandatory=$true)]
-  [System.String] $RevisionNumber
+  [Alias("bn")]
+  [System.String] $DefinitionBuildNumber
 )
 
+$TFB_DefinitionBuildNumber = $DefinitionBuildNumber
 $TFB_FirstReleaseDate = [DateTime](Get-Date -Year 2016 -Month 05 -Day 01)
 
 function Set_BuildNumber()
 {
     $currentDate = [System.DateTime]::UTCNow
+    # The default build number would be of the format $(date:yyyymmdd)$(rev:.rr)
+    $revisionNumber = $TFB_DefinitionBuildNumber.Split(".")[1]
     $monthDiff = ($currentDate.Month - $TFB_FirstReleaseDate.Month) + 12*($currentDate.Year - $TFB_FirstReleaseDate.Year)
-    $buildNumber = $monthDiff.ToString() + $currentDate.ToString("dd") + "." + $RevisionNumber
+    $buildNumber = $monthDiff.ToString() + $currentDate.ToString("dd") + "." + $revisionNumber
     Write-Verbose("Build number used: " + $buildNumber)
     Write-Host("##vso[build.updatebuildnumber]$buildNumber")
 }
