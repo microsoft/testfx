379cc05609ed7b76929aeaed2aad70226ea64277
Abhitej Anoop John Bandi <aajohn@microsoft.com>
2017-04-04 01:01:34 +0530

    Script to set the build number for official builds.

---
+++ scripts/SetBuildNumber.ps1
@@ -0,0 +1,18 @@
+[CmdletBinding(PositionalBinding=$false)]
+Param(  
+  [Parameter(Mandatory=$true)]
+  [System.String] $RevisionNumber
+)
+
+$TFB_FirstReleaseDate = [DateTime](Get-Date -Year 2016 -Month 05 -Day 01)
+
+function Set_BuildNumber()
+{
+    $currentDate = [System.DateTime]::UTCNow
+    $monthDiff = ($currentDate.Month - $TFB_FirstReleaseDate.Month) + 12*($currentDate.Year - $TFB_FirstReleaseDate.Year)
+    $buildNumber = $monthDiff.ToString() + $currentDate.ToString("dd") + "." + $RevisionNumber
+    Write-Verbose("Build number used: " + $buildNumber)
+    Wrte-Host("##vso[build.updatebuildnumber]$buildNumber")
+}
+
+Set_BuildNumber
