32cdb9486cde57f869e66ae292f2d779015b8479
jagarg <jagarg@microsoft.com>
2017-05-03 10:58:02 +0530

    Base commit for Desktop and UWP VB templates

---
+++ Templates/VisualBasic/Desktop/ProjectTemplates/VisualBasic/Test/UnitTest1.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class $itemname$
+
+    <TestMethod()> Public Sub TestMethod1()
+    End Sub
+
+End Class
