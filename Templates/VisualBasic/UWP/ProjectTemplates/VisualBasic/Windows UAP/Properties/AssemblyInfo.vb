d1a9b5f64533c552d94e3e854674189c0ac7f375
jagarg <jagarg@microsoft.com>
2017-05-03 15:34:56 +0530

    PR comments

--- Templates/VisualBasic/UWP/ProjectTemplates/VisualBasic/Windows UAP/Properties/AssemblyInfo.vb
+++ Templates/VisualBasic/UWP/ProjectTemplates/VisualBasic/Windows UAP/Properties/AssemblyInfo.vb
@@ -2,12 +2,6 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
-
 <Assembly: AssemblyTitle("$projectname$")>
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("$registeredorganization$")>
@@ -16,17 +10,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTrademark("")>
 <assembly: AssemblyMetadata("TargetPlatform","UAP")>
 
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
-
 <Assembly: AssemblyVersion("1.0.0.0")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
 <Assembly: ComVisible(False)>
