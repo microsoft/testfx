7df96a59be546bcfd70a7a4a0999598429d9ae15
jagarg <jagarg@microsoft.com>
2018-03-26 14:27:39 +0530

    Adding E2E Tests

--- test/E2ETests/Smoke.E2E.Tests/CompatTests.cs
+++ test/E2ETests/Smoke.E2E.Tests/CompatTests.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace MSTestAdapter.Smoke.E2ETests
 {
-    class CompatTests
+    using Microsoft.MSTestV2.CLIAutomation;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CompatTests : CLITestBase
     {
+        private const string OldAdapterTestProject = "CompatTests\\CompatTestProject.dll";
+        private const string LatestAdapterTestProject = "DesktopTestProjectx86Debug.dll";
+
+        [TestMethod]
+        public void DiscoverCompatTests()
+        {
+            this.InvokeVsTestForDiscovery(new string[] { OldAdapterTestProject, LatestAdapterTestProject });
+            var listOfTests = new string[]
+            {
+                "CompatTestProject.UnitTest1.PassingTest",
+                "CompatTestProject.UnitTest1.FailingTest",
+                "CompatTestProject.UnitTest1.SkippingTest",
+                "SampleUnitTestProject.UnitTest1.PassingTest",
+                "SampleUnitTestProject.UnitTest1.FailingTest",
+                "SampleUnitTestProject.UnitTest1.SkippingTest"
+            };
+            this.ValidateDiscoveredTests(listOfTests);
+        }
+
+        [TestMethod]
+        public void RunAllCompatTests()
+        {
+            this.InvokeVsTestForExecution(new string[] { OldAdapterTestProject, LatestAdapterTestProject });
+
+            this.ValidatePassedTestsContain(
+                "CompatTestProject.UnitTest1.PassingTest",
+                 "SampleUnitTestProject.UnitTest1.PassingTest");
+
+            this.ValidateFailedTestsContain(
+                "CompatTestProject.UnitTest1.FailingTest",
+                "SampleUnitTestProject.UnitTest1.FailingTest");
+
+            this.ValidateSkippedTestsContain(
+                "CompatTestProject.UnitTest1.SkippingTest",
+                "SampleUnitTestProject.UnitTest1.SkippingTest");
+        }
     }
 }
