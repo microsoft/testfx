664f4bb15d9c2a9916cc7a4a2a230ec958709f0a
Mayank Bansal <maban@microsoft.com>
2018-04-10 16:53:19 +0530

    logs

--- test/UnitTests/MSTest.CoreAdapter.Unit.Tests/MSTestDiscovererTests.cs
+++ test/UnitTests/MSTest.CoreAdapter.Unit.Tests/MSTestDiscovererTests.cs
@@ -70,7 +70,7 @@ namespace Microsoft.VisualStudio.TestPlatform.MSTestAdapter.UnitTests
         [TestMethod]
         public void MSTestDiscovererHasCategoryAttribute()
         {
-            Console.WriteLine(typeof(MSTestDiscoverer));
+            Assert.IsTrue(typeof(MSTestDiscoverer).Assembly.Location.Contains(@"MSTest.CoreAdapter.Unit.Tests\Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.dll"));
             var attribute = typeof(MSTestDiscoverer).GetTypeInfo().GetCustomAttribute(typeof(CategoryAttribute));
             Assert.IsNotNull(attribute);
             Assert.AreEqual("managed", (attribute as CategoryAttribute).Category);
