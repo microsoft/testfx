090e9752bd55e765aa347f48ee93f60f9eee7328
Jayarani garg <jagarg@microsoft.com>
2018-04-18 20:01:24 +0530

    Compatibility fix by resetting appbase (#389)
    * Compatibility fix by resetting appbase
    
    * Adding E2E Tests
    
    * Clean up and add documentation
    
    * Removing unnecessary comments
    
    * PR comments
    
    * Resolving PR comments
    
    * Small fixes
    
    * PR comments
    
    * Small Fix
    
    * Nitpick

---
+++ test/E2ETests/Smoke.E2E.Tests/CompatTests.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace MSTestAdapter.Smoke.E2ETests
+{
+    using Microsoft.MSTestV2.CLIAutomation;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CompatTests : CLITestBase
+    {
+        private const string OldAdapterTestProject = "CompatTests\\CompatTestProject.dll";
+        private const string LatestAdapterTestProject = "DesktopTestProjectx86Debug.dll";
+
+        [TestMethod]
+        public void DiscoverCompatTests()
+        {
+            this.InvokeVsTestForDiscovery(new string[] { OldAdapterTestProject, LatestAdapterTestProject });
+            var listOfTests = new string[]
+            {
+                "CompatTestProject.UnitTest1.PassingTest",
+                "CompatTestProject.UnitTest1.FailingTest",
+                "CompatTestProject.UnitTest1.SkippingTest",
+                "SampleUnitTestProject.UnitTest1.PassingTest",
+                "SampleUnitTestProject.UnitTest1.FailingTest",
+                "SampleUnitTestProject.UnitTest1.SkippingTest"
+            };
+            this.ValidateDiscoveredTests(listOfTests);
+        }
+
+        [TestMethod]
+        public void RunAllCompatTests()
+        {
+            this.InvokeVsTestForExecution(new string[] { OldAdapterTestProject, LatestAdapterTestProject });
+
+            this.ValidatePassedTestsContain(
+                "CompatTestProject.UnitTest1.PassingTest",
+                 "SampleUnitTestProject.UnitTest1.PassingTest");
+
+            this.ValidateFailedTestsContain(
+                "CompatTestProject.UnitTest1.FailingTest",
+                "SampleUnitTestProject.UnitTest1.FailingTest");
+
+            this.ValidateSkippedTestsContain(
+                "CompatTestProject.UnitTest1.SkippingTest",
+                "SampleUnitTestProject.UnitTest1.SkippingTest");
+        }
+    }
+}
---
+++ test/E2ETests/TestAssets/CompatTestProject/UnitTest1.cs
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace CompatTestProject
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// The passing test.
+        /// </summary>
+        [TestMethod]
+        public void PassingTest()
+        {
+            Assert.AreEqual(2, 2);
+        }
+
+        /// <summary>
+        /// The failing test.
+        /// </summary>
+        [TestMethod]
+        public void FailingTest()
+        {
+            Assert.AreEqual(2, 3);
+        }
+
+        /// <summary>
+        /// The skipping test.
+        /// </summary>
+        [Ignore]
+        [TestMethod]
+        public void SkippingTest()
+        {
+        }
+    }
+}
