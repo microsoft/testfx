0fa46db26dfdc7bec6e51be6bd5d3e2cbded5825
maban <maban@TESTFX>
2018-04-27 00:06:59 -0700

    Build error fix

--- src/Adapter/PlatformServices.Desktop/Properties/AssemblyInfo.cs
+++ src/Adapter/PlatformServices.Desktop/Properties/AssemblyInfo.cs
@@ -10,7 +10,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PlatformServices.Desktop")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PlatformServices.Desktop")]
