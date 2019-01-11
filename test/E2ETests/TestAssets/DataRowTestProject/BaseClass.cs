ea644c1db3666bf4c3c2491188530d77069b5134
Jayarani garg <jagarg@microsoft.com>
2019-01-11 11:53:53 +0530

    Base class data rows should not be executed (#546)
    * Base class data rows should not be executed
    
    * Adding tests
    
    * PR comments
    
    * Nit

---
+++ test/E2ETests/TestAssets/DataRowTestProject/BaseClass.cs
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information. 
+
+namespace DataRowTestProject
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BaseClass
+    {
+        [TestMethod]
+        [DataRow("BaseString1")]
+        [DataRow("BaseString2")]
+        [DataRow("BaseString3")]
+        public virtual void DataRowTestMethod(string a)
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
