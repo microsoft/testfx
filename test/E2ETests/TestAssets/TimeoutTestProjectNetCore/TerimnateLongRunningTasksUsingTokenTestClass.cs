a208ab7a8eb24d11d3a1e917a44418a164b1d247
Shiva Shankar <shivash@microsoft.com>
2019-04-08 11:23:48 +0530

    Bugfix for CancellationTokenSource (#593)
    * Bugfix for CancellationTokenSource
    
    Added initialization that was missing in the NetCoreTestContext and added a test to cover the net core scenario as well.

---
+++ test/E2ETests/TestAssets/TimeoutTestProjectNetCore/TerimnateLongRunningTasksUsingTokenTestClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TimeoutTestProject
+{
+    [TestClass]
+    public class TerimnateLongRunningTasksUsingTokenTestClass
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [Timeout(5000)]
+        public void TerimnateLongRunningTasksUsingToken()
+        {
+            var longTask = new Thread(ExecuteLong);
+            longTask.Start();
+            longTask.Join();
+        }
+
+        private void ExecuteLong()
+        {
+            try
+            {
+                File.Delete("TimeoutTestOutputNetCore.txt");
+                Task.Delay(100000).Wait(TestContext.CancellationTokenSource.Token);
+            }
+            catch (OperationCanceledException)
+            {
+                File.WriteAllText("TimeoutTestOutputNetCore.txt", "Written from long running thread post termination");
+            }
+        }
+    }
+}
