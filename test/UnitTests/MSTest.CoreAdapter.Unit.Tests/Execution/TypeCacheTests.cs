cdb43cce6811d3a74510c75cb21b4c50a0636c05
nohwnd <me@jakubjares.com>
2020-01-30 19:42:24 +0100

    Revert fix

--- test/UnitTests/MSTest.CoreAdapter.Unit.Tests/Execution/TypeCacheTests.cs
+++ test/UnitTests/MSTest.CoreAdapter.Unit.Tests/Execution/TypeCacheTests.cs
@@ -657,7 +657,6 @@ namespace Microsoft.VisualStudio.TestPlatform.MSTestAdapter.UnitTests.Execution
             this.typeCache.GetTestMethodInfo(
                     testMethod,
                     new TestContextImplementation(testMethod, null, new Dictionary<string, object>()),
-                    this.executionRecorderWrapper,
                     false);
 
             var classInfo = this.typeCache.ClassInfoCache.FirstOrDefault();
