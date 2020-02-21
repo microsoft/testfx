3f8e570a2600c934ae32e1394f5c24510717d5af
Jakub Jareš <me@jakubjares.com>
2020-02-21 08:54:56 +0100

    Update test/UnitTests/PlatformServices.Shared.Unit.Tests/netstandard1.3/ns13TraceListenerManagerTests.cs

--- test/UnitTests/PlatformServices.Shared.Unit.Tests/netstandard1.3/ns13TraceListenerManagerTests.cs
+++ test/UnitTests/PlatformServices.Shared.Unit.Tests/netstandard1.3/ns13TraceListenerManagerTests.cs
@@ -70,7 +70,7 @@ namespace MSTestAdapter.PlatformServices.UnitTests.Services
             traceListenerManager.Add(traceListener);
             traceListenerManager.Dispose(traceListener);
 
-            // String to write after closing textWriter should throw exception
+            // Trying to write after closing textWriter should throw exception
             Action shouldThrowException = () => writer.WriteLine("Try to write something");
             ActionUtility.ActionShouldThrowExceptionOfType(shouldThrowException, typeof(ObjectDisposedException));
         }
