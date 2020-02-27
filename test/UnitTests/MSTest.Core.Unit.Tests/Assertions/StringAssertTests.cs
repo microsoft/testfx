7af1e9ae60197c0daf9d114d6ffe815ff7157915
Maximilian Chaplin <MXCH@simcorp.com>
2020-02-27 22:48:20 +0100

    added tests

--- test/UnitTests/MSTest.Core.Unit.Tests/Assertions/StringAssertTests.cs
+++ test/UnitTests/MSTest.Core.Unit.Tests/Assertions/StringAssertTests.cs
@@ -6,8 +6,12 @@ namespace Microsoft.VisualStudio.TestPlatform.TestFramework.UnitTests.Assertions
     extern alias FrameworkV1;
     extern alias FrameworkV2;
 
+    using System;
+
+    using MSTestAdapter.TestUtilities;
     using Assert = FrameworkV1::Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
     using TestClass = FrameworkV1::Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute;
+    using TestFrameworkV1 = FrameworkV1.Microsoft.VisualStudio.TestTools.UnitTesting;
     using TestFrameworkV2 = FrameworkV2.Microsoft.VisualStudio.TestTools.UnitTesting;
     using TestMethod = FrameworkV1::Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
 
@@ -24,6 +28,63 @@ namespace Microsoft.VisualStudio.TestPlatform.TestFramework.UnitTests.Assertions
         public void ThatShouldCacheStringAssertInstance()
         {
             Assert.AreEqual(TestFrameworkV2.StringAssert.That, TestFrameworkV2.StringAssert.That);
+        }
+
+        [TestMethod]
+        public void StringAssertContains()
+        {
+            string actual = "The quick brown fox jumps over the lazy dog.";
+            string notInString = "I'm not in the string above";
+            var ex = ActionUtility.PerformActionAndReturnException(() => TestFrameworkV2.StringAssert.Contains(actual, notInString));
+            Assert.IsNotNull(ex);
+            TestFrameworkV1.StringAssert.Contains(ex.Message, "StringAssert.Contains failed");
+        }
+
+        [TestMethod]
+        public void StringAssertStartsWith()
+        {
+            string actual = "The quick brown fox jumps over the lazy dog.";
+            string notInString = "I'm not in the string above";
+            var ex = ActionUtility.PerformActionAndReturnException(() => TestFrameworkV2.StringAssert.StartsWith(actual, notInString));
+            Assert.IsNotNull(ex);
+            TestFrameworkV1.StringAssert.Contains(ex.Message, "StringAssert.StartsWith failed");
+        }
+
+        [TestMethod]
+        public void StringAssertEndsWith()
+        {
+            string actual = "The quick brown fox jumps over the lazy dog.";
+            string notInString = "I'm not in the string above";
+            var ex = ActionUtility.PerformActionAndReturnException(() => TestFrameworkV2.StringAssert.EndsWith(actual, notInString));
+            Assert.IsNotNull(ex);
+            TestFrameworkV1.StringAssert.Contains(ex.Message, "StringAssert.EndsWith failed");
+        }
+
+        [TestMethod]
+        public void StringAssertContainsIgnoreCase()
+        {
+            string actual = "The quick brown fox jumps over the lazy dog.";
+            string inString = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.";
+            var ex = ActionUtility.PerformActionAndReturnException(() => TestFrameworkV2.StringAssert.Contains(actual, inString, StringComparison.OrdinalIgnoreCase));
+            Assert.IsNull(ex);
+        }
+
+        [TestMethod]
+        public void StringAssertStartsWithIgnoreCase()
+        {
+            string actual = "The quick brown fox jumps over the lazy dog.";
+            string inString = "THE QUICK";
+            var ex = ActionUtility.PerformActionAndReturnException(() => TestFrameworkV2.StringAssert.StartsWith(actual, inString, StringComparison.OrdinalIgnoreCase));
+            Assert.IsNull(ex);
+        }
+
+        [TestMethod]
+        public void StringAssertEndsWithIgnoreCase()
+        {
+            string actual = "The quick brown fox jumps over the lazy dog.";
+            string inString = "LAZY DOG.";
+            var ex = ActionUtility.PerformActionAndReturnException(() => TestFrameworkV2.StringAssert.EndsWith(actual, inString, StringComparison.OrdinalIgnoreCase));
+            Assert.IsNull(ex);
         }
     }
 }
