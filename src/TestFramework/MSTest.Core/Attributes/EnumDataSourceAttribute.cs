92d9ec3e42408f46d61b6c43ac7845be70b031a8
joboivin <Jean-olivier.boivin-roy@ia.ca>
2020-03-06 15:41:16 -0500

    Added an EnumDataSourceAttribute

---
+++ src/TestFramework/MSTest.Core/Attributes/EnumDataSourceAttribute.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.VisualStudio.TestTools.UnitTesting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    /// <summary>
+    /// Attribute to define dynamic data for a test method.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class EnumDataSourceAttribute : Attribute, ITestDataSource
+    {
+        private readonly Type enumDataSource;
+        private readonly object[] exclusions;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumDataSourceAttribute"/> class.
+        /// </summary>
+        /// <param name="enumDataSource">
+        /// The type of the <see cref="Enum"/> to iterate for the test.
+        /// </param>
+        public EnumDataSourceAttribute(Type enumDataSource)
+            : this(enumDataSource, new object[0])
+        {
+            // Need to have this constructor explicitely to fix a CLS compliant error.
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumDataSourceAttribute"/> class.
+        /// </summary>
+        /// <param name="enumDataSource">
+        /// The type of the <see cref="Enum"/> to iterate for the test.
+        /// </param>
+        /// <param name="exclusions">
+        /// Values to exclude during the iteration.
+        /// </param>
+        public EnumDataSourceAttribute(Type enumDataSource, params object[] exclusions)
+        {
+            this.enumDataSource = enumDataSource ??
+                                  throw new ArgumentNullException(nameof(enumDataSource), "The enum to iterate is not specified.");
+
+            this.exclusions = exclusions;
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<object[]> GetData(MethodInfo methodInfo)
+        {
+            if (!this.enumDataSource.GetTypeInfo().IsEnum)
+            {
+                throw new InvalidCastException("The requested type is not an enum.");
+            }
+
+            foreach (var value in Enum.GetValues(this.enumDataSource))
+            {
+                if (!this.exclusions.Contains(value))
+                {
+                    yield return new[] { value };
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public string GetDisplayName(MethodInfo methodInfo, object[] data)
+        {
+            return data == null ? null : $"{methodInfo.Name} ({string.Join(", ", data)})";
+        }
+    }
+}
---
+++ test/UnitTests/MSTest.Core.Unit.Tests/Attributes/EnumDataSourceAttributeTests.cs
@@ -0,0 +1,161 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.VisualStudio.TestPlatform.TestFramework.UnitTests.Attributes
+{
+    extern alias FrameworkV1;
+    extern alias FrameworkV2;
+
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    using FrameworkV2::Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using MSTestAdapter.TestUtilities;
+
+    using TestFrameworkV1 = FrameworkV1::Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestFrameworkV1.TestClass]
+    public class EnumDataSourceAttributeTests
+    {
+        private MethodInfo methodInfo;
+
+        [TestFrameworkV1.TestInitialize]
+        public void Initialize()
+        {
+            this.methodInfo = this.GetType().GetTypeInfo().GetDeclaredMethod(nameof(this.TestMethod1));
+        }
+
+        [TestFrameworkV1.TestMethod]
+        public void ConstructorShouldThrowArgumentNullIfEnumDataSourceIsNull()
+        {
+            EnumDataSourceAttribute attribute;
+
+            Action action = () =>
+            {
+                attribute = new EnumDataSourceAttribute(null);
+            };
+
+            ActionUtility.ActionShouldThrowExceptionOfType(action, typeof(ArgumentNullException));
+        }
+
+        [TestFrameworkV1.TestMethod]
+        public void ConstructorShouldNotThrowIfEnumDataSourceIsNotNull()
+        {
+            EnumDataSourceAttribute attribute;
+
+            Action action = () =>
+            {
+                attribute = new EnumDataSourceAttribute(typeof(Value));
+            };
+            action();
+        }
+
+        [TestFrameworkV1.TestMethod]
+        public void ConstructorShouldThrowArgumentNullIfEnumDataSourceIsNullAndExclusionsPassed()
+        {
+            EnumDataSourceAttribute attribute;
+
+            Action action = () =>
+            {
+                attribute = new EnumDataSourceAttribute(null, new object());
+            };
+
+            ActionUtility.ActionShouldThrowExceptionOfType(action, typeof(ArgumentNullException));
+        }
+
+        [TestFrameworkV1.TestMethod]
+        public void ConstructorShouldNotThrowIfEnumDataSourceIsNotNullAndExclusionsPassed()
+        {
+            EnumDataSourceAttribute attribute;
+
+            Action action = () =>
+            {
+                attribute = new EnumDataSourceAttribute(typeof(Value), new object());
+            };
+            action();
+        }
+
+        [TestFrameworkV1.TestMethod]
+        public void GetDataShouldThrowInvalidCastIfEnumDataSourceIsNotAnEnum()
+        {
+            Action action = () =>
+            {
+                var attribute = new EnumDataSourceAttribute(typeof(int));
+                attribute.GetData(this.methodInfo).ToList();
+            };
+
+            ActionUtility.ActionShouldThrowExceptionOfType(action, typeof(InvalidCastException));
+        }
+
+        [TestFrameworkV1.TestMethod]
+        public void GetDataShouldReturnsAllValuesOfEnumIfNoExclusionsIsSpecified()
+        {
+            var attribute = new EnumDataSourceAttribute(typeof(Value));
+
+            var results = attribute.GetData(this.methodInfo).ToList();
+
+            var values = results.Select(r => r.First()).ToList();
+            Assert.AreEqual(4, values.Count);
+            Assert.IsTrue(values.Contains(Value.First));
+            Assert.IsTrue(values.Contains(Value.Second));
+            Assert.IsTrue(values.Contains(Value.Third));
+            Assert.IsTrue(values.Contains(Value.Fourth));
+        }
+
+        [TestFrameworkV1.TestMethod]
+        public void GetDataShouldReturnsAllValuesOfEnumExceptExclusionIfOneExclusionSpecified()
+        {
+            var attribute = new EnumDataSourceAttribute(typeof(Value), Value.Second);
+
+            var results = attribute.GetData(this.methodInfo).ToList();
+
+            var values = results.Select(r => r.First()).ToList();
+            Assert.AreEqual(3, values.Count);
+            Assert.IsTrue(values.Contains(Value.First));
+            Assert.IsTrue(values.Contains(Value.Third));
+            Assert.IsTrue(values.Contains(Value.Fourth));
+        }
+
+        [TestFrameworkV1.TestMethod]
+        public void GetDataShouldReturnsAllValuesOfEnumExceptExclusionsIfTwoExclusionSpecified()
+        {
+            var attribute = new EnumDataSourceAttribute(typeof(Value), Value.Second, Value.Third);
+
+            var results = attribute.GetData(this.methodInfo).ToList();
+
+            var values = results.Select(r => r.First()).ToList();
+            Assert.AreEqual(2, values.Count);
+            Assert.IsTrue(values.Contains(Value.First));
+            Assert.IsTrue(values.Contains(Value.Fourth));
+        }
+
+        [TestFrameworkV1.TestMethod]
+        public void GetDisplayNameShouldReturnDisplayNameWithEnumValue()
+        {
+            var attribute = new EnumDataSourceAttribute(typeof(Value));
+
+            var result = attribute.GetDisplayName(this.methodInfo, new object[] { Value.First });
+
+            Assert.AreEqual($"{nameof(this.TestMethod1)} ({Value.First.ToString()})", result);
+        }
+
+        /// <summary>
+        /// The test method 1.
+        /// </summary>
+        [FrameworkV2::Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute]
+        [EnumDataSource(typeof(Value))]
+        public void TestMethod1()
+        {
+        }
+
+        private enum Value
+        {
+            First,
+            Second,
+            Third,
+            Fourth
+        }
+    }
+}
