690dbd70edb97f967212ddb943c31c5f06f7043e
Medeni Baykal <433724+Haplois@users.noreply.github.com>
2021-02-12 15:36:59 +0100

    Bumped the Test Platfrom version to `16.10.0-preview-20210211-01`.

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -75,52 +75,54 @@ function Print-Help {
 
 function Invoke-Test
 {
+    & dotnet --info
+
     $timer = Start-Timer
     
     Write-Log "Run-Test: Started."
-	
+    
     Write-Log "    Computing Test Containers."
     # Get all the test project folders. They should all be ending with ".Tests"
-	$outDir = Join-Path $env:TF_OUT_DIR -ChildPath $TFT_Configuration
-	$testFolders = Get-ChildItem $outDir -Directory -Filter $env:TF_TESTS_OUTDIR_PATTERN | %{$_.FullName}
+    $outDir = Join-Path $env:TF_OUT_DIR -ChildPath $TFT_Configuration
+    $testFolders = Get-ChildItem $outDir -Directory -Filter $env:TF_TESTS_OUTDIR_PATTERN | %{$_.FullName}
 
     # Get test assemblies from these folders that match the pattern specified.
-	foreach($container in $testFolders)
-	{
-		$testContainer = Get-ChildItem $container\* -Recurse -Include $env:TF_UNITTEST_FILES_PATTERN, $env:TF_COMPONENTTEST_FILES_PATTERN, $env:TF_E2ETEST_FILES_PATTERN
-		
+    foreach($container in $testFolders)
+    {
+        $testContainer = Get-ChildItem $container\* -Recurse -Include $env:TF_UNITTEST_FILES_PATTERN, $env:TF_COMPONENTTEST_FILES_PATTERN, $env:TF_E2ETEST_FILES_PATTERN
+        
         $testContainerName = $testContainer.Name
         $testContainerPath = $testContainer.FullName
         $allContainers += ,"$testContainerName"
         
         if($TFT_All)
         {
-			if($env:TF_NetCoreContainers -Contains $testContainerName)
-			{
-				$netCoreTestContainers += ,"$testContainerPath" 
-			}
-    		else
-			{
-				$testContainers += ,"$testContainerPath"
-			}
+            if($env:TF_NetCoreContainers -Contains $testContainerName)
+            {
+                $netCoreTestContainers += ,"$testContainerPath" 
+            }
+            else
+            {
+                $testContainers += ,"$testContainerPath"
+            }
         }
         else 
         {
             if($testContainerPath -match $TFT_Pattern)
             {
-    		    if($env:TF_NetCoreContainers -Contains $testContainerName)
-				{
-					$netCoreTestContainers += ,"$testContainerPath" 
+                if($env:TF_NetCoreContainers -Contains $testContainerName)
+                {
+                    $netCoreTestContainers += ,"$testContainerPath" 
 
-				}
-				else
-				{
-					$testContainers += ,"$testContainerPath"
-				}
+                }
+                else
+                {
+                    $testContainers += ,"$testContainerPath"
+                }
             }
         }
-	}
-						
+    }
+                        
     if($testContainers.Count -gt 0 -Or $netCoreTestContainers.Count -gt 0)
     {
         $testContainersString = [system.String]::Join(",",$testContainers)
@@ -133,8 +135,8 @@ function Invoke-Test
         Write-Log "    None of the test containers matched the pattern $TFT_Pattern."
         Write-Log "    Test Containers available: $allContainersString."
     }
-	
-	Write-Log "Run-Test: Complete. {$(Get-ElapsedTime($timer))}"
+    
+    Write-Log "Run-Test: Complete. {$(Get-ElapsedTime($timer))}"
 }
 
 function Run-Test([string[]] $testContainers, [string[]] $netCoreTestContainers)
@@ -146,41 +148,41 @@ function Run-Test([string[]] $testContainers, [string[]] $netCoreTestContainers)
     {
        $additionalArguments += "/parallel"
     }
-	
-	 if($testContainers.Count -gt 0)
-	 {
-	 	if(!(Test-Path $vstestPath))
-	 	{
-	 		Write-Error "Unable to find vstest.console.exe at $vstestPath. Test aborted."
-	 	}
-	
-	 	Write-Verbose "$vstestPath $testContainers $additionalArguments /logger:trx"
-	 	& $vstestPath $testContainers $additionalArguments /logger:trx
+    
+     if($testContainers.Count -gt 0)
+     {
+        if(!(Test-Path $vstestPath))
+        {
+            Write-Error "Unable to find vstest.console.exe at $vstestPath. Test aborted."
+        }
+    
+        Write-Verbose "$vstestPath $testContainers $additionalArguments /logger:trx"
+        & $vstestPath $testContainers $additionalArguments /logger:trx
 
-	 	if ($lastExitCode -ne 0) 
-	 	{
-	 	    throw "Tests failed."
-	 	}
-	 }
-	
-	if($netCoreTestContainers.Count -gt 0)
-	{
-		Try
-		{
-			Write-Verbose "dotnet test $netCoreTestContainers /framework:$TestFramework $additionalArguments /logger:trx"
-			& dotnet test $netCoreTestContainers /framework:$TestFramework $additionalArguments /logger:trx
-		}
+        if ($lastExitCode -ne 0) 
+        {
+            throw "Tests failed."
+        }
+     }
+    
+    if($netCoreTestContainers.Count -gt 0)
+    {
+        Try
+        {
+            Write-Verbose "dotnet test $netCoreTestContainers /framework:$TestFramework $additionalArguments /logger:trx"
+            & dotnet test $netCoreTestContainers /framework:$TestFramework $additionalArguments /logger:trx
+        }
 
-		Catch [System.Management.Automation.CommandNotFoundException]
-		{
-			Write-Error "Unable to find dotnet.exe. Test aborted."
-		}
+        Catch [System.Management.Automation.CommandNotFoundException]
+        {
+            Write-Error "Unable to find dotnet.exe. Test aborted."
+        }
 
-		if ($lastExitCode -ne 0) 
-		{
-		    throw "Tests failed."
-		}
-	}
+        if ($lastExitCode -ne 0) 
+        {
+            throw "Tests failed."
+        }
+    }
 }
 
 function Get-VSTestPath
@@ -188,9 +190,9 @@ function Get-VSTestPath
     $versionsFile = "$PSScriptRoot\build\TestFx.Versions.targets"
     $TestPlatformVersion = (([XML](Get-Content $versionsFile)).Project.PropertyGroup.TestPlatformVersion).InnerText
 
-	$vsInstallPath = "$PSScriptRoot\..\packages\Microsoft.TestPlatform.$TestPlatformVersion\"
-	$vstestPath = Join-Path -path $vsInstallPath "tools\net451\Common7\IDE\Extensions\TestPlatform\vstest.console.exe"
-	return Resolve-Path -path $vstestPath
+    $vsInstallPath = "$PSScriptRoot\..\packages\Microsoft.TestPlatform.$TestPlatformVersion\"
+    $vstestPath = Join-Path -path $vsInstallPath "tools\net451\Common7\IDE\Extensions\TestPlatform\vstest.console.exe"
+    return Resolve-Path -path $vstestPath
 }
 
 Print-Help
