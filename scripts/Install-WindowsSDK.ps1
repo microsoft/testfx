6e0160fd7656d614337e4d94518ba44ace671347
Medeni Baykal <433724+Haplois@users.noreply.github.com>
2021-06-25 12:18:55 +0200

    Add missing framework references for WinUI.
     * Added missing framework references
     * Updated license headers on PowerShell files
     * Included a script to install required Windows SDK

--- scripts/Install-WindowsSDK.ps1
+++ scripts/Install-WindowsSDK.ps1
@@ -1,34 +1,18 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-function Write-Log ([string] $message, $messageColor = "Green") {
-    $currentColor = $Host.UI.RawUI.ForegroundColor
-    $Host.UI.RawUI.ForegroundColor = $messageColor
-    if ($message) {
-      Write-Output "... $message"
-    }
-    $Host.UI.RawUI.ForegroundColor = $currentColor
+Write-Host "Downloading Windows SDK 10.0.14393.795..." -ForegroundColor Green
+Invoke-WebRequest -Method Get -Uri https://go.microsoft.com/fwlink/p/?LinkId=838916 -OutFile sdksetup.exe -UseBasicParsing
+
+Write-Host "Installing Windows SDK, if setup requests elevation please approve." -ForegroundColor Green
+$process = Start-Process -Wait sdksetup.exe -ArgumentList "/quiet", "/norestart", "/ceip off", "/features OptionId.WindowsSoftwareDevelopmentKit"  -PassThru
+Remove-Item sdksetup.exe -Force
+
+if($process.ExitCode -eq 0) 
+{
+    Write-Host "Done" -ForegroundColor Green
 }
-
-Push-Location
-try {
-    Write-Log "Downloading the Windows SDK 10.0.14393.795..."
-    Invoke-WebRequest -Method Get -Uri https://go.microsoft.com/fwlink/p/?LinkId=838916 -OutFile sdksetup.exe -UseBasicParsing
-
-    Write-Log "Installing the Windows SDK, setup might request elevation please approve."
-    $process = Start-Process -Wait sdksetup.exe -ArgumentList "/quiet", "/norestart", "/ceip off", "/features OptionId.WindowsSoftwareDevelopmentKit"  -PassThru
-    Remove-Item sdksetup.exe -Force
-
-    if($process.ExitCode -eq 0) 
-    {
-        Write-Log "Done"
-    }
-    else 
-    {
-        Write-Log "Failed (Exit code: $($process.ExitCode))" -messageColor "Red"
-    }
+else 
+{
+    Write-Error "Failed to install Windows SDK (Exit code: $($process.ExitCode))"
 }
-finally {
-    Pop-Location
-}
-
