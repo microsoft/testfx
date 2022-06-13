b031778d12fac7ef23e60f43b1c87ee59254a464
Medeni Baykal <433724+Haplois@users.noreply.github.com>
2022-06-13 14:21:27 +0300

    Removed conflicting dependencies (#1129)

--- test/E2ETests/Automation.CLI/CLITestBase.common.cs
+++ test/E2ETests/Automation.CLI/CLITestBase.common.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Microsoft.MSTestV2.CLIAutomation
@@ -17,7 +17,7 @@ namespace Microsoft.MSTestV2.CLIAutomation
         private const string PackagesFolder = "packages";
 
         // This value is automatically updated by "build.ps1" script.
-        private const string TestPlatformCLIPackage = @"Microsoft.TestPlatform\17.1.0";
+        private const string TestPlatformCLIPackage = @"Microsoft.TestPlatform\17.2.0";
         private const string VstestConsoleRelativePath = @"tools\net451\Common7\IDE\Extensions\TestPlatform\vstest.console.exe";
 
         /// <summary>
