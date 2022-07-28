e2edb0983794753c0d80f85faf47e4237b617f8c
Medeni Baykal <433724+Haplois@users.noreply.github.com>
2022-07-28 15:06:47 +0200

    Removed net6.0 support.

--- test/E2ETests/Automation.CLI/CLITestBase.common.cs
+++ test/E2ETests/Automation.CLI/CLITestBase.common.cs
@@ -17,7 +17,7 @@ namespace Microsoft.MSTestV2.CLIAutomation
         private const string PackagesFolder = "packages";
 
         // This value is automatically updated by "build.ps1" script.
-        private const string TestPlatformCLIPackage = @"Microsoft.TestPlatform\17.2.0";
+        private const string TestPlatformCLIPackage = @"Microsoft.TestPlatform\17.4.0-preview-20220727-01";
         private const string VstestConsoleRelativePath = @"tools\net451\Common7\IDE\Extensions\TestPlatform\vstest.console.exe";
 
         /// <summary>
