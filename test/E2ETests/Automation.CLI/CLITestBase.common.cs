38dd011cd86a3081926a484763396d21db383d2f
Amaury Levé <amauryleve@microsoft.com>
2022-07-29 10:06:57 +0200

    Update test SDK version

--- test/E2ETests/Automation.CLI/CLITestBase.common.cs
+++ test/E2ETests/Automation.CLI/CLITestBase.common.cs
@@ -17,7 +17,7 @@ namespace Microsoft.MSTestV2.CLIAutomation
         private const string PackagesFolder = "packages";
 
         // This value is automatically updated by "build.ps1" script.
-        private const string TestPlatformCLIPackage = @"Microsoft.TestPlatform\17.4.0-preview-20220727-01";
+        private const string TestPlatformCLIPackage = @"Microsoft.TestPlatform\17.4.0-preview-20220728-01";
         private const string VstestConsoleRelativePath = @"tools\net462\Common7\IDE\Extensions\TestPlatform\vstest.console.exe";
 
         /// <summary>
