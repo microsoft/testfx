20d4abc21a12f609bbdfb289bdde31f80d1b3421
Amaury Levé <amauryleve@microsoft.com>
2022-08-04 14:01:06 +0200

    PlatformServices.Desktop

--- src/Adapter/PlatformServices.Desktop/Properties/AssemblyInfo.cs
+++ src/Adapter/PlatformServices.Desktop/Properties/AssemblyInfo.cs
@@ -9,14 +9,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PlatformServices.Desktop")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("PlatformServices.Desktop")]
-[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
