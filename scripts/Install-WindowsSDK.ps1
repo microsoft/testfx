ea34d07e747640789ecdc66af8c5265476fd7759
Amaury Levé <amauryleve@microsoft.com>
2022-08-28 19:14:46 +0200

    Fix setup command line arguments

--- scripts/Install-WindowsSDK.ps1
+++ scripts/Install-WindowsSDK.ps1
@@ -5,10 +5,10 @@ Write-Host "Downloading Windows SDK 10.0.16299..." -ForegroundColor Green
 Invoke-WebRequest -Method Get -Uri https://go.microsoft.com/fwlink/p/?linkid=864422 -OutFile sdksetup.exe -UseBasicParsing
 
 Write-Host "Installing Windows SDK, if setup requests elevation please approve." -ForegroundColor Green
-$process = Start-Process -Wait sdksetup.exe -ArgumentList "/quiet", "/norestart", "/ceip off", "/features OptionId.WindowsSoftwareDevelopmentKit"  -PassThru
-Remove-Item sdksetup.exe -Force
+$process = Start-Process -Wait sdksetup.exe -ArgumentList "/quiet", "/norestart", "/ceip off", "/features OptionId.UWPManaged"  -PassThru
 
-if($process.ExitCode -eq 0) {
+if ($process.ExitCode -eq 0) {
+    Remove-Item sdksetup.exe -Force
     Write-Host "Done" -ForegroundColor Green
 }
 else {
