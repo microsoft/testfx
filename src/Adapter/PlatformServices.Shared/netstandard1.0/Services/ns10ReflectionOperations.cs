ded9efe5cbb0c1c1d4e5a93a842978fe2733b16b
Amaury Levé <amauryleve@microsoft.com>
2022-09-01 22:24:37 +0200

    Merge ReflectionOperations classes

--- src/Adapter/PlatformServices.Shared/netstandard1.0/Services/ns10ReflectionOperations.cs
+++ src/Adapter/PlatformServices.Shared/netstandard1.0/Services/ns10ReflectionOperations.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#if !NETFRAMEWORK
 namespace Microsoft.VisualStudio.TestPlatform.MSTestAdapter.PlatformServices;
 
 using System;
@@ -9,6 +8,9 @@ using System.Linq;
 using System.Reflection;
 
 using Microsoft.VisualStudio.TestPlatform.MSTestAdapter.PlatformServices.Interface;
+#if NETFRAMEWORK
+using Microsoft.VisualStudio.TestPlatform.MSTestAdapter.PlatformServices.Utilities;
+#endif
 
 #pragma warning disable SA1649 // SA1649FileNameMustMatchTypeName
 
@@ -17,6 +19,18 @@ using Microsoft.VisualStudio.TestPlatform.MSTestAdapter.PlatformServices.Interfa
 /// </summary>
 public class ReflectionOperations : IReflectionOperations
 {
+#if NETFRAMEWORK
+    private readonly ReflectionUtility _reflectionUtility;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ReflectionOperations"/> class.
+    /// </summary>
+    public ReflectionOperations()
+    {
+        _reflectionUtility = new ReflectionUtility();
+    }
+#endif
+
     /// <summary>
     /// Gets all the custom attributes adorned on a member.
     /// </summary>
@@ -25,7 +39,11 @@ public class ReflectionOperations : IReflectionOperations
     /// <returns> The list of attributes on the member. Empty list if none found. </returns>
     public object[] GetCustomAttributes(MemberInfo memberInfo, bool inherit)
     {
+#if NETFRAMEWORK
+        return _reflectionUtility.GetCustomAttributes(memberInfo, inherit);
+#else
         return memberInfo.GetCustomAttributes(inherit).ToArray<object>();
+#endif
     }
 
     /// <summary>
@@ -37,7 +55,11 @@ public class ReflectionOperations : IReflectionOperations
     /// <returns> The list of attributes on the member. Empty list if none found. </returns>
     public object[] GetCustomAttributes(MemberInfo memberInfo, Type type, bool inherit)
     {
+#if NETFRAMEWORK
+        return _reflectionUtility.GetCustomAttributes(memberInfo, type, inherit);
+#else
         return memberInfo.GetCustomAttributes(type, inherit).ToArray<object>();
+#endif
     }
 
     /// <summary>
@@ -48,9 +70,12 @@ public class ReflectionOperations : IReflectionOperations
     /// <returns> The list of attributes of the given type on the member. Empty list if none found. </returns>
     public object[] GetCustomAttributes(Assembly assembly, Type type)
     {
+#if NETFRAMEWORK
+        return _reflectionUtility.GetCustomAttributes(assembly, type);
+#else
         return assembly.GetCustomAttributes(type).ToArray<object>();
+#endif
     }
 }
-#endif
 
 #pragma warning restore SA1649 // SA1649FileNameMustMatchTypeName
