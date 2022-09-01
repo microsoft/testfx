4b8f0ac521e9c8ec900596b8dde5d4a5ac646c79
Amaury Levé <amauryleve@microsoft.com>
2022-09-01 21:34:55 +0200

    Merge AdapterTraceLogger classes

--- src/Adapter/PlatformServices.Shared/netstandard1.0/Services/ns10TraceLogger.cs
+++ src/Adapter/PlatformServices.Shared/netstandard1.0/Services/ns10TraceLogger.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#if NETSTANDARD || (NETCOREAPP && !WIN_UI)
 namespace Microsoft.VisualStudio.TestPlatform.MSTestAdapter.PlatformServices;
 
 using Microsoft.VisualStudio.TestPlatform.MSTestAdapter.PlatformServices.Interface;
@@ -10,7 +9,7 @@ using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 #pragma warning disable SA1649 // SA1649FileNameMustMatchTypeName
 
 /// <summary>
-/// The trace logger for .Net Core.
+/// A service to log any trace messages from the adapter that would be shown in *.TpTrace files.
 /// </summary>
 public class AdapterTraceLogger : IAdapterTraceLogger
 {
@@ -21,10 +20,14 @@ public class AdapterTraceLogger : IAdapterTraceLogger
     /// <param name="args"> The args. </param>
     public void LogError(string format, params object[] args)
     {
+#if NETFRAMEWORK || NETSTANDARD || (NETCOREAPP && !WIN_UI)
         if (EqtTrace.IsErrorEnabled)
         {
             EqtTrace.Error(PrependAdapterName(format), args);
         }
+#else
+        EqtTrace.ErrorIf(EqtTrace.IsErrorEnabled, format, args);
+#endif
     }
 
     /// <summary>
@@ -34,10 +37,14 @@ public class AdapterTraceLogger : IAdapterTraceLogger
     /// <param name="args"> The args. </param>
     public void LogWarning(string format, params object[] args)
     {
+#if NETFRAMEWORK || NETSTANDARD || (NETCOREAPP && !WIN_UI)
         if (EqtTrace.IsWarningEnabled)
         {
             EqtTrace.Warning(PrependAdapterName(format), args);
         }
+#else
+        EqtTrace.WarningIf(EqtTrace.IsWarningEnabled, format, args);
+#endif
     }
 
     /// <summary>
@@ -47,17 +54,22 @@ public class AdapterTraceLogger : IAdapterTraceLogger
     /// <param name="args"> The args. </param>
     public void LogInfo(string format, params object[] args)
     {
+#if NETFRAMEWORK || NETSTANDARD || (NETCOREAPP && !WIN_UI)
         if (EqtTrace.IsInfoEnabled)
         {
             EqtTrace.Info(PrependAdapterName(format), args);
         }
+#else
+        EqtTrace.InfoIf(EqtTrace.IsInfoEnabled, format, args);
+#endif
     }
 
+#if NETFRAMEWORK || NETSTANDARD || (NETCOREAPP && !WIN_UI)
     private string PrependAdapterName(string format)
     {
         return $"MSTest - {format}";
     }
+#endif
 }
 
 #pragma warning restore SA1649 // SA1649FileNameMustMatchTypeName
-#endif
