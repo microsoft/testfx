09b95aa84ceabe583e8eb657351ce1cd2a4f6744
Engy <enjieid@microsoft.com>
2022-09-12 13:44:38 +0200

    merge

--- src/TestFramework/MSTest.Core/Assertions/Assert.cs
+++ src/TestFramework/MSTest.Core/Assertions/Assert.cs
@@ -191,22 +191,4 @@ public sealed partial class Assert
         return false;
     } 
     #endregion
-
-    #region EqualsAssertion
-    /// <summary>
-    /// Static equals overloads are used for comparing instances of two types for reference
-    /// equality. This method should <b>not</b> be used for comparison of two instances for
-    /// equality. This object will <b>always</b> throw with Assert.Fail. Please use
-    /// Assert.AreEqual and associated overloads in your unit tests.
-    /// </summary>
-    /// <param name="objA"> Object A </param>
-    /// <param name="objB"> Object B </param>
-    /// <returns> False, always. </returns>
-    [SuppressMessage("Microsoft.Naming", "CA1720:IdentifiersShouldNotContainTypeNames", MessageId = "obj", Justification = "We want to compare 'object A' with 'object B', so it makes sense to have 'obj' in the parameter name")]
-    public static new bool Equals(object objA, object objB)
-    {
-        Fail(FrameworkMessages.DoNotUseAssertEquals);
-        return false;
-    } 
-    #endregion
 }
