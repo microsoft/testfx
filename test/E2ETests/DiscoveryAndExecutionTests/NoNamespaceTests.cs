3a773d055a767f9872bc0a7f1b8d4501dc2659fd
Amaury Levé <amauryleve@microsoft.com>
2022-09-27 11:41:26 +0200

    Fix issue causing null ref when test class has no namespace (#1283)

---
+++ test/E2ETests/DiscoveryAndExecutionTests/NoNamespaceTests.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.MSTestV2.Smoke.DiscoveryAndExecutionTests;
+
+using Microsoft.MSTestV2.CLIAutomation;
+
+using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.Extensions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
+public class NoNamespaceTests : CLITestBase
+{
+    private const string TestAssembly = "HierarchyProject.dll";
+
+    public void TestsAreDiscoveredWithExpectedHierarchy()
+    {
+        // Arrange & Act
+        var testCases = DiscoverTests(GetAssetFullPath(TestAssembly));
+
+        // Assert
+        Verify(testCases.Count == 2);
+
+        VerifyHierarchy(testCases[0], null, "ClassWithNoNamespace", "MyMethodUnderTest");
+        VerifyHierarchy(testCases[1], "SomeNamespace.WithMultipleLevels", "ClassWithNamespace", "MyMethodUnderTest");
+    }
+
+    private static void VerifyHierarchy(TestCase testCase, string expectedNamespace, string expectedClassName, string expectedMethodName)
+    {
+        var hierarchy = testCase.GetPropertyValue(TestCaseExtensions.HierarchyProperty) as string[];
+        Verify(hierarchy?.Length == 4);
+        // This level is always null.
+        Verify(hierarchy[0] == null);
+        Verify(hierarchy[1] == expectedNamespace);
+        Verify(hierarchy[2] == expectedClassName);
+        Verify(hierarchy[3] == expectedMethodName);
+    }
+}
