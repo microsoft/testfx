0fe3d0d8ce08f64242a91a3c657cd2f8f441c326
Engy <enjieid@microsoft.com>
2022-10-11 17:34:05 +0200

    clean the assembly class

--- test/E2ETests/TestAssets/SuiteLifeCycleTestProject/SuiteLifeCycleTestClass_AssemblyInitializeAndCleanup.cs
+++ test/E2ETests/TestAssets/SuiteLifeCycleTestProject/SuiteLifeCycleTestClass_AssemblyInitializeAndCleanup.cs
@@ -4,79 +4,20 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Diagnostics;
-#if NET6_0_OR_GREATER
-using System.Threading.Tasks; 
-#endif
-
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SuiteLifeCycleTestProject;
 
 [TestClass]
-public class SuiteLifeCycleTestClass_AssemblyInitializeAndCleanup : IDisposable
-#if NET6_0_OR_GREATER
-        , IAsyncDisposable 
-#endif
+public class SuiteLifeCycleTestClass_AssemblyInitializeAndCleanup
 {
     private static TestContext s_testContext;
-
-    public TestContext TestContext { get; set; }
-
-    public SuiteLifeCycleTestClass_AssemblyInitializeAndCleanup()
-    {
-        s_testContext.WriteLine("Ctor was called");
-    }
 
     [AssemblyInitialize]
     public static void AssemblyInit(TestContext testContext)
     {
         s_testContext = testContext;
         s_testContext.WriteLine("AssemblyInit was called");
-    }
-
-    [ClassInitialize]
-    public static void ClassInitialize(TestContext testContext)
-    {
-        s_testContext.WriteLine("ClassInitialize was called");
-    }
-
-    [TestInitialize]
-    public void TestInitialize()
-    {
-        TestContext.WriteLine("TestInitialize was called");
-    }
-
-    [TestMethod]
-    public void TestMethod()
-    {
-        TestContext.WriteLine("TestMethod was called");
-    }
-
-    [TestCleanup]
-    public void TestCleanup()
-    {
-        TestContext.WriteLine("TestCleanup was called");
-    }
-
-    public void Dispose()
-    {
-        TestContext.WriteLine("Dispose was called");
-    }
-
-#if NET6_0_OR_GREATER
-    public ValueTask DisposeAsync()
-    {
-        TestContext.WriteLine("DisposeAsync was called");
-        return ValueTask.CompletedTask;
-    }
-#endif
-
-    [ClassCleanup]
-    public static void ClassCleanup()
-    {
-        s_testContext.WriteLine("ClassCleanup was called");
     }
 
     [AssemblyCleanup]
