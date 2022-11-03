c6377e6fd62909308a92b94e8663f104feedff24
Amaury Levé <amauryleve@microsoft.com>
2022-11-03 21:57:15 +0100

    Fix CI
    Fix recursion caused by search and replace
    Fix Debug.Assert

--- src/TestFramework/TestFramework/Internal/StringEx.cs
+++ src/TestFramework/TestFramework/Internal/StringEx.cs
@@ -1,18 +1,16 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#nullable enable
-
 using System.Diagnostics.CodeAnalysis;
 
 namespace Microsoft.VisualStudio.TestTools.UnitTesting;
 
 internal static class StringEx
 {
-    /// <inheritdoc cref="StringEx.IsNullOrEmpty(string)"/>
+    /// <inheritdoc cref="string.IsNullOrEmpty(string)"/>
     [SuppressMessage("ApiDesign", "RS0030:Do not used banned APIs", Justification = "Replacement API to allow nullable hints for compiler")]
     public static bool IsNullOrEmpty([NotNullWhen(returnValue: false)] string? value)
-        => StringEx.IsNullOrEmpty(value);
+        => string.IsNullOrEmpty(value);
 
     /// <inheritdoc cref="string.IsNullOrWhiteSpace(string)"/>
     [SuppressMessage("ApiDesign", "RS0030:Do not used banned APIs", Justification = "Replacement API to allow nullable hints for compiler")]
