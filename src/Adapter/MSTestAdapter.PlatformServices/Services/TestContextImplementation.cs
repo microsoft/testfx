25393972b54e990d1d05e5f308d1a94217ac8ee2
Engy <enjieid@microsoft.com>
2022-12-19 15:04:32 +0100

    Update TestContext.Properties return type to IDictionary<string, object>

--- src/Adapter/MSTestAdapter.PlatformServices/Services/TestContextImplementation.cs
+++ src/Adapter/MSTestAdapter.PlatformServices/Services/TestContextImplementation.cs
@@ -49,7 +49,7 @@ public class TestContextImplementation : TestContext, ITestContext
     /// <summary>
     /// Properties.
     /// </summary>
-    private IDictionary<string, object?> _properties;
+    private Dictionary<string, object?> _properties;
 
     /// <summary>
     /// Unit test outcome.
@@ -116,7 +116,7 @@ public class TestContextImplementation : TestContext, ITestContext
 #endif
 
     /// <inheritdoc/>
-    public override IDictionary Properties => (IDictionary)_properties;
+    public override Dictionary<string, object?> Properties => _properties;
 
 #if !WINDOWS_UWP && !WIN_UI
     /// <inheritdoc/>
