c993efab429a241f7373378c1f2a590e960479b5
Amaury Levé <amauryleve@microsoft.com>
2023-11-20 12:42:30 +0100

    Add MSTest to auto-generated entry point (#1793)

---
+++ src/Adapter/MSTest.TestAdapter/TestingPlatformAdapter/TestingPlatformBuilderHook.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Reflection;
+
+using Microsoft.Testing.Platform.Builder;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Testing.Platform.Extensions.MSTest;
+
+public static class TestingPlatformBuilderHook
+{
+    public static void AddExtensions(ITestApplicationBuilder testApplicationBuilder, string[] arguments)
+    {
+        testApplicationBuilder.AddMSTest(() => new[] { Assembly.GetEntryAssembly()! });
+    }
+}
