480b8ac656f7768faec449c8eb62c06cbbf538b9
Jakub Jareš <me@jakubjares.com>
2023-11-21 19:00:34 +0100

    any other complaints about comments?

--- test/UnitTests/Microsoft.Testing.Platform.UnitTests/Program.cs
+++ test/UnitTests/Microsoft.Testing.Platform.UnitTests/Program.cs
@@ -25,7 +25,6 @@ builder.AddCodeCoverage();
 
 var commandLine = new FakeTrxReportGeneratorCommandLine();
 builder.CommandLine.AddProvider(() => commandLine);
-// builder.AddAppInsightsTelemetryProvider();
 
 // Custom suite tools
 CompositeExtensionFactory<SlowestTestsConsumer> slowestTestCompositeServiceFactory
