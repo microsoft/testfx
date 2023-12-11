e3d00882e7549dbfa9de017a118875a21fb0b6ac
Marco Rossignoli <mrossignol@microsoft.com>
2023-12-11 15:22:45 +0100

    save work

--- test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/Helpers/BaseAcceptance.cs
+++ test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/Helpers/BaseAcceptance.cs
@@ -1,6 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Runtime.InteropServices;
+using System.Threading;
+
+using Microsoft.Testing.Platform.Hosts;
+
+using NuGet.Packaging;
+
 namespace Microsoft.Testing.Platform.Acceptance.IntegrationTests;
 
 /// <summary>
@@ -10,6 +17,39 @@ namespace Microsoft.Testing.Platform.Acceptance.IntegrationTests;
 /// </summary>
 public abstract class BaseAcceptanceTests : TestBase
 {
+    internal static string RID { get; private set; } = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "win-x64" : "linux-x64";
+
+    private static readonly string MsTestCurrentVersion = string.Empty;
+
+    static BaseAcceptanceTests()
+    {
+        foreach (var package in Directory.GetFiles(DotnetMuxer.ARTIFACTS_PACKAGES_NONSHIPPING, "*.nupkg", SearchOption.AllDirectories)
+            .Union(Directory.GetFiles(DotnetMuxer.ARTIFACTS_PACKAGES_SHIPPING, "*.nupkg", SearchOption.AllDirectories)).Distinct())
+        {
+            using FileStream fs = File.OpenRead(package);
+            var packageReader = new PackageArchiveReader(fs);
+            var version = packageReader.NuspecReader.GetVersion();
+            if (version.OriginalVersion is null)
+            {
+                throw new InvalidOperationException("Unexpected null nupkg version");
+            }
+
+            if (MsTestCurrentVersion == string.Empty)
+            {
+                MsTestCurrentVersion = version.OriginalVersion;
+            }
+            else
+            {
+                if (version.OriginalVersion != MsTestCurrentVersion)
+                {
+                    throw new InvalidOperationException($"Unexpected different package version found in the package folder {version.OriginalVersion} != {MsTestCurrentVersion}");
+                }
+            }
+        }
+    }
+
+    public string MSTestCurrentVersion => MsTestCurrentVersion;
+
     protected BaseAcceptanceTests(ITestExecutionContext testExecutionContext, AcceptanceFixture acceptanceFixture)
         : base(testExecutionContext)
     {
@@ -17,4 +57,38 @@ public abstract class BaseAcceptanceTests : TestBase
     }
 
     public AcceptanceFixture AcceptanceFixture { get; }
+
+    internal static IEnumerable<BuildConfiguration> GetBuildConfiguration()
+    {
+        string[] compilationModes = new[] { "Debug", "Release" };
+        foreach (string compilationMode in compilationModes)
+        {
+            yield return compilationMode == "Debug" ? BuildConfiguration.Debug : BuildConfiguration.Release;
+        }
+    }
+
+    internal static TestArgumentsEntry<(string Tfm, BuildConfiguration BuildConfiguration)> FormatGetBuildMatrixTfmBuildConfigurationEntry(TestArgumentsContext ctx)
+    {
+        var entry = ((string, BuildConfiguration))ctx.Arguments;
+        return new TestArgumentsEntry<(string, BuildConfiguration)>(entry, $"{entry.Item1},{entry.Item2}");
+    }
+
+    internal static IEnumerable<(string Tfm, BuildConfiguration BuildConfiguration)> GetBuildMatrixTfmBuildConfiguration()
+    {
+        foreach (TestArgumentsEntry<string> tfm in All_Tfms)
+        {
+            foreach (BuildConfiguration compilationMode in GetBuildConfiguration())
+            {
+                yield return (tfm.Arguments, compilationMode);
+            }
+        }
+    }
+
+    internal static IEnumerable<(string TargetFrameworksElementContent, BuildConfiguration BuildConfiguration)> GetBuildMatrixMultiTfm()
+    {
+        foreach (BuildConfiguration compilationMode in GetBuildConfiguration())
+        {
+            yield return (All_Tfms.ToTargetFrameworksElementContent(), compilationMode);
+        }
+    }
 }
---
+++ test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/Runner.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Testing.Platform.Acceptance.IntegrationTests;
+
+[TestGroup]
+public class RunnerTests : BaseAcceptanceTests
+{
+    private readonly AcceptanceFixture _acceptanceFixture;
+    private const string AssetName = "MSTestProject";
+
+    public RunnerTests(ITestExecutionContext testExecutionContext, AcceptanceFixture acceptanceFixture)
+        : base(testExecutionContext, acceptanceFixture)
+    {
+        _acceptanceFixture = acceptanceFixture;
+    }
+
+    [ArgumentsProvider(nameof(GetBuildMatrixTfmBuildConfiguration), TestArgumentsEntryProviderMethodName = nameof(FormatGetBuildMatrixTfmBuildConfigurationEntry))]
+    public async Task EnableMSTestRunner_True_Will_Run_Standalone(string tfm, BuildConfiguration buildConfiguration)
+    {
+        using TestAsset generator = await TestAsset.GenerateAssetAsync(
+            AssetName,
+            CurrentTemplateSourceCode.PatchCodeWithRegularExpression("tfm", tfm).PatchCodeWithRegularExpression("mstestversion", MSTestCurrentVersion));
+        string binlogFile = Path.Combine(generator.TargetAssetPath, "msbuild.binlog");
+        var compilationResult = await DotnetCli.RunAsync($"restore -nodeReuse:false {generator.TargetAssetPath} -r {RID}", _acceptanceFixture.NuGetGlobalPackagesFolder);
+        compilationResult = await DotnetCli.RunAsync($"build -nodeReuse:false {generator.TargetAssetPath} -c {buildConfiguration} -bl:{binlogFile} -r {RID}", _acceptanceFixture.NuGetGlobalPackagesFolder);
+        var testHost = TestInfrastructure.TestHost.LocateFrom(generator.TargetAssetPath, AssetName, tfm, buildConfiguration: buildConfiguration);
+        var testHostResult = await testHost.ExecuteAsync(environmentVariables: new Dictionary<string, string>() { { "TESTINGPLATFORM_TELEMETRY_OPTOUT", "1" } });
+        Assert.IsTrue(testHostResult.StandardOutput.Contains("Passed! - Failed: 0, Passed: 1, Skipped: 0, Total: 1"));
+    }
+
+    private const string CurrentTemplateSourceCode = """
+#file MSTestProject.csproj
+<Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <TargetFramework>tfm</TargetFramework>
+    <IsPackable>false</IsPackable>
+    <IsTestProject>true</IsTestProject>
+    <EnableMSTestRunner>true</EnableMSTestRunner>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <PackageReference Include="Microsoft.NET.Test.Sdk" Version="*" />
+    <PackageReference Include="MSTest.TestAdapter" Version="mstestversion" />
+    <PackageReference Include="MSTest.TestFramework" Version="mstestversion" />
+    <PackageReference Include="coverlet.collector" Version="6.0.0" />
+  </ItemGroup>
+
+</Project>
+
+#file UnitTest1.cs
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
+""";
+}
