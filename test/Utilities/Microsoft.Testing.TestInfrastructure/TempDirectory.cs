0154c9fb6c212d0db58c8d84741ad655e9e9db5d
Amaury Levé <amauryleve@microsoft.com>
2023-12-11 10:06:31 +0100

    Fix formatting for TempDirectory

--- test/Utilities/Microsoft.Testing.TestInfrastructure/TempDirectory.cs
+++ test/Utilities/Microsoft.Testing.TestInfrastructure/TempDirectory.cs
@@ -204,16 +204,15 @@ public class TempDirectory : IDisposable
         }
     }
 
-    private static string GetTempPath() =>
-
-        // AGENT_TEMPDIRECTORY is Azure DevOps variable, which is set to path
-        // that is cleaned up after every job. This is preferable to use over
-        // just the normal TEMP, because that is not cleaned up for every run.
-        //
-        // System.IO.Path.GetTempPath is banned from the rest of the code. This is the only
-        // place where we are allowed to use it. All other methods should use our GetTempPath (this method).
-        Environment.GetEnvironmentVariable("AGENT_TEMPDIRECTORY")
-            ?? Path.GetTempPath();
+    // AGENT_TEMPDIRECTORY is Azure DevOps variable, which is set to path
+    // that is cleaned up after every job. This is preferable to use over
+    // just the normal TEMP, because that is not cleaned up for every run.
+    //
+    // System.IO.Path.GetTempPath is banned from the rest of the code. This is the only
+    // place where we are allowed to use it. All other methods should use our GetTempPath (this method).
+    private static string GetTempPath()
+        => Environment.GetEnvironmentVariable("AGENT_TEMPDIRECTORY")
+        ?? Path.GetTempPath();
 
     public static void TryRemoveDirectory(string directory)
     {
