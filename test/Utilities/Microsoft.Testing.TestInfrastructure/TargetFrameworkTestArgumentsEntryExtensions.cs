ec7f3491a6c1558d5d185df864f282951385184d
Amaury Levé <amauryleve@microsoft.com>
2023-12-11 09:02:30 +0100

    Update platform test infra

---
+++ test/Utilities/Microsoft.Testing.TestInfrastructure/TargetFrameworkTestArgumentsEntryExtensions.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Testing.Framework;
+
+namespace Microsoft.Testing.TestInfrastructure;
+
+public static class TargetFrameworkTestArgumentsEntryExtensions
+{
+    public static string ToTargetFrameworksElementContent(this TestArgumentsEntry<string>[] targetFrameworksEntries)
+        => string.Join(";", targetFrameworksEntries.Select(x => x.Arguments));
+}
