9562eafbe3e0dca8ad4101ce3b29aea8846dd18b
Amaury Levé <amauryleve@microsoft.com>
2023-12-11 12:26:05 +0100

    Refactor helper

--- test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/Helpers/AcceptanceAssert.cs
+++ test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/Helpers/AcceptanceAssert.cs
@@ -8,11 +8,17 @@ namespace Microsoft.Testing.Platform.Acceptance.IntegrationTests.Helpers;
 internal static class AcceptanceAssert
 {
     public static void HasExitCode(int exitCode, TestHostResult testHostResult)
-        => Assert.That(exitCode == testHostResult.ExitCode, $"Output of the test host is:\n{testHostResult}");
+        => Assert.That(exitCode == testHostResult.ExitCode, GenerateFailedAssertionMessage(testHostResult));
 
     public static void OutputMatchesRegex(string pattern, TestHostResult testHostResult)
-        => Assert.That(Regex.IsMatch(testHostResult.StandardOutput, pattern), $"Output of the test host is:\n{testHostResult}");
+        => Assert.That(Regex.IsMatch(testHostResult.StandardOutput, pattern), GenerateFailedAssertionMessage(testHostResult));
 
     public static void OutputDoesNotMatchRegex(string pattern, TestHostResult testHostResult)
-        => Assert.That(!Regex.IsMatch(testHostResult.StandardOutput, pattern), $"Output of the test host is:\n{testHostResult}");
+        => Assert.That(!Regex.IsMatch(testHostResult.StandardOutput, pattern), GenerateFailedAssertionMessage(testHostResult));
+
+    public static void OutputContains(string pattern, TestHostResult testHostResult)
+        => Assert.That(testHostResult.StandardOutput.Contains(pattern, StringComparison.Ordinal), GenerateFailedAssertionMessage(testHostResult));
+
+    private static string GenerateFailedAssertionMessage(TestHostResult testHostResult)
+        => $"Output of the test host is:\n{testHostResult}";
 }
