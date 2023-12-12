7089af8708bf38d4e74752f68d40aa437dc4cb5c
Marco Rossignoli <mrossignol@microsoft.com>
2023-12-12 15:13:43 +0100

    save work

--- test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/Runner.cs
+++ test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/Runner.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics;
+
 namespace Microsoft.Testing.Platform.Acceptance.IntegrationTests;
 
 [TestGroup]
@@ -15,18 +17,38 @@ public class RunnerTests : BaseAcceptanceTests
         _acceptanceFixture = acceptanceFixture;
     }
 
+    // [ArgumentsProvider(nameof(GetBuildMatrixTfmBuildConfiguration), TestArgumentsEntryProviderMethodName = nameof(FormatGetBuildMatrixTfmBuildConfigurationEntry))]
+    // public async Task EnableMSTestRunner_True_Will_Run_Standalone(string tfm, BuildConfiguration buildConfiguration)
+    // {
+    //    using TestAsset generator = await TestAsset.GenerateAssetAsync(
+    //        AssetName,
+    //        CurrentTemplateSourceCode
+    //        .PatchCodeWithRegularExpression("tfm", tfm)
+    //        .PatchCodeWithRegularExpression("mstestversion", MSTestCurrentVersion)
+    //        .PatchCodeWithRegularExpression("enablemstestrunner", "<EnableMSTestRunner>true</EnableMSTestRunner>"));
+    //    string binlogFile = Path.Combine(generator.TargetAssetPath, "msbuild.binlog");
+    //    var compilationResult = await DotnetCli.RunAsync($"restore -nodeReuse:false {generator.TargetAssetPath} -r {RID}", _acceptanceFixture.NuGetGlobalPackagesFolder);
+    //    compilationResult = await DotnetCli.RunAsync($"build -nodeReuse:false {generator.TargetAssetPath} -c {buildConfiguration} -bl:{binlogFile} -r {RID}", _acceptanceFixture.NuGetGlobalPackagesFolder);
+    //    var testHost = TestInfrastructure.TestHost.LocateFrom(generator.TargetAssetPath, AssetName, tfm, buildConfiguration: buildConfiguration);
+    //    var testHostResult = await testHost.ExecuteAsync(environmentVariables: new Dictionary<string, string>() { { "TESTINGPLATFORM_TELEMETRY_OPTOUT", "1" } });
+    //    Assert.IsTrue(testHostResult.StandardOutput.Contains("Passed! - Failed: 0, Passed: 1, Skipped: 0, Total: 1"));
+    // }
     [ArgumentsProvider(nameof(GetBuildMatrixTfmBuildConfiguration), TestArgumentsEntryProviderMethodName = nameof(FormatGetBuildMatrixTfmBuildConfigurationEntry))]
-    public async Task EnableMSTestRunner_True_Will_Run_Standalone(string tfm, BuildConfiguration buildConfiguration)
+    public async Task EnableMSTestRunner_False_Will_Run_Empty_Program_EntryPoint_From_Tpv2_SDK(string tfm, BuildConfiguration buildConfiguration)
     {
         using TestAsset generator = await TestAsset.GenerateAssetAsync(
             AssetName,
-            CurrentTemplateSourceCode.PatchCodeWithRegularExpression("tfm", tfm).PatchCodeWithRegularExpression("mstestversion", MSTestCurrentVersion));
+            CurrentTemplateSourceCode
+            .PatchCodeWithRegularExpression("tfm", tfm)
+            .PatchCodeWithRegularExpression("mstestversion", MSTestCurrentVersion)
+            .PatchCodeWithRegularExpression("enablemstestrunner", string.Empty));
         string binlogFile = Path.Combine(generator.TargetAssetPath, "msbuild.binlog");
         var compilationResult = await DotnetCli.RunAsync($"restore -nodeReuse:false {generator.TargetAssetPath} -r {RID}", _acceptanceFixture.NuGetGlobalPackagesFolder);
-        compilationResult = await DotnetCli.RunAsync($"build -nodeReuse:false {generator.TargetAssetPath} -c {buildConfiguration} -bl:{binlogFile} -r {RID}", _acceptanceFixture.NuGetGlobalPackagesFolder);
+        compilationResult = await DotnetCli.RunAsync($"build -nodeReuse:false {generator.TargetAssetPath} -c {buildConfiguration} -bl:{binlogFile} -r {RID}", _acceptanceFixture.NuGetGlobalPackagesFolder,
+            failIfReturnValueIsNotZero: !Debugger.IsAttached);
         var testHost = TestInfrastructure.TestHost.LocateFrom(generator.TargetAssetPath, AssetName, tfm, buildConfiguration: buildConfiguration);
         var testHostResult = await testHost.ExecuteAsync(environmentVariables: new Dictionary<string, string>() { { "TESTINGPLATFORM_TELEMETRY_OPTOUT", "1" } });
-        Assert.IsTrue(testHostResult.StandardOutput.Contains("Passed! - Failed: 0, Passed: 1, Skipped: 0, Total: 1"));
+        Assert.AreEqual(string.Empty, testHostResult.StandardOutput);
     }
 
     private const string CurrentTemplateSourceCode = """
@@ -37,7 +59,7 @@ public class RunnerTests : BaseAcceptanceTests
     <TargetFramework>tfm</TargetFramework>
     <IsPackable>false</IsPackable>
     <IsTestProject>true</IsTestProject>
-    <EnableMSTestRunner>true</EnableMSTestRunner>
+    enablemstestrunner
   </PropertyGroup>
 
   <ItemGroup>
