353ee2fcc981a038868a9d767e15a3fbd872368b
Amaury Levé <amauryleve@microsoft.com>
2023-12-15 21:13:48 +0100

    PatchTargetFrameworks accept params array

--- test/Utilities/Microsoft.Testing.TestInfrastructure/SourceCodeExtensions.cs
+++ test/Utilities/Microsoft.Testing.TestInfrastructure/SourceCodeExtensions.cs
@@ -10,6 +10,6 @@ public static class SourceCodeExtensions
     public static string PatchCodeWithReplace(this string code, string pattern, string value)
         => code.Replace(pattern, value);
 
-    public static string PatchTargetFrameworks(this string code, TestArgumentsEntry<string>[] targetFrameworks)
+    public static string PatchTargetFrameworks(this string code, params TestArgumentsEntry<string>[] targetFrameworks)
         => PatchCodeWithReplace(code, "$TargetFrameworks$", targetFrameworks.ToMSBuildTargetFrameworks());
 }
