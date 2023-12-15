e2266ca6f26ba6992a4335e87010bda7a33ec740
Marco Rossignoli <mrossignol@microsoft.com>
2023-12-15 16:04:23 +0100

    fix baseline

--- test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/Helpers/AcceptanceTestBase.cs
+++ test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/Helpers/AcceptanceTestBase.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Runtime.InteropServices;
+using System.Xml.Linq;
 
 namespace Microsoft.Testing.Platform.Acceptance.IntegrationTests;
 
