a8d68f756abc6b8985f8a874195f7788b2ed19c7
Marco Rossignoli <mrossignol@microsoft.com>
2023-12-18 14:11:19 +0100

    fix search for nix

--- test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/MSBuildMSTestRunnerTests.cs
+++ test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/MSBuildMSTestRunnerTests.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Text;
-
 using Microsoft.Testing.Platform.Acceptance.IntegrationTests.Helpers;
 
 namespace Microsoft.Testing.Platform.Acceptance.IntegrationTests;
@@ -37,64 +35,51 @@ public class MSBuildMSTestRunnerTests : AcceptanceTestBase
     [ArgumentsProvider(nameof(GetBuildMatrixSingleAndMultiTfmBuildConfiguration))]
     public async Task MSBuildTestTarget_SingleAndMultiTfm_Should_Run_Solution_Tests(string singleTfmOrMultiTfm, BuildConfiguration buildConfiguration, bool isMultiTfm)
     {
-        StringBuilder stringBuilder = new();
-        try
+        // Get the template project
+        using TestAsset generator = await TestAsset.GenerateAssetAsync(
+           AssetName,
+           CurrentMSTestSourceCode
+           .PatchCodeWithReplace("$TargetFramework$", isMultiTfm ? $"<TargetFrameworks>{singleTfmOrMultiTfm}</TargetFrameworks>" : $"<TargetFramework>{singleTfmOrMultiTfm}</TargetFramework>")
+           .PatchCodeWithReplace("$MicrosoftNETTestSdkVersion$", MicrosoftNETTestSdkVersion)
+           .PatchCodeWithReplace("$MSTestVersion$", MSTestVersion)
+           .PatchCodeWithReplace("$EnableMSTestRunner$", "<EnableMSTestRunner>true</EnableMSTestRunner>")
+           .PatchCodeWithReplace("$OutputType$", "<OutputType>Exe</OutputType>")
+           .PatchCodeWithReplace("$Extra$", string.Empty),
+           addPublicFeeds: true);
+
+        string projectContent = File.ReadAllText(Directory.GetFiles(generator.TargetAssetPath, "MSTestProject.csproj", SearchOption.AllDirectories).Single());
+        string testSourceContent = File.ReadAllText(Directory.GetFiles(generator.TargetAssetPath, "UnitTest1.cs", SearchOption.AllDirectories).Single());
+        string nugetConfigContent = File.ReadAllText(Directory.GetFiles(generator.TargetAssetPath, "NuGet.config", SearchOption.AllDirectories).Single());
+
+        // Create a solution with 3 projects
+        using TempDirectory tempDirectory = new();
+        string solutionFolder = Path.Combine(tempDirectory.Path, "Solution");
+        VSSolution solution = new(solutionFolder, "MSTestSolution");
+        solution.AddOrUpdateFileContent("Nuget.config", nugetConfigContent);
+        for (int i = 0; i < 3; i++)
         {
-            // Get the template project
-            using TestAsset generator = await TestAsset.GenerateAssetAsync(
-               AssetName,
-               CurrentMSTestSourceCode
-               .PatchCodeWithReplace("$TargetFramework$", isMultiTfm ? $"<TargetFrameworks>{singleTfmOrMultiTfm}</TargetFrameworks>" : $"<TargetFramework>{singleTfmOrMultiTfm}</TargetFramework>")
-               .PatchCodeWithReplace("$MicrosoftNETTestSdkVersion$", MicrosoftNETTestSdkVersion)
-               .PatchCodeWithReplace("$MSTestVersion$", MSTestVersion)
-               .PatchCodeWithReplace("$EnableMSTestRunner$", "<EnableMSTestRunner>true</EnableMSTestRunner>")
-               .PatchCodeWithReplace("$OutputType$", "<OutputType>Exe</OutputType>")
-               .PatchCodeWithReplace("$Extra$", string.Empty),
-               addPublicFeeds: true);
+            CSharpProject project = solution.CreateCSharpProject($"TestProject{i}", isMultiTfm ? singleTfmOrMultiTfm.Split(';') : new[] { singleTfmOrMultiTfm });
+            File.WriteAllText(project.ProjectFile, projectContent);
+            project.AddOrUpdateFileContent("UnitTest1.cs", testSourceContent);
+        }
 
-            foreach (var item in Directory.GetFiles(generator.TargetAssetPath, "*.*", SearchOption.AllDirectories))
+        // Build the solution
+        await DotnetCli.RunAsync($"build -nodeReuse:false {solution.SolutionFile}", _acceptanceFixture.NuGetGlobalPackagesFolder.Path);
+        var testResult = await DotnetCli.RunAsync($"msbuild /t:Test {solution.SolutionFile}", _acceptanceFixture.NuGetGlobalPackagesFolder.Path);
+        if (isMultiTfm)
+        {
+            foreach (string tfm in singleTfmOrMultiTfm.Split(';'))
             {
-                stringBuilder.AppendLine(item);
-            }
-
-            string projectContent = File.ReadAllText(Directory.GetFiles(generator.TargetAssetPath, "MSTestProject.csproj", SearchOption.AllDirectories).Single());
-            string testSourceContent = File.ReadAllText(Directory.GetFiles(generator.TargetAssetPath, "UnitTest1.cs", SearchOption.AllDirectories).Single());
-            string nugetConfigContent = File.ReadAllText(Directory.GetFiles(generator.TargetAssetPath, "Nuget.config", SearchOption.AllDirectories).Single());
-
-            // Create a solution with 3 projects
-            using TempDirectory tempDirectory = new();
-            string solutionFolder = Path.Combine(tempDirectory.Path, "Solution");
-            VSSolution solution = new(solutionFolder, "MSTestSolution");
-            solution.AddOrUpdateFileContent("Nuget.config", nugetConfigContent);
-            for (int i = 0; i < 3; i++)
-            {
-                CSharpProject project = solution.CreateCSharpProject($"TestProject{i}", isMultiTfm ? singleTfmOrMultiTfm.Split(';') : new[] { singleTfmOrMultiTfm });
-                File.WriteAllText(project.ProjectFile, projectContent);
-                project.AddOrUpdateFileContent("UnitTest1.cs", testSourceContent);
-            }
-
-            // Build the solution
-            await DotnetCli.RunAsync($"build -nodeReuse:false {solution.SolutionFile}", _acceptanceFixture.NuGetGlobalPackagesFolder.Path);
-            var testResult = await DotnetCli.RunAsync($"msbuild /t:Test {solution.SolutionFile}", _acceptanceFixture.NuGetGlobalPackagesFolder.Path);
-            if (isMultiTfm)
-            {
-                foreach (string tfm in singleTfmOrMultiTfm.Split(';'))
-                {
-                    testResult.AssertOutputRegEx($@"Tests succeeded: '.*TestProject0\..*' \[{tfm}\|x64\]");
-                    testResult.AssertOutputRegEx($@"Tests succeeded: '.*TestProject1\..*' \[{tfm}\|x64\]");
-                    testResult.AssertOutputRegEx($@"Tests succeeded: '.*TestProject2\..*' \[{tfm}\|x64\]");
-                }
-            }
-            else
-            {
-                testResult.AssertOutputRegEx($@"Tests succeeded: '.*TestProject0\..*' \[{singleTfmOrMultiTfm}\|x64\]");
-                testResult.AssertOutputRegEx($@"Tests succeeded: '.*TestProject1\..*' \[{singleTfmOrMultiTfm}\|x64\]");
-                testResult.AssertOutputRegEx($@"Tests succeeded: '.*TestProject2\..*' \[{singleTfmOrMultiTfm}\|x64\]");
+                testResult.AssertOutputRegEx($@"Tests succeeded: '.*TestProject0\..*' \[{tfm}\|x64\]");
+                testResult.AssertOutputRegEx($@"Tests succeeded: '.*TestProject1\..*' \[{tfm}\|x64\]");
+                testResult.AssertOutputRegEx($@"Tests succeeded: '.*TestProject2\..*' \[{tfm}\|x64\]");
             }
         }
-        catch (Exception ex)
+        else
         {
-            throw new InvalidOperationException(ex.Message + Environment.NewLine + stringBuilder.ToString());
+            testResult.AssertOutputRegEx($@"Tests succeeded: '.*TestProject0\..*' \[{singleTfmOrMultiTfm}\|x64\]");
+            testResult.AssertOutputRegEx($@"Tests succeeded: '.*TestProject1\..*' \[{singleTfmOrMultiTfm}\|x64\]");
+            testResult.AssertOutputRegEx($@"Tests succeeded: '.*TestProject2\..*' \[{singleTfmOrMultiTfm}\|x64\]");
         }
     }
 }
