bcd043e23e58b6c5fadd1fbf2c211456cdf123ac
Marco Rossignoli <mrossignol@microsoft.com>
2023-12-27 18:32:48 +0100

    fix build

--- test/Performance/MSTest.Performance.Runner/Steps/DotnetTrace.cs
+++ test/Performance/MSTest.Performance.Runner/Steps/DotnetTrace.cs
@@ -1,12 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.CommandLine;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO.Compression;
 using System.Runtime.InteropServices;
-using System.Text;
 
 using Microsoft.Testing.TestInfrastructure;
 
