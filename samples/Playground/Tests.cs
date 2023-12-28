d3c0c77c9e46706289351da586aa8eeef36affd7
Marco Rossignoli <mrossignol@microsoft.com>
2023-12-28 19:05:27 +0100

    suppress warning

--- samples/Playground/Tests.cs
+++ samples/Playground/Tests.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#pragma warning disable VSTHRD200 // Use "Async" suffix for async methods
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [assembly: Parallelize(Scope = ExecutionScope.ClassLevel, Workers = 0)]
