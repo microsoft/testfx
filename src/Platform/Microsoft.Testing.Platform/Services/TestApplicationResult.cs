3dd939d27a8853a95d3c7c8831aaaab1cb440a36
Marco Rossignoli <mrossignol@microsoft.com>
2024-01-05 12:38:40 +0100

    Add intergration tests

--- src/Platform/Microsoft.Testing.Platform/Services/TestApplicationResult.cs
+++ src/Platform/Microsoft.Testing.Platform/Services/TestApplicationResult.cs
@@ -117,7 +117,7 @@ internal sealed class TestApplicationResult(
 
         // If the user has specified the IgnoreExitCode, then we don't want to return a non-zero exit code if the exit code matches the one specified.
         string? exitCodeToIgnore = _environment.GetEnvironmentVariable(EnvironmentVariableConstants.TESTINGPLATFORM_EXITCODE_IGNORE);
-        if (exitCodeToIgnore is null)
+        if (TAString.IsNullOrEmpty(exitCodeToIgnore))
         {
             if (_commandLineOptions.TryGetOptionArgumentList(PlatformCommandLineProvider.IgnoreExitCodeOptionKey, out string[]? commandLineExitCodes) && commandLineExitCodes.Length > 0)
             {
@@ -129,7 +129,7 @@ internal sealed class TestApplicationResult(
         {
             if (exitCodeToIgnore.Split(';').Any(code => code == exitCode.ToString(CultureInfo.InvariantCulture)))
             {
-                return 0;
+                exitCode = ExitCodes.Success;
             }
         }
 
