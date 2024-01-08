00f3bd63309b1d3db1a7d1b72a6809ab27d6c6b8
Amaury Levé <amauryleve@microsoft.com>
2024-01-08 11:52:01 +0100

    Add samples of runner vs dotnet test

---
+++ samples/runner_vs_vstest/1000C_100M/MyTestClass0786.cs
@@ -0,0 +1,610 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyNamespace;
+
+[TestClass]
+public class MyTestClass0786
+{
+    [TestMethod]
+    public void MyTestMethod001()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod002()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod003()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod004()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod005()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod006()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod007()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod008()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod009()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod010()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod011()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod012()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod013()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod014()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod015()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod016()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod017()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod018()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod019()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod020()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod021()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod022()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod023()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod024()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod025()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod026()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod027()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod028()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod029()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod030()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod031()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod032()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod033()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod034()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod035()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod036()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod037()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod038()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod039()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod040()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod041()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod042()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod043()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod044()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod045()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod046()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod047()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod048()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod049()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod050()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod051()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod052()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod053()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod054()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod055()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod056()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod057()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod058()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod059()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod060()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod061()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod062()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod063()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod064()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod065()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod066()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod067()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod068()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod069()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod070()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod071()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod072()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod073()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod074()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod075()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod076()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod077()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod078()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod079()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod080()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod081()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod082()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod083()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod084()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod085()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod086()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod087()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod088()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod089()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod090()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod091()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod092()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod093()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod094()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod095()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod096()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod097()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod098()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod099()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void MyTestMethod100()
+    {
+        Assert.IsTrue(true);
+    }
+}
