f8986da99c0627909939fdb08e3a2c70fa554f98
Marco Rossignoli <mrossignol@microsoft.com>
2024-01-10 10:40:42 +0100

    Add `ReadOnlyCollectionIEnumerableWrapper`

--- src/Platform/Microsoft.Testing.Platform/Services/ExecutableInfo.cs
+++ src/Platform/Microsoft.Testing.Platform/Services/ExecutableInfo.cs
@@ -3,11 +3,11 @@
 
 namespace Microsoft.Testing.Platform.Services;
 
-internal sealed class ExecutableInfo(string fileName, IEnumerable<string> arguments, string workspace)
+internal sealed class ExecutableInfo(string fileName, IReadOnlyCollection<string> arguments, string workspace)
 {
     public string FileName { get; } = fileName;
 
-    public IEnumerable<string> Arguments { get; } = arguments;
+    public IReadOnlyCollection<string> Arguments { get; } = arguments;
 
     public string Workspace { get; } = workspace;
 }
