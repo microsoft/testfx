7f36f5875dcb3a06427eed25c3685465fa417b34
Amaury Levé <amauryleve@microsoft.com>
2024-01-16 11:31:48 +0100

    wip

---
+++ test/IntegrationTests/MSTest.Acceptance.IntegrationTests/AssemblyResolutionTests.cs
@@ -0,0 +1,95 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Testing.Platform.Acceptance.IntegrationTests;
+using Microsoft.Testing.Platform.Acceptance.IntegrationTests.Helpers;
+
+namespace MSTest.Acceptance.IntegrationTests;
+
+[TestGroup]
+public sealed class AssemblyResolutionTests : AcceptanceTestBase
+{
+    private readonly TestAssetFixture _testAssetFixture;
+
+    public AssemblyResolutionTests(ITestExecutionContext testExecutionContext, TestAssetFixture testAssetFixture)
+        : base(testExecutionContext)
+    {
+        _testAssetFixture = testAssetFixture;
+    }
+
+    public async Task A()
+    {
+        TestHost testHost = TestHost.LocateFrom(_testAssetFixture.TargetAssetPath, TestAssetFixture.AssetName);
+        TestHostResult testHostResult = await testHost.ExecuteAsync();
+
+        testHostResult.AssertExitCodeIs(0);
+    }
+
+    [TestFixture(TestFixtureSharingStrategy.PerTestGroup)]
+    public sealed class TestAssetFixture(AcceptanceFixture acceptanceFixture) : TestAssetFixtureBase(acceptanceFixture.NuGetGlobalPackagesFolder)
+    {
+        public const string AssetName = "AssemblyResolution";
+
+        private const string MSTestCode = """
+#file AssemblyResolution.csproj
+<Project Sdk="Microsoft.NET.Sdk">
+    <PropertyGroup>
+        <TargetFrameworks>$TargetFrameworks$</TargetFrameworks>
+        <ImplicitUsings>enable</ImplicitUsings>
+        <Nullable>enable</Nullable>
+        <OutputType>Exe</OutputType>
+        <UseAppHost>true</UseAppHost>
+        <LangVersion>preview</LangVersion>
+        <EnableMSTestRunner>true</EnableMSTestRunner>
+    </PropertyGroup>
+    <ItemGroup>
+        <PackageReference Include="Microsoft.Testing.Platform" Version="$MicrosoftTestingPlatformVersion$" />
+        <PackageReference Include="MSTest" Version="$MSTestVersion$" />
+    </ItemGroup>
+    <ItemGroup>
+        <ProjectReference Include="$(RepoRoot)samples\FxExtensibility\FxExtensibility.csproj" />
+    </ItemGroup>
+</Project>
+
+#file Program.cs
+using AssemblyResolution;
+
+ITestApplicationBuilder builder = await TestApplication.CreateBuilderAsync(args);
+builder.AddMSTest(() => new[] { typeof(Program).Assembly });
+using ITestApplication app = await builder.BuildAsync();
+return await app.RunAsync();
+
+#file UnitTest1.cs
+using MSTest.Extensibility.Samples;
+
+namespace AssemblyResolution;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        AssertIs.Positive(10);
+    }
+}
+
+#file Usings.cs
+global using Microsoft.Testing.Platform.Builder;
+global using Microsoft.Testing.Extensions;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+""";
+
+        public string TargetAssetPath => GetAssetPath(AssetName);
+
+        public override IEnumerable<(string ID, string Name, string Code)> GetAssetsToGenerate()
+        {
+            yield return (AssetName, AssetName,
+                MSTestCode
+                .PatchTargetFrameworks(TargetFrameworks.NetCurrent)
+                .PatchCodeWithReplace("$MicrosoftTestingPlatformVersion$", MicrosoftTestingPlatformVersion)
+                .PatchCodeWithReplace("$MicrosoftTestingPlatformExtensionsVersion$", MicrosoftTestingPlatformExtensionsVersion)
+                .PatchCodeWithReplace("$MSTestVersion$", MSTestVersion));
+        }
+    }
+}
