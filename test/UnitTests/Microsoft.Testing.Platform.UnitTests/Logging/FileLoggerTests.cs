49b76b8dc43cf4d170568d4b51465ce66e862033
Amaury Levé <amauryleve@microsoft.com>
2024-01-18 15:32:02 +0100

    Fix newline for Linux

--- test/UnitTests/Microsoft.Testing.Platform.UnitTests/Logging/FileLoggerTests.cs
+++ test/UnitTests/Microsoft.Testing.Platform.UnitTests/Logging/FileLoggerTests.cs
@@ -180,7 +180,7 @@ public class FileLoggerTests : TestBase, IDisposable
                 await Task.Delay(100);
             }
 
-            Assert.AreEqual($"[00:00:00.000 Test - {currentLogLevel}] Message\r\n", Encoding.Default.GetString(_memoryStream.ToArray()));
+            Assert.AreEqual($"[00:00:00.000 Test - {currentLogLevel}] Message{Environment.NewLine}", Encoding.Default.GetString(_memoryStream.ToArray()));
         }
         else
         {
@@ -214,7 +214,7 @@ public class FileLoggerTests : TestBase, IDisposable
                 await Task.Delay(100);
             }
 
-            Assert.AreEqual($"[00:00:00.000 Test - {currentLogLevel}] Message\r\n", Encoding.Default.GetString(_memoryStream.ToArray()));
+            Assert.AreEqual($"[00:00:00.000 Test - {currentLogLevel}] Message{Environment.NewLine}", Encoding.Default.GetString(_memoryStream.ToArray()));
         }
         else
         {
