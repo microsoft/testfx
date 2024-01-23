20f45f93c79988428a71cb49ec3e35711e6ce587
Marco Rossignoli <mrossignol@microsoft.com>
2024-01-23 18:50:26 +0100

    fix retry

--- test/Utilities/Microsoft.Testing.TestInfrastructure/RetryHelper.cs
+++ test/Utilities/Microsoft.Testing.TestInfrastructure/RetryHelper.cs
@@ -1,17 +1,39 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Polly;
+
 namespace Microsoft.Testing.TestInfrastructure;
 
 public class RetryHelper
 {
     public static async Task RetryAsync(Func<Task> action, uint times, TimeSpan every, Func<Exception, bool>? predicate = null)
     {
-        await action();
+        await Policy.Handle<Exception>(exception =>
+        {
+            if (predicate is null)
+            {
+                return true;
+            }
+
+            return predicate(exception);
+        })
+       .WaitAndRetryAsync((int)times, _ => every)
+       .ExecuteAsync(action);
     }
 
     public static async Task<T> RetryAsync<T>(Func<Task<T>> action, uint times, TimeSpan every, Func<Exception, bool>? predicate = null)
     {
-        return await action();
+        return await Policy.Handle<Exception>(exception =>
+        {
+            if (predicate is null)
+            {
+                return true;
+            }
+
+            return predicate(exception);
+        })
+        .WaitAndRetryAsync((int)times, _ => every)
+        .ExecuteAsync(action);
     }
 }
