e33d8d03e61e4d3d47f02505133c11860b6974a6
Amaury Levé <amauryleve@microsoft.com>
2024-02-02 16:20:07 +0100

    Add a console logger sample for the runner (#2246)

---
+++ samples/mstest-runner/CustomReportExtension/CustomReportExtension/Program.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Testing.Platform.Builder;
+using Microsoft.Testing.Platform.Extensions;
+
+ITestApplicationBuilder builder = await TestApplication.CreateBuilderAsync(args);
+
+// Register MSTest as the test framework to use.
+builder.AddMSTest(() => new[] { typeof(Program).Assembly });
+
+// This is a convenience helper that simplifies the process of creating a composite extension (a type that implements multiple extension points).
+// Thanks to that, you can implement multiple extension points in a single class and you don't have to handle communication/synchronization between them.
+CompositeExtensionFactory<TestResultConsoleLogger> testUpdateConsoleReporter =
+    new(serviceProvider => new TestResultConsoleLogger());
+
+// Register the extension as a data consumer and test session lifetime handler.
+builder.TestHost.AddDataConsumer(testUpdateConsoleReporter);
+builder.TestHost.AddTestSessionLifetimeHandle(testUpdateConsoleReporter);
+
+ITestApplication app = await builder.BuildAsync();
+
+return await app.RunAsync();
