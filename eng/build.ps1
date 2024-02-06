738f808b87dd8b2f268d7d759191f491a2e457ce
Amaury Levé <amauryleve@microsoft.com>
2024-02-06 16:51:33 +0100

    Refactor install windows sdk

--- eng/build.ps1
+++ eng/build.ps1
@@ -58,24 +58,7 @@ if ($vs) {
 }
 
 if ($installWindowsSdk) {
-    $windowsSdkPath = [Environment]::GetEnvironmentVariable("ProgramFiles(x86)") + "\Windows Kits\10\UnionMetadata\10.0.16299.0"
-    if (Test-Path $windowsSdkPath) {
-        Write-Host "Windows SDK 10.0.16299 is already installed, skipping..."
-    } else {
-        Write-Host "Downloading Windows SDK 10.0.16299..." -ForegroundColor Green
-        Invoke-WebRequest -Method Get -Uri https://go.microsoft.com/fwlink/p/?linkid=864422 -OutFile sdksetup.exe -UseBasicParsing
-
-        Write-Host "Installing Windows SDK, if setup requests elevation please approve." -ForegroundColor Green
-        $process = Start-Process -Wait sdksetup.exe -ArgumentList "/quiet", "/norestart", "/ceip off", "/features OptionId.UWPManaged"  -PassThru
-
-        if ($process.ExitCode -eq 0) {
-            Remove-Item sdksetup.exe -Force
-            Write-Host "Installation succeeded"
-        }
-        else {
-            Write-Error "Failed to install Windows SDK (Exit code: $($process.ExitCode))"
-        }
-    }
+    & $PSScriptRoot\install-windows-sdk.ps1
 } else {
     Write-Host "Skipping Windows SDK installation"
 }
