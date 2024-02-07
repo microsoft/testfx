2abb9c1127cec923165f953804a1ac2ebc31cba0
Amaury Levé <amauryleve@microsoft.com>
2024-02-07 17:09:20 +0100

    Fix handling of timeout/cancellation

--- src/Adapter/MSTest.TestAdapter/Helpers/MethodRunner.cs
+++ src/Adapter/MSTest.TestAdapter/Helpers/MethodRunner.cs
@@ -16,35 +16,47 @@ internal static class MethodRunner
         Action action, CancellationTokenSource cancellationTokenSource, int? timeout, MethodInfo methodInfo,
         string methodCancelledMessageFormat, string methodTimedOutMessageFormat)
     {
+        Task? executionTask = null;
         try
         {
-            var executionTask = Task.Run(action, cancellationTokenSource.Token);
+            executionTask = Task.Run(action, cancellationTokenSource.Token);
+        }
+        catch (TaskCanceledException)
+        {
+            return new(
+                UnitTestOutcome.Timeout,
+                string.Format(CultureInfo.InvariantCulture, methodCancelledMessageFormat, methodInfo.DeclaringType!.FullName, methodInfo.Name));
+        }
 
+        try
+        {
             if (!timeout.HasValue)
             {
                 executionTask.Wait(cancellationTokenSource.Token);
-                return cancellationTokenSource.IsCancellationRequested
-                    ? new(
-                        UnitTestOutcome.Timeout,
-                        string.Format(CultureInfo.InvariantCulture, methodCancelledMessageFormat, methodInfo.DeclaringType!.FullName, methodInfo.Name))
-                    : null;
+
+                // If we reach this line then the task has completed successfully.
+                // If the cancellation was requested, then either OCE or AggregateException with TCE will be thrown.
+                return null;
             }
 
             if (executionTask.Wait(timeout.Value, cancellationTokenSource.Token))
             {
+                // If we reach this line then the task has completed successfully.
                 return null;
             }
 
-            // Timed out or cancelled.
+            // Timed out. For cancellation, either OCE or AggregateException with TCE will be thrown.
             return new(
                 UnitTestOutcome.Timeout,
                 string.Format(
                     CultureInfo.InvariantCulture,
-                    cancellationTokenSource.IsCancellationRequested ? methodCancelledMessageFormat : methodTimedOutMessageFormat,
+                    methodTimedOutMessageFormat,
                     methodInfo.DeclaringType!.FullName,
                     methodInfo.Name));
         }
-        catch (OperationCanceledException)
+        catch (Exception ex) when
+            (ex is OperationCanceledException
+            || (ex is AggregateException aggregateEx && aggregateEx.InnerExceptions.OfType<TaskCanceledException>().Any()))
         {
             return new(
                 UnitTestOutcome.Timeout,
