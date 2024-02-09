af0dc339e9d2e34ec9ee9e9c9e3b8649bc616462
Amaury Levé <amauryleve@microsoft.com>
2024-02-09 17:09:44 +0100

    Document logging public APIs

--- src/Platform/Microsoft.Testing.Platform/Logging/ILoggerFactory.cs
+++ src/Platform/Microsoft.Testing.Platform/Logging/ILoggerFactory.cs
@@ -3,7 +3,15 @@
 
 namespace Microsoft.Testing.Platform.Logging;
 
+/// <summary>
+/// Represents a factory for creating loggers.
+/// </summary>
 public interface ILoggerFactory
 {
+    /// <summary>
+    /// Creates a logger with the specified category name.
+    /// </summary>
+    /// <param name="categoryName">The name of the category for the logger.</param>
+    /// <returns>A new instance of the logger.</returns>
     ILogger CreateLogger(string categoryName);
 }
--- src/Platform/Microsoft.Testing.Platform/Logging/LoggerFactoryExtensions.cs
+++ src/Platform/Microsoft.Testing.Platform/Logging/LoggerFactoryExtensions.cs
@@ -5,8 +5,17 @@ using Microsoft.Testing.Platform.Helpers;
 
 namespace Microsoft.Testing.Platform.Logging;
 
+/// <summary>
+/// Provides extension methods for the <see cref="ILoggerFactory"/> interface.
+/// </summary>
 public static class LoggerFactoryExtensions
 {
+    /// <summary>
+    /// Creates a logger instance for the specified category name.
+    /// </summary>
+    /// <typeparam name="TCategoryName">The type of the category name.</typeparam>
+    /// <param name="factory">The logger factory.</param>
+    /// <returns>A logger instance.</returns>
     public static ILogger<TCategoryName> CreateLogger<TCategoryName>(this ILoggerFactory factory)
     {
         ArgumentGuard.IsNotNull(factory);
