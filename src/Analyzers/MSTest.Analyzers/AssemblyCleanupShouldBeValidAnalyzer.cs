9f0fed423836133b4656ca006ccbfde0d002ff31
Enji Eid <enjieid@microsoft.com>
2024-02-19 16:17:05 +0100

    add comment

--- src/Analyzers/MSTest.Analyzers/AssemblyCleanupShouldBeValidAnalyzer.cs
+++ src/Analyzers/MSTest.Analyzers/AssemblyCleanupShouldBeValidAnalyzer.cs
@@ -94,6 +94,8 @@ public sealed class AssemblyCleanupShouldBeValidAnalyzer : DiagnosticAnalyzer
             context.ReportDiagnostic(methodSymbol.CreateDiagnostic(NotAsyncVoidRule, methodSymbol.Name));
         }
 
+        // The first check makes sure that the class always has public on the member,
+        // the other two check the visibility due to placement in a internal or private class (or public class in internal class etc.)
         if (methodSymbol.DeclaredAccessibility != Accessibility.Public
             || (!canDiscoverInternals && methodSymbol.GetResultantVisibility() != SymbolVisibility.Public)
             || (canDiscoverInternals && methodSymbol.GetResultantVisibility() == SymbolVisibility.Private))
