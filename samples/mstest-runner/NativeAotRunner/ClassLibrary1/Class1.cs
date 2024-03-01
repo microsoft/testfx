428be2966cc7e70c753025930e18cc72381bf112
Jakub Jareš <me@jakubjares.com>
2024-03-01 16:00:48 +0100

    Add nativeAOT runner example

---
+++ samples/mstest-runner/NativeAotRunner/ClassLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int Add(int left, int right)
+        {
+            return left + right;
+        }
+    }
+}
---
+++ samples/mstest-runner/NativeAotRunner/TestProject1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Testing.Extensions;
+using Microsoft.Testing.Framework;
+using Microsoft.Testing.Platform.Builder;
+namespace TestProject1
+{
+    internal static class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            var builder = await TestApplication.CreateBuilderAsync(args);
+            // Registers TestFramework, with tree of test nodes that are generated into your project by source generator.
+            builder.AddTestFramework(new SourceGeneratedTestNodesBuilder());
+            builder.AddTrxReportProvider();
+            var app = await builder.BuildAsync();
+            return await app.RunAsync();
+        }
+    }
+}
