98a3441753a50a1fd5716384d448d54366992430
Amaury Levé <amauryleve@microsoft.com>
2024-03-12 14:55:55 +0100

    wip

---
+++ src/Analyzers/MSTest.Analyzers/AssertionArgsShouldBePassedInCorrectOrder.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Immutable;
+
+using Analyzer.Utilities.Extensions;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+using MSTest.Analyzers.Helpers;
+
+namespace MSTest.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+public sealed class AssertionArgsShouldBePassedInCorrectOrder : DiagnosticAnalyzer
+{
+    private static readonly ImmutableArray<string> SupportedMethodNames = ImmutableArray.Create(new[]
+    {
+        "AreEqual",
+        "AreNotEqual",
+        "AreSame",
+        "AreNotSame",
+    });
+
+    private static readonly LocalizableResourceString Title = new(nameof(Resources.AssertionArgsShouldBePassedInCorrectOrderTitle), Resources.ResourceManager, typeof(Resources));
+    private static readonly LocalizableResourceString Description = new(nameof(Resources.AssertionArgsShouldBePassedInCorrectOrderDescription), Resources.ResourceManager, typeof(Resources));
+    private static readonly LocalizableResourceString MessageFormat = new(nameof(Resources.AssertionArgsShouldBePassedInCorrectOrderMessageFormat), Resources.ResourceManager, typeof(Resources));
+
+    internal static readonly DiagnosticDescriptor Rule = DiagnosticDescriptorHelper.Create(
+        DiagnosticIds.AssertionArgsShouldBePassedInCorrectOrderRuleId,
+        Title,
+        MessageFormat,
+        Description,
+        Category.Usage,
+        DiagnosticSeverity.Info,
+        isEnabledByDefault: true);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+        = ImmutableArray.Create(Rule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+
+        context.RegisterCompilationStartAction(context =>
+        {
+            if (context.Compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.MicrosoftVisualStudioTestToolsUnitTestingAssert, out var assertSymbol))
+            {
+                context.RegisterOperationAction(context => AnalyzeOperation(context, assertSymbol), OperationKind.Invocation);
+            }
+        });
+    }
+
+    private static void AnalyzeOperation(OperationAnalysisContext context, INamedTypeSymbol assertSymbol)
+    {
+        IInvocationOperation invocationOperation = (IInvocationOperation)context.Operation;
+
+        // This is not an invocation of the expected assertion methods.
+        if (!SupportedMethodNames.Contains(invocationOperation.TargetMethod.Name)
+            || !SymbolEqualityComparer.Default.Equals(assertSymbol, invocationOperation.TargetMethod.ContainingType)
+            || FindExpectedAndActualArguments(invocationOperation) is not (IArgumentOperation expectedArgument, IArgumentOperation actualArgument))
+        {
+            return;
+        }
+
+        // If the actual value is a constant or a literal, then the arguments are in the wrong order.
+        if (actualArgument.ConstantValue.HasValue
+            || actualArgument.Value.Kind == OperationKind.Literal)
+        {
+            // TODO: Decide if we want to be strict and check that expected is not a constant or a literal.
+            context.ReportDiagnostic(invocationOperation.CreateDiagnostic(Rule));
+            return;
+        }
+
+        if (actualArgument.Value is not ILocalReferenceOperation localRefSymbol)
+        {
+            // TODO: Decide if we want to be strict
+            context.ReportDiagnostic(invocationOperation.CreateDiagnostic(Rule));
+            return;
+        }
+
+        if (localRefSymbol.Local.Name.StartsWith("expected", StringComparison.Ordinal))
+        {
+            context.ReportDiagnostic(invocationOperation.CreateDiagnostic(Rule));
+            return;
+        }
+
+        if (expectedArgument.Value is ISymbol expectedSymbol && expectedSymbol.Name.StartsWith("actual", StringComparison.Ordinal))
+        {
+            context.ReportDiagnostic(invocationOperation.CreateDiagnostic(Rule));
+        }
+    }
+
+    private static (IArgumentOperation ExpectedArgument, IArgumentOperation ActualArgument)? FindExpectedAndActualArguments(IInvocationOperation invocationOperation)
+    {
+        if (invocationOperation.Arguments.Length < 2)
+        {
+            return null;
+        }
+
+        IArgumentOperation? expectedArgument = invocationOperation.Arguments.FirstOrDefault(x => string.Equals(x.Parameter?.Name, "expected", StringComparison.Ordinal));
+        if (expectedArgument is null)
+        {
+            return null;
+        }
+
+        IArgumentOperation? actualArgument = invocationOperation.Arguments.FirstOrDefault(x => string.Equals(x.Parameter?.Name, "actual", StringComparison.Ordinal));
+        return actualArgument is null
+            ? null
+            : (expectedArgument, actualArgument);
+    }
+}
---
+++ test/UnitTests/MSTest.Analyzers.UnitTests/AssertionArgsShouldBePassedInCorrectOrderTests.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Testing.Framework;
+using Microsoft.Testing.TestInfrastructure;
+
+using VerifyCS = MSTest.Analyzers.Test.CSharpCodeFixVerifier<
+    MSTest.Analyzers.AssertionArgsShouldBePassedInCorrectOrder,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace MSTest.Analyzers.UnitTests;
+
+[TestGroup]
+public sealed class AssertionArgsShouldBePassedInCorrectOrderTests(ITestExecutionContext testExecutionContext) : TestBase(testExecutionContext)
+{
+    private async Task WhenUsingLiterals()
+    {
+        var code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class MyTestClass
+            {
+                [TestMethod]
+                public void NonCompliant()
+                {
+                    string s = "";
+                    bool b = true;
+                    int i = 42;
+
+                    [|Assert.AreEqual(s, "")|];
+                    [|Assert.AreEqual(b, true)|];
+                    [|Assert.AreEqual(i, 1)|];
+
+                    [|Assert.AreSame(s, "")|];
+                    [|Assert.AreSame(b, true)|];
+                    [|Assert.AreSame(i, 1)|];
+                }
+
+                [TestMethod]
+                public void Compliant()
+                {
+                    string s = "";
+                    bool b = true;
+                    int i = 42;
+            
+                    Assert.AreEqual("", s);
+                    Assert.AreEqual(true, b);
+                    Assert.AreEqual(1, i);
+            
+                    Assert.AreSame("", s);
+                    Assert.AreSame(true, b);
+                    Assert.AreSame(1, i);
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+
+    public async Task LiteralUsingNamedArgument()
+    {
+        var code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class MyTestClass
+            {
+                [TestMethod]
+                public void NonCompliant()
+                {
+                    string s = "";
+                    bool b = true;
+                    int i = 42;
+
+                    [|Assert.AreEqual(actual: "", expected: s)|];
+                    [|Assert.AreEqual(actual: true, expected: b)|];
+                    [|Assert.AreEqual(actual: 1, expected: i)|];
+
+                    [|Assert.AreSame(actual: "", expected: s)|];
+                    [|Assert.AreSame(actual: true, expected: b)|];
+                    [|Assert.AreSame(actual: 1, expected: i)|];
+                }
+
+                [TestMethod]
+                public void Compliant()
+                {
+                    string s = "";
+                    bool b = true;
+                    int i = 42;
+            
+                    Assert.AreEqual(actual: s, expected: "");
+                    Assert.AreEqual(actual: b, expected: true);
+                    Assert.AreEqual(actual: i, expected: 1);
+            
+                    Assert.AreSame(actual: s, expected: "");
+                    Assert.AreSame(actual: b, expected: true);
+                    Assert.AreSame(actual: i, expected: 1);
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+}
