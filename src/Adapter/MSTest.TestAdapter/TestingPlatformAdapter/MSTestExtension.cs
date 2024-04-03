38b65dee9dfc8d2ca722a43afacd100d5bda32dd
Amaury Levé <amauryleve@microsoft.com>
2024-04-03 18:09:44 +0200

    Use different extension Uid for MSTest.Sdk (#2649)

--- src/Adapter/MSTest.TestAdapter/TestingPlatformAdapter/MSTestExtension.cs
+++ src/Adapter/MSTest.TestAdapter/TestingPlatformAdapter/MSTestExtension.cs
@@ -2,13 +2,15 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #if !WINDOWS_UWP
+using System.Reflection;
+
 using Microsoft.Testing.Platform.Extensions;
 
 namespace Microsoft.VisualStudio.TestTools.UnitTesting;
 
 internal sealed class MSTestExtension : IExtension
 {
-    public string Uid => nameof(MSTestExtension);
+    public string Uid { get; } = GetExtensionUid();
 
     public string DisplayName => "MSTest";
 
@@ -17,5 +19,11 @@ internal sealed class MSTestExtension : IExtension
     public string Description => "MSTest Framework for Microsoft Testing Platform";
 
     public Task<bool> IsEnabledAsync() => Task.FromResult(true);
+
+    private static string GetExtensionUid()
+    {
+        var assemblyMetadataAttributes = Assembly.GetEntryAssembly()?.GetCustomAttributes<AssemblyMetadataAttribute>();
+        return assemblyMetadataAttributes?.FirstOrDefault(x => x.Key == "MSTest.Extension.Uid")?.Value ?? nameof(MSTestExtension);
+    }
 }
 #endif
