dd10f4926a67b51207685cac559c9feb15b179bb
Amaury Levé <amauryleve@microsoft.com>
2024-04-08 12:14:12 +0200

    Add project samples for MSTest.Sdk

---
+++ samples/DemoMSTestSdk/ProjectUsingMSTestRunner/UnitTest1.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace ProjectUsingMSTestRunner;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
