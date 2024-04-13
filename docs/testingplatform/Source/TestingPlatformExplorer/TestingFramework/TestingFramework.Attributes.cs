bee13303d6b631a8563d583298531773dffad32d
Marco Rossignoli <marco.rossignoli@gmail.com>
2024-04-13 19:09:53 +0200

    Complete `ITestFramework` documentation, document `IMessageBus` complete the sample code. (#2707)

--- docs/testingplatform/Source/TestingPlatformExplorer/TestingFramework/TestingFramework.Attributes.cs
+++ docs/testingplatform/Source/TestingPlatformExplorer/TestingFramework/TestingFramework.Attributes.cs
@@ -1,15 +1,14 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace TestingPlatformExplorer.TestingFramework;
 
-[AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
-public sealed class TestMethodAttribute : Attribute
+[AttributeUsage(AttributeTargets.Method)]
+public class SkipAttribute : Attribute
 {
-    public TestMethodAttribute(bool skip = false)
-    {
-        Skip = skip;
-    }
+}
 
-    public bool Skip { get; }
+[AttributeUsage(AttributeTargets.Method)]
+public class TestMethodAttribute : Attribute
+{
 }
