c7c9939f774cf2e3a4467eedf8a80bd171bd77a7
Marco Rossignoli <mrossignol@microsoft.com>
2024-04-13 11:53:24 +0200

    Introduce `RequestNotSupportedException`

---
+++ src/Platform/Microsoft.Testing.Platform/Requests/RequestNotSupportedException.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Testing.Platform.Requests;
+
+[Serializable]
+public class RequestNotSupportedException : Exception
+{
+    public RequestNotSupportedException(IRequest request)
+    {
+        Request = request;
+    }
+
+    public IRequest Request { get; }
+}
