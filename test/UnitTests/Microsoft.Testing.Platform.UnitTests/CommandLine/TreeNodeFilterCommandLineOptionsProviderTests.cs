c2ed6f3fbea706fc9332b2dc0c5d61e657a978b8
fhnaseer <fhnaseer@live.com>
2024-05-13 14:10:27 +0200

    Cleaning command line validations and adding unit tests (#2842)

---
+++ test/UnitTests/Microsoft.Testing.Platform.UnitTests/CommandLine/TreeNodeFilterCommandLineOptionsProviderTests.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Testing.Internal.Framework;
+using Microsoft.Testing.Platform.CommandLine;
+using Microsoft.Testing.Platform.Extensions;
+using Microsoft.Testing.Platform.Extensions.CommandLine;
+using Microsoft.Testing.Platform.UnitTests.Helpers;
+using Microsoft.Testing.TestInfrastructure;
+
+namespace Microsoft.Testing.Platform.UnitTests.CommandLine;
+
+[TestGroup]
+public class TreeNodeFilterCommandLineOptionsProviderTests : TestBase
+{
+    public TreeNodeFilterCommandLineOptionsProviderTests(ITestExecutionContext testExecutionContext)
+    : base(testExecutionContext)
+    {
+    }
+
+    public async Task TreenodeFilter_AlwaysValid()
+    {
+        var provider = new TreeNodeFilterCommandLineOptionsProvider(new TestExtension());
+        CommandLineOption option = provider.GetCommandLineOptions().First(x => x.Name == TreeNodeFilterCommandLineOptionsProvider.TreenodeFilter);
+
+        ValidationResult validateOptionsResult = await provider.ValidateOptionArgumentsAsync(option, []).ConfigureAwait(false);
+        Assert.IsTrue(validateOptionsResult.IsValid);
+    }
+
+    public async Task CommandLineOptions_AlwaysValid()
+    {
+        var provider = new TreeNodeFilterCommandLineOptionsProvider(new TestExtension());
+
+        ValidationResult validateOptionsResult = await provider.ValidateCommandLineOptionsAsync(new TestCommandLineOptions([])).ConfigureAwait(false);
+        Assert.IsTrue(validateOptionsResult.IsValid);
+    }
+}
