8ba64bf35e953fc0055ecfe724f20b9cc0c62f7f
fhnaseer <fhnaseer@live.com>
2024-05-13 12:56:30 +0200

    removing mock, splitting valid and invalid tests,

--- test/UnitTests/Microsoft.Testing.Platform.UnitTests/CommandLine/TreeNodeFilterCommandLineOptionsProviderTests.cs
+++ test/UnitTests/Microsoft.Testing.Platform.UnitTests/CommandLine/TreeNodeFilterCommandLineOptionsProviderTests.cs
@@ -5,9 +5,8 @@ using Microsoft.Testing.Internal.Framework;
 using Microsoft.Testing.Platform.CommandLine;
 using Microsoft.Testing.Platform.Extensions;
 using Microsoft.Testing.Platform.Extensions.CommandLine;
+using Microsoft.Testing.Platform.UnitTests.Helpers;
 using Microsoft.Testing.TestInfrastructure;
-
-using Moq;
 
 namespace Microsoft.Testing.Platform.UnitTests.CommandLine;
 
@@ -19,19 +18,9 @@ public class TreeNodeFilterCommandLineOptionsProviderTests : TestBase
     {
     }
 
-    private static TreeNodeFilterCommandLineOptionsProvider GetProvider()
-    {
-        var extension = new Mock<IExtension>();
-        _ = extension.Setup(x => x.Uid).Returns("Uid");
-        _ = extension.Setup(x => x.Version).Returns("Version");
-        _ = extension.Setup(x => x.DisplayName).Returns("DisplayName");
-        _ = extension.Setup(x => x.Description).Returns("Description");
-        return new TreeNodeFilterCommandLineOptionsProvider(extension.Object);
-    }
-
     public async Task TreenodeFilter_AlwaysValid()
     {
-        TreeNodeFilterCommandLineOptionsProvider provider = GetProvider();
+        var provider = new TreeNodeFilterCommandLineOptionsProvider(new TestExtension());
         CommandLineOption option = provider.GetCommandLineOptions().First(x => x.Name == TreeNodeFilterCommandLineOptionsProvider.TreenodeFilter);
 
         ValidationResult validateOptionsResult = await provider.ValidateOptionArgumentsAsync(option, []).ConfigureAwait(false);
@@ -40,9 +29,9 @@ public class TreeNodeFilterCommandLineOptionsProviderTests : TestBase
 
     public async Task CommandLineOptions_AlwaysValid()
     {
-        TreeNodeFilterCommandLineOptionsProvider provider = GetProvider();
+        var provider = new TreeNodeFilterCommandLineOptionsProvider(new TestExtension());
 
-        ValidationResult validateOptionsResult = await provider.ValidateCommandLineOptionsAsync(new Mock<ICommandLineOptions>().Object).ConfigureAwait(false);
+        ValidationResult validateOptionsResult = await provider.ValidateCommandLineOptionsAsync(new TestCommandLineOptions([])).ConfigureAwait(false);
         Assert.IsTrue(validateOptionsResult.IsValid);
     }
 }
