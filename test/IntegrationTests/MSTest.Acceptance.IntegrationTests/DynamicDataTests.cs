70c8b5662690a9d2749f07df03d79d3f93950e1b
fhnaseer <fhnaseer@live.com>
2024-05-17 16:54:38 +0200

    More tests,

--- test/IntegrationTests/MSTest.Acceptance.IntegrationTests/DynamicDataTests.cs
+++ test/IntegrationTests/MSTest.Acceptance.IntegrationTests/DynamicDataTests.cs
@@ -128,6 +128,8 @@ public class TestClass
     [TestMethod]
     [DynamicData(nameof(AdditionalData))]
     [DynamicData(nameof(AdditionalData2))]
+    [CustomTestDataSource]
+    [CustomEmptyTestDataSource]
     public void Test()
     {
     }
@@ -142,6 +144,23 @@ public class TestClass
         }
     }
 }
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public class CustomTestDataSourceAttribute : Attribute, ITestDataSource
+{
+    public IEnumerable<object[]> GetData(MethodInfo methodInfo) => new[] { new object[] { 1, 2, 3 }, [4, 5, 6] };
+
+    public string GetDisplayName(MethodInfo methodInfo, object[] data) => data != null ? string.Format(CultureInfo.CurrentCulture, "{0} ({1})", methodInfo.Name, string.Join(",", data)) : null;
+}
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public class CustomEmptyTestDataSourceAttribute : Attribute, ITestDataSource
+{
+    public IEnumerable<object[]> GetData(MethodInfo methodInfo) => [];
+
+    public string GetDisplayName(MethodInfo methodInfo, object[] data) => data != null ? string.Format(CultureInfo.CurrentCulture, "{0} ({1})", methodInfo.Name, string.Join(",", data)) : null;
+}
+
 """;
     }
 }
