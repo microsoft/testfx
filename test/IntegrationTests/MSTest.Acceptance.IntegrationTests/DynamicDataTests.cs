89caaca91d696b3fa8b43600eae0927f576fc29e
fhnaseer <fhnaseer@live.com>
2024-05-20 13:51:53 +0200

    more,

--- test/IntegrationTests/MSTest.Acceptance.IntegrationTests/DynamicDataTests.cs
+++ test/IntegrationTests/MSTest.Acceptance.IntegrationTests/DynamicDataTests.cs
@@ -157,6 +157,7 @@ public class TestClass
     <OutputType>Exe</OutputType>
     <EnableMSTestRunner>true</EnableMSTestRunner>
     <TargetFrameworks>$TargetFrameworks$</TargetFrameworks>
+    <LangVersion>latest</LangVersion>
   </PropertyGroup>
 
   <ItemGroup>
@@ -171,8 +172,9 @@ public class TestClass
 
 using System;
 using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Reflection;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [TestClass]
 public class TestClass
@@ -188,7 +190,7 @@ public class TestClass
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
 public class CustomTestDataSourceAttribute : Attribute, ITestDataSource
 {
-    public IEnumerable<object[]> GetData(MethodInfo methodInfo) => new[] { new object[] { 1, 2, 3 }, [4, 5, 6] };
+    public IEnumerable<object[]> GetData(MethodInfo methodInfo) => [[1, 2, 3], [4, 5, 6]];
 
     public string GetDisplayName(MethodInfo methodInfo, object[] data) => data != null ? string.Format(CultureInfo.CurrentCulture, "{0} ({1})", methodInfo.Name, string.Join(",", data)) : null;
 }
