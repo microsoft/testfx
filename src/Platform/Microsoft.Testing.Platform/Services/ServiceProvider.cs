5bb5a68b889c0c8e04816621d91ed0a836ee962e
Simon Cropp <simon.cropp@gmail.com>
2024-05-24 21:26:50 +1000

    Update ServiceProvider.cs

--- src/Platform/Microsoft.Testing.Platform/Services/ServiceProvider.cs
+++ src/Platform/Microsoft.Testing.Platform/Services/ServiceProvider.cs
@@ -1,10 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#if !NETCOREAPP
-using System.Reflection;
-#endif
-
 using System.Globalization;
 
 using Microsoft.Testing.Platform.Extensions.TestFramework;
