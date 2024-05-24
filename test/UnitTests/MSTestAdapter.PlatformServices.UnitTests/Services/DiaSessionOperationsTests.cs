991d22f6b07a9bc32d61a2088c1844ece149c228
Simon Cropp <simon.cropp@gmail.com>
2024-05-24 22:12:13 +1000

    Update DiaSessionOperationsTests.cs

--- test/UnitTests/MSTestAdapter.PlatformServices.UnitTests/Services/DiaSessionOperationsTests.cs
+++ test/UnitTests/MSTestAdapter.PlatformServices.UnitTests/Services/DiaSessionOperationsTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #if !NET462
-using System.Reflection;
 
 using Microsoft.VisualStudio.TestPlatform.MSTestAdapter.PlatformServices;
 
