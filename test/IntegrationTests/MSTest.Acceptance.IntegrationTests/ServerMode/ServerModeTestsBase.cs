7af031d2c43e68a41d1f108836e042d76ac76e18
Simon Cropp <simon.cropp@gmail.com>
2024-05-24 21:56:42 +1000

    Update test/IntegrationTests/MSTest.Acceptance.IntegrationTests/ServerMode/ServerModeTestsBase.cs
    Co-authored-by: Amaury Levé <amaury.leve@gmail.com>

--- test/IntegrationTests/MSTest.Acceptance.IntegrationTests/ServerMode/ServerModeTestsBase.cs
+++ test/IntegrationTests/MSTest.Acceptance.IntegrationTests/ServerMode/ServerModeTestsBase.cs
@@ -35,6 +35,7 @@ public partial /* for codegen regx */ class ServerModeTestsBase : AcceptanceTest
         foreach (DictionaryEntry entry in Environment.GetEnvironmentVariables())
         {
             // Skip all unwanted environment variables.
+            string? key = entry.Key.ToString();
             if (WellKnownEnvironmentVariables.ToSkipEnvironmentVariables.Contains(entry.Key.ToString(), StringComparer.OrdinalIgnoreCase))
             {
                 continue;
