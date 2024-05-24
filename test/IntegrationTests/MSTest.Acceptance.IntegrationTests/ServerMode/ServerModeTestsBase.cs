7ee232eaeadf2c5c03cf46e86c9753ec563f509f
Simon Cropp <simon.cropp@gmail.com>
2024-05-24 21:56:49 +1000

    Update test/IntegrationTests/MSTest.Acceptance.IntegrationTests/ServerMode/ServerModeTestsBase.cs
    Co-authored-by: Amaury Levé <amaury.leve@gmail.com>

--- test/IntegrationTests/MSTest.Acceptance.IntegrationTests/ServerMode/ServerModeTestsBase.cs
+++ test/IntegrationTests/MSTest.Acceptance.IntegrationTests/ServerMode/ServerModeTestsBase.cs
@@ -41,7 +41,7 @@ public partial /* for codegen regx */ class ServerModeTestsBase : AcceptanceTest
                 continue;
             }
 
-            environmentVariables[entry.Key.ToString()!] = entry.Value!.ToString()!;
+            environmentVariables[key] = entry.Value!.ToString()!;
         }
 
         // We expect to not fail for unhandled exception in server mode for IDE needs.
