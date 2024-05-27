d776506646399dddcbe5462cdc7be6616dbf351d
fhnaseer <fhnaseer@live.com>
2024-05-27 10:05:03 +0200

    fix,

--- src/Adapter/MSTest.TestAdapter/Execution/NonRunnableTestResult.cs
+++ src/Adapter/MSTest.TestAdapter/Execution/NonRunnableTestResult.cs
@@ -1,11 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.ObjectModel;
+
 namespace Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.Execution;
 
+[Serializable]
 internal class NonRunnableTestResult
 {
-    public NonRunnableTestResult(bool reportTest, ObjectModel.UnitTestOutcome outcome, string? exceptionMessage)
+    public NonRunnableTestResult(bool reportTest, UnitTestOutcome outcome, string? exceptionMessage)
     {
         ReportTest = reportTest;
         Outcome = outcome;
@@ -20,7 +23,7 @@ internal class NonRunnableTestResult
     /// <summary>
     /// Gets or sets the outcome of the test.
     /// </summary>
-    public ObjectModel.UnitTestOutcome Outcome { get; set; }
+    public UnitTestOutcome Outcome { get; set; }
 
     /// <summary>
     /// Gets or sets the exception message if any.
