4b011ce71009029c247f60e2af3c5a2dfacdfb39
Amaury Levé <amauryleve@microsoft.com>
2024-05-29 23:19:00 +0200

    Make NopFilter public and experimental

--- src/Platform/Microsoft.Testing.Platform/Requests/NopFilter.cs
+++ src/Platform/Microsoft.Testing.Platform/Requests/NopFilter.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Microsoft.Testing.Platform.Requests;
 
-internal sealed class NopFilter : ITestExecutionFilter;
+[Experimental("TAEXP001")]
+[SuppressMessage("ApiDesign", "RS0016:Add public types and members to the declared API", Justification = "Experimental API")]
+public sealed class NopFilter : ITestExecutionFilter;
