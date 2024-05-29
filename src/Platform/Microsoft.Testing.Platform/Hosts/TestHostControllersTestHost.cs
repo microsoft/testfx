5addf01bbfd59e2f47f9d6b5f43bea6bd48a4af8
Simon Cropp <simon.cropp@gmail.com>
2024-05-29 19:03:10 +1000

    last of the collection expressions (#3009)
    Co-authored-by: Amaury Levé <amauryleve@microsoft.com>

--- src/Platform/Microsoft.Testing.Platform/Hosts/TestHostControllersTestHost.cs
+++ src/Platform/Microsoft.Testing.Platform/Hosts/TestHostControllersTestHost.cs
@@ -80,11 +80,12 @@ internal sealed class TestHostControllersTestHost : CommonTestHost, ITestHost, I
         try
         {
             string processIdString = process.GetCurrentProcess().Id.ToString(CultureInfo.InvariantCulture);
-            List<string> partialCommandLine = new(executableInfo.Arguments)
-            {
+            List<string> partialCommandLine =
+            [
+                .. executableInfo.Arguments,
                 $"--{PlatformCommandLineProvider.TestHostControllerPIDOptionKey}",
-                processIdString,
-            };
+                processIdString
+            ];
 
             // Prepare the environment variables used by the test host
             string processCorrelationId = Guid.NewGuid().ToString("N");
@@ -119,11 +120,7 @@ internal sealed class TestHostControllersTestHost : CommonTestHost, ITestHost, I
 #endif
             };
 
-            List<IDataConsumer> dataConsumersBuilder = [];
-            if (_testHostsInformation.DataConsumer.Length > 0)
-            {
-                dataConsumersBuilder.AddRange(_testHostsInformation.DataConsumer);
-            }
+            List<IDataConsumer> dataConsumersBuilder = [.. _testHostsInformation.DataConsumer];
 
             IPlatformOutputDevice? display = ServiceProvider.GetServiceInternal<IPlatformOutputDevice>();
             if (display is IDataConsumer dataConsumerDisplay)
