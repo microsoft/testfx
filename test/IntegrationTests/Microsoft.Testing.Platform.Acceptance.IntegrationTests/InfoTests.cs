5255757bfe60e7b18ae74e3572cb26fd7cdd0c24
Amaury Levé <amauryleve@microsoft.com>
2024-06-18 12:15:18 +0200

    Fix test

--- test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/InfoTests.cs
+++ test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/InfoTests.cs
@@ -147,71 +147,71 @@ Registered tools:
 
         testHostResult.AssertExitCodeIs(ExitCodes.Success);
 
-        string regexMatchPattern = $"""
-\.NET Testing Platform v.+ \[.+\]
+        string wildcardPattern = $"""
+.NET Testing Platform v* [*]
 Microsoft Testing Platform:
-  Version: .+
+  Version: *
   Dynamic Code Supported: True
-  Runtime information: .+({Environment.NewLine}  Runtime location: .+)?
-  Test module: .+{TestAssetFixture.AllExtensionsAssetName}.*
+  Runtime information: *{(tfm == TargetFrameworks.NetFramework[0].Arguments ? $"{Environment.NewLine}  Runtime location: *" : string.Empty)}
+  Test module: *{TestAssetFixture.AllExtensionsAssetName}*
 Built-in command line providers:
   PlatformCommandLineProvider
     Name: Platform command line provider
-    Version: .+
+    Version: *
     Description: Microsoft Testing Platform command line provider
     Options:
       --client-host
         Arity: 1
         Hidden: True
-        Description: Specify the hostname of the client\.
+        Description: Specify the hostname of the client.
       --client-port
         Arity: 1
         Hidden: True
-        Description: Specify the port of the client\.
+        Description: Specify the port of the client.
       --diagnostic
         Arity: 0
         Hidden: False
-        Description: Enable the diagnostic logging\. The default log level is 'Trace'\. The file will be written in the output directory with the name log_\[MMddHHssfff\]\.diag
+        Description: Enable the diagnostic logging. The default log level is 'Trace'. The file will be written in the output directory with the name log_[MMddHHssfff].diag
       --diagnostic-filelogger-synchronouswrite
         Arity: 0
         Hidden: False
-        Description: Force the built-in file logger to write the log synchronously\. Useful for scenario where you don't want to lose any log \(i\.e\. in case of crash\)\. Note that this is slowing down the test execution\.
+        Description: Force the built-in file logger to write the log synchronously. Useful for scenario where you don't want to lose any log (i.e. in case of crash). Note that this is slowing down the test execution.
       --diagnostic-output-directory
         Arity: 1
         Hidden: False
-        Description: Output directory of the diagnostic logging, if not specified the file will be generated inside the default 'TestResults' directory\.
+        Description: Output directory of the diagnostic logging, if not specified the file will be generated inside the default 'TestResults' directory.
       --diagnostic-output-fileprefix
         Arity: 1
         Hidden: False
-        Description: Prefix for the log file name that will replace '\[log\]_\.'
+        Description: Prefix for the log file name that will replace '[log]_.'
       --diagnostic-verbosity
         Arity: 1
         Hidden: False
-        Description: Define the level of the verbosity for the --diagnostic\. The available values are 'Trace', 'Debug', 'Information', 'Warning', 'Error', and 'Critical'
+        Description: Define the level of the verbosity for the --diagnostic. The available values are 'Trace', 'Debug', 'Information', 'Warning', 'Error', and 'Critical'
       --dotnet-test-pipe
         Arity: 1
         Hidden: True
-        Description: dotnet test pipe\.
+        Description: dotnet test pipe.
       --exit-on-process-exit
         Arity: 1
         Hidden: False
-        Description: Exit the test process if dependent process exits\. PID must be provided\.
+        Description: Exit the test process if dependent process exits. PID must be provided.
       --help
         Arity: 0
         Hidden: False
-        Description: Show the command line help\.
+        Description: Show the command line help.
       --ignore-exit-code
         Arity: 1
         Hidden: False
-        Description: Do not report non successful exit value for specific exit codes \(e\.g\. '--ignore-exit-code 8;9' ignore exit code 8 and 9 and will return 0 in these case\)
+        Description: Do not report non successful exit value for specific exit codes (e.g. '--ignore-exit-code 8;9' ignore exit code 8 and 9 and will return 0 in these case)
       --info
         Arity: 0
         Hidden: False
-        Description: Display \.NET test application information\.
+        Description: Display .NET test application information.
       --internal-testhostcontroller-pid
-        Arity: 0\.\.1
+        Arity: 0..1
         Hidden: True
-        Description: Eventual parent test host controller PID\.
+        Description: Eventual parent test host controller PID.
       --internal-testingplatform-skipbuildercheck
         Arity: 0
         Hidden: True
@@ -223,37 +223,37 @@ Built-in command line providers:
       --list-tests
         Arity: 0
         Hidden: False
-        Description: List available tests\.
+        Description: List available tests.
       --minimum-expected-tests
-        Arity: 0\.\.1
+        Arity: 0..1
         Hidden: False
-        Description: Specifies the minimum number of tests that are expected to run\.
+        Description: Specifies the minimum number of tests that are expected to run.
       --no-banner
-        Arity: 0\.\.1
+        Arity: 0..1
         Hidden: True
-        Description: Do not display the startup banner, the copyright message or the telemetry banner\.
+        Description: Do not display the startup banner, the copyright message or the telemetry banner.
       --port
         Arity: 1
         Hidden: True
-        Description: Specify the port of the server\.
+        Description: Specify the port of the server.
       --results-directory
         Arity: 1
         Hidden: False
-        Description: The directory where the test results are going to be placed\. If the specified directory doesn't exist, it's created\. The default is TestResults in the directory that contains the test application\.
+        Description: The directory where the test results are going to be placed. If the specified directory doesn't exist, it's created. The default is TestResults in the directory that contains the test application.
       --server
-        Arity: 0\.\.1
+        Arity: 0..1
         Hidden: True
-        Description: Enable the server mode\.
+        Description: Enable the server mode.
 Registered command line providers:
   CrashDumpCommandLineProvider
     Name: Crash dump
-    Version: .+
-    Description: \[net6\.0\+ only\] Produce crash dump files when the test execution process crashes unexpectedly
+    Version: *
+    Description: [net6.0+ only] Produce crash dump files when the test execution process crashes unexpectedly
     Options:
       --crashdump
         Arity: 0
         Hidden: False
-        Description: \[net6\.0\+ only\] Generate a dump file if the test process crashes
+        Description: [net6.0+ only] Generate a dump file if the test process crashes
       --crashdump-filename
         Arity: 1
         Hidden: False
@@ -261,12 +261,12 @@ Registered command line providers:
       --crashdump-type
         Arity: 1
         Hidden: False
-        Description: Specify the type of the dump\. Valid values are 'Mini', 'Heap', 'Triage' or 'Full'\. Default type is 'Full'\.
-For more information visit https:\/\/learn\.microsoft\.com\/dotnet\/core\/diagnostics\/collect-dumps-crash#types-of-mini-dumps
+        Description: Specify the type of the dump. Valid values are 'Mini', 'Heap', 'Triage' or 'Full'. Default type is 'Full'. 
+For more information visit https://learn.microsoft.com/dotnet/core/diagnostics/collect-dumps-crash#types-of-mini-dumps
   HangDumpCommandLineProvider
     Name: Hang dump
-    Version: .+
-    Description: Produce hang dump files when a test execution exceed a given time\.
+    Version: *
+    Description: Produce hang dump files when a test execution exceed a given time.
     Options:
       --hangdump
         Arity: 0
@@ -279,20 +279,20 @@ For more information visit https:\/\/learn\.microsoft\.com\/dotnet\/core\/diagno
       --hangdump-timeout
         Arity: 1
         Hidden: False
-        Description: Specify the timeout after which the dump will be generated\. The timeout value is specified in one of the following formats: 1\.5h, 1\.5hour, 1\.5hours, 90m, 90min, 90minute, 90minutes 5400s, 5400sec, 5400second, 5400seconds\. Default is 30m\.
+        Description: Specify the timeout after which the dump will be generated. The timeout value is specified in one of the following formats: 1.5h, 1.5hour, 1.5hours, 90m, 90min, 90minute, 90minutes 5400s, 5400sec, 5400second, 5400seconds. Default is 30m.
       --hangdump-type
         Arity: 1
         Hidden: False
-        Description: Specify the type of the dump\. Valid values are 'Mini', 'Heap', 'Triage' \(only available in \.NET 6\+\) or 'Full'\. Default type is 'Full'
+        Description: Specify the type of the dump. Valid values are 'Mini', 'Heap', 'Triage' (only available in .NET 6+) or 'Full'. Default type is 'Full'
   RetryCommandLineOptionsProvider
     Name: Retry failed tests
-    Version: .+
-    Description: Retry failed tests feature allows to restart test execution upon failure\.
+    Version: *
+    Description: Retry failed tests feature allows to restart test execution upon failure.
     Options:
       --internal-retry-pipename
         Arity: 1
         Hidden: True
-        Description: Communication between the test host and the retry infra\.
+        Description: Communication between the test host and the retry infra.
       --retry-failed-tests
         Arity: 1
         Hidden: False
@@ -307,8 +307,8 @@ For more information visit https:\/\/learn\.microsoft\.com\/dotnet\/core\/diagno
         Description: Disable retry mechanism if the number of failed tests is greater than the specified value
   TestingFrameworkExtension
     Name: Microsoft Testing Framework
-    Version: .+
-    Description: Microsoft Testing Framework\. This framework allows you to test your code anywhere in any mode \(all OSes, all platforms, all configurations\.\.\.\)\.
+    Version: *
+    Description: Microsoft Testing Framework. This framework allows you to test your code anywhere in any mode (all OSes, all platforms, all configurations...).
     Options:
       --treenode-filter
         Arity: 1
@@ -316,7 +316,7 @@ For more information visit https:\/\/learn\.microsoft\.com\/dotnet\/core\/diagno
         Description: Use a tree filter to filter down the tests to execute
   TrxCompareTool
     Name: TRX comparer tool
-    Version: .+
+    Version: *
     Description: This tool allows to compare and highights differences between 2 TRX reports
     Options:
       --baseline-trx
@@ -329,7 +329,7 @@ For more information visit https:\/\/learn\.microsoft\.com\/dotnet\/core\/diagno
         Description: The TRX file to compare with the baseline
   TrxReportGeneratorCommandLine
     Name: TRX report generator
-    Version: .+
+    Version: *
     Description: Produce a TRX report for the current test session
     Options:
       --report-trx
@@ -344,13 +344,13 @@ Registered tools:
   TrxCompareTool
     Command: ms-trxcompare
     Name: TRX comparer tool
-    Version: .+
+    Version: *
     Description: This tool allows to compare and highights differences between 2 TRX reports
     Tool command line providers:
-      There are no registered command line providers\.
+      There are no registered command line providers.
 """;
 
-        testHostResult.AssertOutputMatchesRegex(regexMatchPattern);
+        testHostResult.AssertOutputMatches(wildcardPattern);
     }
 
     [ArgumentsProvider(nameof(TargetFrameworks.All), typeof(TargetFrameworks))]
