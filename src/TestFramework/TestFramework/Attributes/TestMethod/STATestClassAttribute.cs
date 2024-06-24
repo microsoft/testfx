b7cec3328db485ec53e6a88c9a2f274995ca831f
Enji Eid <enjieid@microsoft.com>
2024-06-24 16:53:56 +0200

    added the attribute

---
+++ src/TestFramework/TestFramework/Attributes/TestMethod/STATestClassAttribute.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/// <summary>
+/// The test class attribute.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class STATestClassAttribute : TestClassAttribute
+{
+    /// <summary>
+    /// Gets a test method attribute that enables running this test.
+    /// </summary>
+    /// <param name="testMethodAttribute">The test method attribute instance defined on this method.</param>
+    /// <returns>The <see cref="TestMethodAttribute"/> to be used to run this test.</returns>
+    /// <remarks>Extensions can override this method to customize how all methods in a class are run.</remarks>
+    public override TestMethodAttribute? GetTestMethodAttribute(TestMethodAttribute? testMethodAttribute) => testMethodAttribute;
+}
