3b211692091b696e682083d071dcf19b87a99a5a
Amaury Levé <amauryleve@microsoft.com>
2024-06-26 22:44:02 +0200

    fix

--- src/TestFramework/TestFramework/Assertions/Assert.AreSame.cs
+++ src/TestFramework/TestFramework/Assertions/Assert.AreSame.cs
@@ -1,6 +1,7 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 
 namespace Microsoft.VisualStudio.TestTools.UnitTesting;
