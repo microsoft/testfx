c6ada3da3366f5b9fc286c7d62896f78af9a9740
Enji Eid <enjieid@microsoft.com>
2024-07-02 17:03:48 +0200

    add analayzer

--- src/Analyzers/MSTest.Analyzers/Helpers/WellKnownTypeNames.cs
+++ src/Analyzers/MSTest.Analyzers/Helpers/WellKnownTypeNames.cs
@@ -35,6 +35,7 @@ internal static class WellKnownTypeNames
 
     public const string SystemIAsyncDisposable = "System.IAsyncDisposable";
     public const string SystemIDisposable = "System.IDisposable";
+    public const string SystemDescriptionAttribute = "System.ComponentModel.DescriptionAttribute";
     public const string SystemThreadingTasksTask = "System.Threading.Tasks.Task";
     public const string SystemThreadingTasksTask1 = "System.Threading.Tasks.Task`1";
     public const string SystemThreadingTasksValueTask = "System.Threading.Tasks.ValueTask";
