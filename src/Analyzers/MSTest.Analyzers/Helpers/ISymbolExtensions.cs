769723b514122da573a8b3affec935c15ea4f753
Enji Eid <enjieid@microsoft.com>
2024-07-10 11:07:48 +0200

    update helpers

---
+++ src/Analyzers/MSTest.Analyzers/Helpers/ISymbolExtensions.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+#nullable disable warnings
+
+using Microsoft.CodeAnalysis;
+
+namespace MSTest.Analyzers.Helpers;
+
+internal static class ISymbolExtensions
+{
+    public static ITypeSymbol? GetReferencedMemberOrLocalOrParameter(this ISymbol? symbol) => symbol switch
+    {
+        IEventSymbol eventSymbol => eventSymbol.Type,
+
+        IFieldSymbol fieldSymbol => fieldSymbol.Type,
+
+        IMethodSymbol methodSymbol => methodSymbol.ReturnType,
+
+        IPropertySymbol propertySymbol => propertySymbol.Type,
+
+        ILocalSymbol localSymbol => localSymbol.Type,
+
+        _ => null,
+    };
+}
