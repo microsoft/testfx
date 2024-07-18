aba781fdce2d0084aedbe245e2a75c92a75f251c
Amaury Levé <amauryleve@microsoft.com>
2024-07-18 16:16:26 +0200

    Update playground to allow testing server mode (#3285)
    Co-authored-by: Marco Rossignoli <mrossignol@microsoft.com>

---
+++ samples/Playground/ServerMode/TestNodeUpdateCollector.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Concurrent;
+
+using Microsoft.Testing.Platform.ServerMode.IntegrationTests.Messages.V100;
+
+namespace MSTest.Acceptance.IntegrationTests.Messages.V100;
+
+public class TestNodeUpdateCollector
+{
+    private readonly TaskCompletionSource _taskCompletionSource = new();
+    private readonly Func<TestNodeUpdate, bool>? _completeCollector;
+
+    public ConcurrentBag<TestNodeUpdate> TestNodeUpdates { get; } = new();
+
+    public TestNodeUpdateCollector(Func<TestNodeUpdate, bool>? completeCollectorWhen = null)
+    {
+        _completeCollector = completeCollectorWhen;
+    }
+
+    public Task CollectNodeUpdatesAsync(TestNodeUpdate[] messages)
+    {
+        foreach (TestNodeUpdate nodeUpdate in messages)
+        {
+            if (_completeCollector is not null && _completeCollector(nodeUpdate))
+            {
+                if (!_taskCompletionSource.Task.IsCompleted)
+                {
+                    _taskCompletionSource.SetResult();
+                }
+            }
+
+            TestNodeUpdates.Add(nodeUpdate);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task IsComplete => _taskCompletionSource.Task;
+}
---
+++ samples/Playground/ServerMode/v1.0.0/ServerCapabilities.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Newtonsoft.Json;
+
+namespace Microsoft.Testing.Platform.ServerMode.IntegrationTests.Messages.V100;
+
+public sealed record ServerCapabilities(
+    [property: JsonProperty("testing")]
+    ServerTestingCapabilities Testing);
+
+public sealed record ServerTestingCapabilities(
+    [property: JsonProperty("supportsDiscovery")]
+    bool SupportsDiscovery,
+    [property: JsonProperty("experimental_multiRequestSupport")]
+    bool MultiRequestSupport,
+    [property: JsonProperty("vsTestProvider")]
+    bool VSTestProvider);
