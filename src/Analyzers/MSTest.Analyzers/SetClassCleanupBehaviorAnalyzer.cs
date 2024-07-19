82df47b79df4d8eb2782b73cfc71239d1f54e6a8
Enji Eid <enjieid@microsoft.com>
2024-07-19 16:15:56 +0200

    added the analyzer(still not working)

---
+++ src/Analyzers/MSTest.Analyzers/SetClassCleanupBehaviorAnalyzer.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Immutable;
+
+using Analyzer.Utilities.Extensions;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using MSTest.Analyzers.Helpers;
+
+namespace MSTest.Analyzers;
+
+/// <summary>
+/// MSTEST0011: <inheritdoc cref="Resources.ClassCleanupShouldBeValidTitle"/>.
+/// </summary>
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+public sealed class SetClassCleanupBehaviorAnalyzer : DiagnosticAnalyzer
+{
+
+
+    private static readonly LocalizableResourceString Title = new(nameof(Resources.SetClassCleanupBehaviorTitle), Resources.ResourceManager, typeof(Resources));
+    private static readonly LocalizableResourceString Description = new(nameof(Resources.SetClassCleanupBehaviorDescription), Resources.ResourceManager, typeof(Resources));
+    private static readonly LocalizableResourceString MessageFormat = new(nameof(Resources.SetClassCleanupBehaviorMessageFormat), Resources.ResourceManager, typeof(Resources));
+
+    internal static readonly DiagnosticDescriptor SetClassCleanupBehaviorRule = DiagnosticDescriptorHelper.Create(
+        id: DiagnosticIds.SetClassCleanupBehaviorRuleId,
+        Title,
+        MessageFormat,
+        Description,
+        Category.Usage,
+        DiagnosticSeverity.Info,
+        isEnabledByDefault: true);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+        = ImmutableArray.Create(SetClassCleanupBehaviorRule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+
+        context.RegisterCompilationStartAction(context =>
+        {
+            if (context.Compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.MicrosoftVisualStudioTestToolsUnitTestingClassCleanupAttribute, out INamedTypeSymbol? classCleanupAttributeSymbol)
+                 && context.Compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.MicrosoftVisualStudioTestToolsUnitTestingTestClassAttribute, out INamedTypeSymbol? testClassAttributeSymbol))
+            {
+                context.RegisterSymbolAction(
+                    context => AnalyzeSymbol(context, classCleanupAttributeSymbol, testClassAttributeSymbol),
+                    SymbolKind.Method);
+            }
+        });
+    }
+
+    private static void AnalyzeSymbol(SymbolAnalysisContext context, INamedTypeSymbol classCleanupAttributeSymbol, INamedTypeSymbol testClassAttributeSymbol)
+    {
+        var methodSymbol = (IMethodSymbol)context.Symbol;
+
+        if (!methodSymbol.ContainingType.GetAttributes().Any(x => x.AttributeClass.Inherits(testClassAttributeSymbol))
+            || !methodSymbol.IsClassCleanupMethod(classCleanupAttributeSymbol))
+        {
+            return;
+        }
+
+        ImmutableArray<AttributeData> methodAttributes = methodSymbol.GetAttributes();
+        bool hasCleanupAttr = false;
+        foreach (AttributeData methodAttribute in methodAttributes)
+        {
+            if (SymbolEqualityComparer.Default.Equals(methodAttribute.AttributeClass, classCleanupAttributeSymbol))
+            {
+                hasCleanupAttr = true;
+            }
+        }
+
+        if (hasCleanupAttr)
+        {
+            context.ReportDiagnostic(methodSymbol.CreateDiagnostic(SetClassCleanupBehaviorRule, methodSymbol.Name));
+        }
+    }
+}
