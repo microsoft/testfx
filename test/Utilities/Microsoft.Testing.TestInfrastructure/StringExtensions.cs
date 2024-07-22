2ce0c860c37912589cfd42231a87c9f2aa88a4cf
Simon Cropp <simon.cropp@gmail.com>
2024-07-22 20:55:46 +1000

    Update StringExtensions.cs

--- test/Utilities/Microsoft.Testing.TestInfrastructure/StringExtensions.cs
+++ test/Utilities/Microsoft.Testing.TestInfrastructure/StringExtensions.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Microsoft.Testing.Platform;
+
 namespace Microsoft.Testing.TestInfrastructure;
 
 public static class StringExtensions
@@ -8,7 +10,7 @@ public static class StringExtensions
     // Double checking that is is not null on purpose.
     public static string OrDefault(this string? value, string defaultValue) => string.IsNullOrEmpty(defaultValue)
                 ? throw new ArgumentNullException(nameof(defaultValue))
-                : !string.IsNullOrWhiteSpace(value)
+                : !RoslynString.IsNullOrWhiteSpace(value)
                     ? value
                     : defaultValue;
 }
