03dc22b4146e365ea4b6e595629aa429e91f966b
Simon Cropp <simon.cropp@gmail.com>
2024-08-03 20:28:17 +1000

    missing process using in GetVsFromPid

--- samples/Playground/DebuggerUtility.cs
+++ samples/Playground/DebuggerUtility.cs
@@ -215,6 +215,7 @@ public class DebuggerUtility
         Process? parent = process;
         while (!IsVsOrNull(parent))
         {
+            using Process toDispose = parent;
             parent = GetParentProcess(parent);
         }
 
