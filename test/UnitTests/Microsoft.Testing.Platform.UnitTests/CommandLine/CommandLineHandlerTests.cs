19dc501872ebe31abdda314ea0ef18b468994f31
Simon Cropp <simon.cropp@gmail.com>
2024-08-04 22:28:38 +1000

    remove CommandLineHandler _environment (#3429)

--- test/UnitTests/Microsoft.Testing.Platform.UnitTests/CommandLine/CommandLineHandlerTests.cs
+++ test/UnitTests/Microsoft.Testing.Platform.UnitTests/CommandLine/CommandLineHandlerTests.cs
@@ -194,7 +194,7 @@ public class CommandLineHandlerTests : TestBase
         string[] args = ["--help"];
         CommandLineParseResult parseResult = CommandLineParser.Parse(args, new SystemEnvironment());
         CommandLineHandler commandLineHandler = new(parseResult, _extensionCommandLineOptionsProviders, _systemCommandLineOptionsProviders,
-            _testApplicationModuleInfoMock.Object, _runtimeFeatureMock.Object, _outputDisplayMock.Object, _environmentMock.Object);
+            _testApplicationModuleInfoMock.Object, _runtimeFeatureMock.Object, _outputDisplayMock.Object);
 
         // Act
         bool result = commandLineHandler.IsHelpInvoked();
@@ -211,7 +211,7 @@ public class CommandLineHandlerTests : TestBase
         string[] args = ["--info"];
         CommandLineParseResult parseResult = CommandLineParser.Parse(args, new SystemEnvironment());
         CommandLineHandler commandLineHandler = new(parseResult, _extensionCommandLineOptionsProviders, _systemCommandLineOptionsProviders,
-            _testApplicationModuleInfoMock.Object, _runtimeFeatureMock.Object, _outputDisplayMock.Object, _environmentMock.Object);
+            _testApplicationModuleInfoMock.Object, _runtimeFeatureMock.Object, _outputDisplayMock.Object);
 
         // Act
         bool result = commandLineHandler.IsInfoInvoked();
@@ -228,7 +228,7 @@ public class CommandLineHandlerTests : TestBase
         string[] args = ["--version"];
         CommandLineParseResult parseResult = CommandLineParser.Parse(args, new SystemEnvironment());
         CommandLineHandler commandLineHandler = new(parseResult, _extensionCommandLineOptionsProviders, _systemCommandLineOptionsProviders,
-            _testApplicationModuleInfoMock.Object, _runtimeFeatureMock.Object, _outputDisplayMock.Object, _environmentMock.Object);
+            _testApplicationModuleInfoMock.Object, _runtimeFeatureMock.Object, _outputDisplayMock.Object);
 
         // Act
         bool result = commandLineHandler.IsOptionSet("version");
@@ -245,8 +245,7 @@ public class CommandLineHandlerTests : TestBase
         OptionRecord optionRecord = new("name", ["value1", "value2"]);
         CommandLineHandler commandLineHandler = new(
             new CommandLineParseResult(string.Empty, [optionRecord], [], []), _extensionCommandLineOptionsProviders,
-            _systemCommandLineOptionsProviders, _testApplicationModuleInfoMock.Object, _runtimeFeatureMock.Object, _outputDisplayMock.Object,
-            _environmentMock.Object);
+            _systemCommandLineOptionsProviders, _testApplicationModuleInfoMock.Object, _runtimeFeatureMock.Object, _outputDisplayMock.Object);
 
         // Act
         bool result = commandLineHandler.TryGetOptionArgumentList("name", out string[]? optionValue);
@@ -273,7 +272,7 @@ public class CommandLineHandlerTests : TestBase
             });
 
         CommandLineHandler commandLineHandler = new(parseResult, _extensionCommandLineOptionsProviders, _systemCommandLineOptionsProviders,
-            _testApplicationModuleInfoMock.Object, _runtimeFeatureMock.Object, _outputDisplayMock.Object, _environmentMock.Object);
+            _testApplicationModuleInfoMock.Object, _runtimeFeatureMock.Object, _outputDisplayMock.Object);
 
         // Act
         bool result = commandLineHandler.TryGetOptionArgumentList("name", out string[]? optionValue);
