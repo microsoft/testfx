c0c9f375fdad41e8d09336a3ff0a324b6d4b7752
Simon Cropp <simon.cropp@gmail.com>
2024-08-05 18:16:45 +1000

    remove redundant processExited variable (#3454)

--- src/Platform/Microsoft.Testing.Platform/Hosts/TestHostControllersTestHost.cs
+++ src/Platform/Microsoft.Testing.Platform/Hosts/TestHostControllersTestHost.cs
@@ -224,11 +224,8 @@ internal sealed class TestHostControllersTestHost : CommonTestHost, ITestHost, I
                 // System.InvalidOperationException: No process is associated with this object.
             }
 
-            testHostProcess.Exited += (sender, e) =>
-            {
-                var processExited = sender as Process;
+            testHostProcess.Exited += (_, _) =>
                 _logger.LogDebug($"Test host process exited, PID: '{testHostProcessId}'");
-            };
 
             await _logger.LogDebugAsync($"Started test host process '{testHostProcessId}' HasExited: {testHostProcess.HasExited}");
 
