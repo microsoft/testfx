91e496776d56f26f77be81e79c2065924dbcba72
Simon Cropp <simon.cropp@gmail.com>
2024-08-09 15:13:36 +1000

    .

---
+++ src/TestFramework/TestFramework/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#pragma warning disable IDE0005
+global using Polyfills;
