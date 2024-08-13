c743aa7da30b05d6bacfb9b6d171f7a757c154e0
Jakub Jareš <me@jakubjares.com>
2024-08-13 13:56:25 +0200

    Update src/Platform/Microsoft.Testing.Platform/OutputDevice/Console/HumanReadableDurationFormatter.cs

--- src/Platform/Microsoft.Testing.Platform/OutputDevice/Console/HumanReadableDurationFormatter.cs
+++ src/Platform/Microsoft.Testing.Platform/OutputDevice/Console/HumanReadableDurationFormatter.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#if !NET7_0_OR_GREATER
-#endif
-
 using System.Globalization;
 using System.Text;
 
