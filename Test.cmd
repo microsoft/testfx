2c264e5f6f1c4d06806ee4bde4768d06228bad72
Amaury Levé <amauryleve@microsoft.com>
2024-08-27 14:45:02 +0200

    Disable integration tests

--- Test.cmd
+++ Test.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\build.ps1""" -test -integrationTest %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\build.ps1""" -test %*"
 exit /b %ErrorLevel%
