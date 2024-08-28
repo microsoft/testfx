1a3cda451e6bc94d7fe972792493c11fb664c3ca
Amaury Levé <amauryleve@microsoft.com>
2024-08-28 10:44:17 +0200

    Bring back integration tests

--- Test.cmd
+++ Test.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\build.ps1""" -test %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\build.ps1""" -test -integrationTest %*"
 exit /b %ErrorLevel%
