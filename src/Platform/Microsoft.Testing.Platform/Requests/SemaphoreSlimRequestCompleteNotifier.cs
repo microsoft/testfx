26bf645b0a79477f95ed2107956e246eee06cc26
Marco Rossignoli <marco.rossignoli@gmail.com>
2024-08-30 17:09:47 +0200

    Expose `ExecuteRequestContext` ctor for testability (#3717)
    Co-authored-by: Marco Rossignoli <mrossignol@microsoft.com>
    Co-authored-by: Amaury Levé <amauryleve@microsoft.com>

---
+++ src/Platform/Microsoft.Testing.Platform/Requests/SemaphoreSlimRequestCompleteNotifier.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Testing.Platform.Requests;
+
+internal sealed class SemaphoreSlimRequestCompleteNotifier : IExecuteRequestCompletionNotifier
+{
+    private readonly SemaphoreSlim _semaphore;
+
+    public SemaphoreSlimRequestCompleteNotifier(SemaphoreSlim semaphore)
+    {
+        _semaphore = semaphore;
+    }
+
+    public void Complete()
+        => _semaphore.Release();
+}
