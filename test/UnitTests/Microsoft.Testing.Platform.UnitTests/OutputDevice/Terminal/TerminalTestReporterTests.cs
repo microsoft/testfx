025ce484fe1202755b763bec5b97bc841309a88d
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-09-08 14:23:45 +0100

    Fix test assertion

--- test/UnitTests/Microsoft.Testing.Platform.UnitTests/OutputDevice/Terminal/TerminalTestReporterTests.cs
+++ test/UnitTests/Microsoft.Testing.Platform.UnitTests/OutputDevice/Terminal/TerminalTestReporterTests.cs
@@ -145,30 +145,30 @@ public sealed class TerminalTestReporterTests : TestBase
 
         terminalReporter.AssemblyRunStarted(assembly, targetFramework, architecture);
         terminalReporter.TestCompleted(assembly, targetFramework, architecture, "FailedTest1", TestOutcome.Fail, TimeSpan.FromSeconds(10),
-            errorMessages: ["Nested error 2", "  ---> Nested error 1", "  ---> Tests failed"], errorStackTraces: [@$"   at NestedMethod2() in {folder}codefile.cs:line 100{Environment.NewLine}   --- End of inner exception stack trace ---", @$"   at NestedMethod() in {folder}codefile.cs:line 42{Environment.NewLine}   --- End of inner exception stack trace ---", @$"   at FailingTest() in {folder}codefile.cs:line 10"], expected: "ABC", actual: "DEF");
+            errorMessages: ["System.Exception: Nested error 2", "  ---> System.Exception: Nested error 1", "  ---> System.Exception: Tests failed"], errorStackTraces: [@$"   at NestedMethod2() in {folder}codefile.cs:line 100{Environment.NewLine}   --- End of inner exception stack trace ---", @$"   at NestedMethod() in {folder}codefile.cs:line 42{Environment.NewLine}   --- End of inner exception stack trace ---", @$"   at FailingTest() in {folder}codefile.cs:line 10"], expected: "ABC", actual: "DEF");
         terminalReporter.AssemblyRunCompleted(assembly, targetFramework, architecture);
         terminalReporter.TestExecutionCompleted(endTime);
 
         string output = stringBuilderConsole.Output;
 
-        string expected = $$"""
+        string expected = $$$"""
             ␛[31;1mfailed␛[m FailedTest1␛[90;1m ␛[90;1m(10s 000ms)␛[m
-            ␛[91;1m  Nested error 2
-            ␛[91;1m  ---> Nested error 1
-            ␛[91;1m  ---> Tests failed
+            ␛[91;1m  System.Exception: Nested error 2
+            ␛[91;1m  ---> System.Exception: Nested error 1
+            ␛[91;1m  ---> System.Exception: Tests failed
             ␛[m␛[91;1m  Expected
                 ABC
               Actual
                 DEF
             ␛[m␛[31;1m  Stack Trace:
-                ␛[90;1mat ␛[m␛[91;NestedMethod()␛[90;1m in ␛[90;1m␛]8;;file:///{{folderLink}}codefile.cs␛\{{folder}}codefile.cs:42␛]8;;␛\␛[m
-                --- End of inner exception stack trace ---
                 ␛[90;1mat ␛[m␛[91;NethodMethod2()␛[90;1m in ␛[90;1m␛]8;;file:///{{folderLink}}codefile.cs␛\{{folder}}codefile.cs:100␛]8;;␛\␛[m
                 --- End of inner exception stack trace ---
-                ␛[90;1mat ␛[m␛[91;1mFailingTest()␛[90;1m in ␛[90;1m␛]8;;file:///{{folderLink}}codefile.cs␛\{{folder}}codefile.cs:10␛]8;;␛\␛[m
+                ␛[90;1mat ␛[m␛[91;NestedMethod()␛[90;1m in ␛[90;1m␛]8;;file:///{{{folderLink}}}codefile.cs␛\{{{folder}}}codefile.cs:42␛]8;;␛\␛[m
+                --- End of inner exception stack trace ---
+                ␛[90;1mat ␛[m␛[91;1mFailingTest()␛[90;1m in ␛[90;1m␛]8;;file:///{{{folderLink}}}codefile.cs␛\{{{folder}}}codefile.cs:10␛]8;;␛\␛[m
 
 
-            ␛[91;1mTest run summary: Failed!␛[90;1m - ␛[m␛[90;1m␛]8;;file:///{{folderLinkNoSlash}}␛\{{folder}}assembly.dll␛]8;;␛\␛[m (net8.0|x64)
+            ␛[91;1mTest run summary: Failed!␛[90;1m - ␛[m␛[90;1m␛]8;;file:///{{{folderLinkNoSlash}}}␛\{{{folder}}}assembly.dll␛]8;;␛\␛[m (net8.0|x64)
             ␛[m  total: 1
             ␛[91;1m  failed: 1
             ␛[m  succeeded: 0
