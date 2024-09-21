d3c40a539167f95677acf250919c44bb266bf712
Simon Cropp <simon.cropp@gmail.com>
2024-09-22 01:15:53 +1000

    remove Validate and use Guard (#3838)
    Co-authored-by: Amaury Levé <amauryleve@microsoft.com>

--- src/Adapter/MSTestAdapter.PlatformServices/Utilities/DeploymentUtilityBase.cs
+++ src/Adapter/MSTestAdapter.PlatformServices/Utilities/DeploymentUtilityBase.cs
@@ -128,12 +128,11 @@ internal abstract class DeploymentUtilityBase
     /// <returns>Returns a list of deployment warnings.</returns>
     protected IEnumerable<string> Deploy(IList<DeploymentItem> deploymentItems, string testSource, string deploymentDirectory, string resultsDirectory)
     {
+        Guard.NotNullOrWhiteSpace(deploymentDirectory);
+        Guard.NotNullOrWhiteSpace(testSource);
+        ApplicationStateGuard.Ensure(FileUtility.DoesDirectoryExist(deploymentDirectory), $"Deployment directory {deploymentDirectory} does not exist");
+        ApplicationStateGuard.Ensure(FileUtility.DoesFileExist(testSource), $"TestSource {testSource} does not exist.");
         ApplicationStateGuard.Ensure(!SourceGeneratorToggle.UseSourceGenerator, "Deployment is not supported in source generator mode.");
-
-        Validate.IsFalse(StringEx.IsNullOrWhiteSpace(deploymentDirectory), "Deployment directory is null or empty");
-        Validate.IsTrue(FileUtility.DoesDirectoryExist(deploymentDirectory), $"Deployment directory {deploymentDirectory} does not exist");
-        Validate.IsFalse(StringEx.IsNullOrWhiteSpace(testSource), "TestSource directory is null/empty");
-        Validate.IsTrue(FileUtility.DoesFileExist(testSource), $"TestSource {testSource} does not exist.");
 
         testSource = Path.GetFullPath(testSource);
         var warnings = new List<string>();
