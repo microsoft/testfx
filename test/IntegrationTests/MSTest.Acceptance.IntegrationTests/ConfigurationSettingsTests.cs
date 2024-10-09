31a8237b06d24d6b226acef9ce351e126a2a7834
Enji Eid <enjieid@microsoft.com>
2024-10-09 15:56:38 +0200

    added integration tests (not baild sussessfully yet)

---
+++ test/IntegrationTests/MSTest.Acceptance.IntegrationTests/ConfigurationSettingsTests.cs
@@ -0,0 +1,198 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Testing.Platform.Acceptance.IntegrationTests;
+using Microsoft.Testing.Platform.Acceptance.IntegrationTests.Helpers;
+
+namespace MSTest.Acceptance.IntegrationTests;
+
+[TestGroup]
+public sealed class ConfigurationSettingsTests : AcceptanceTestBase
+{
+    private readonly TestAssetFixture _testAssetFixture;
+
+    public ConfigurationSettingsTests(ITestExecutionContext testExecutionContext, TestAssetFixture testAssetFixture)
+        : base(testExecutionContext) => _testAssetFixture = testAssetFixture;
+
+    [ArgumentsProvider(nameof(TargetFrameworks.All), typeof(TargetFrameworks))]
+    public async Task TestConfigJson_AndRunSettingsHasMstest_Throws(string tfm)
+    {
+        var testHost = TestHost.LocateFrom(_testAssetFixture.ProjectPathWithMSTestRunSettings, TestAssetFixture.ProjectNameWithMSTestRunSettings, tfm);
+        TestHostResult testHostResult = await testHost.ExecuteAsync("--settings my.runsettings");
+
+        // Assert
+        testHostResult.AssertOutputContains("You are using both 'runsettings' and 'testconfig' files. Please use only one type of configuration file.");
+    }
+
+    [ArgumentsProvider(nameof(TargetFrameworks.All), typeof(TargetFrameworks))]
+    public async Task TestConfigJson_AndRunSettingsHasMstestv2_Throws(string tfm)
+    {
+        var testHost = TestHost.LocateFrom(_testAssetFixture.ProjectPathWithMSTestV2RunSettings, TestAssetFixture.ProjectNameWithMSTestV2RunSettings, tfm);
+        TestHostResult testHostResult = await testHost.ExecuteAsync("--settings my.runsettings");
+
+        // Assert
+        testHostResult.AssertOutputContains("You are using both 'runsettings' and 'testconfig' files. Please use only one type of configuration file.");
+    }
+
+    [ArgumentsProvider(nameof(TargetFrameworks.All), typeof(TargetFrameworks))]
+    public async Task TestConfigJson_AndRunSettingsWithouMstestv2_OverrideRunConfigration(string tfm)
+    {
+        var testHost = TestHost.LocateFrom(_testAssetFixture.ProjectPath, TestAssetFixture.ProjectName, tfm);
+        TestHostResult testHostResult = await testHost.ExecuteAsync("--settings my.runsettings");
+
+        // Assert
+        testHostResult.AssertOutputDoesNotContain("You are using both 'runsettings' and 'testconfig' files. Please use only one type of configuration file.");
+    }
+
+    [TestFixture(TestFixtureSharingStrategy.PerTestGroup)]
+    public sealed class TestAssetFixture(AcceptanceFixture acceptanceFixture) : TestAssetFixtureBase(acceptanceFixture.NuGetGlobalPackagesFolder)
+    {
+        public const string ProjectName = "ConfigrationSettings";
+
+        public string ProjectPath => GetAssetPath(ProjectName);
+
+        public const string ProjectNameWithMSTestRunSettings = "ConfigrationSettings";
+
+        public string ProjectPathWithMSTestRunSettings => GetAssetPath(ProjectNameWithMSTestRunSettings);
+
+        public const string ProjectNameWithMSTestV2RunSettings = "ConfigrationSettings";
+
+        public string ProjectPathWithMSTestV2RunSettings => GetAssetPath(ProjectNameWithMSTestV2RunSettings);
+
+        public override IEnumerable<(string ID, string Name, string Code)> GetAssetsToGenerate()
+        {
+            yield return (ProjectName, ProjectName,
+                SourceCode
+                .PatchTargetFrameworks(TargetFrameworks.All)
+                .PatchCodeWithReplace("$MSTestVersion$", MSTestVersion)
+                .PatchCodeWithReplace("$AppendSettings$", string.Empty));
+
+            yield return (ProjectNameWithMSTestRunSettings, ProjectNameWithMSTestRunSettings,
+                SourceCode
+                .PatchTargetFrameworks(TargetFrameworks.All)
+                .PatchCodeWithReplace("$MSTestVersion$", MSTestVersion)
+                .PatchCodeWithReplace("$AppendSettings$", MSTestSettings));
+
+            yield return (ProjectNameWithMSTestV2RunSettings, ProjectNameWithMSTestV2RunSettings,
+                SourceCode
+                .PatchTargetFrameworks(TargetFrameworks.All)
+                .PatchCodeWithReplace("$MSTestVersion$", MSTestVersion)
+                .PatchCodeWithReplace("$AppendSettings$", MSTestV2Settings));
+        }
+
+        private const string MSTestSettings = """
+<mstest>
+<mstest>
+""";
+
+        private const string MSTestV2Settings = """
+<mstest>
+<mstest>
+""";
+
+        private const string SourceCode = """
+#file ConfigrationSettings.csproj
+<Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <OutputType>Exe</OutputType>
+    <EnableMSTestRunner>true</EnableMSTestRunner>
+    <TargetFrameworks>$TargetFrameworks$</TargetFrameworks>
+    <LangVersion>Preview</LangVersion>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <PackageReference Include="MSTest.TestAdapter" Version="$MSTestVersion$" />
+    <PackageReference Include="MSTest.TestFramework" Version="$MSTestVersion$" />
+  </ItemGroup>
+
+  <ItemGroup>
+    <None Update="*.runsettings">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </None>
+    <None Update="*.testconfig.json">
+      <CopyToOutputDirectory>Always</CopyToOutputDirectory>
+    </None>
+  </ItemGroup>
+
+</Project>
+
+#file my.runsettings
+<?xml version="1.0" encoding="utf-8"?>
+<RunSettings>
+    <RunConfiguration>
+        <CollectSourceInformation>true</CollectSourceInformation>
+        <EXECUTIONTHREADAPARTMENTSTATE>STA</EXECUTIONTHREADAPARTMENTSTATE>
+        <DisableAppDomain>true</DisableAppDomain>
+    </RunConfiguration>
+
+    $AppendSettings$
+</RunSettings>
+
+#file ConfigrationSettings.testconfig.json
+{
+  "mstest": {
+    "timeout": {
+      "assemblyInitialize": 300,
+      "assemblyCleanup": 300,
+      "classInitialize": 200,
+      "classCleanup": 200,
+      "testInitialize": 100,
+      "testCleanup": 100,
+      "test": 60,
+      "useCooperativeCancellation": true
+    },
+    "parallelism": {
+      "enabled": true,
+      "workers": 4,
+      "scope": "class"
+    },
+    "execution": {
+      "collectSourceInformation": true,
+      "executionApartmentState": "MTA",
+      "disableAppDomain": false,
+      "mapInconclusiveToFailed": true,
+      "mapNotRunnableToFailed": true,
+      "treatDiscoveryWarningsAsErrors": true,
+      "considerEmptyDataSourceAsInconclusive": true,
+      "treatClassAndAssemblyCleanupWarningsAsErrors": true,
+      "considerFixturesAsSpecialTests": true
+    },
+    "deployment": {
+      "enabled": true,
+      "deployTestSourceDependencies": false,
+      "deleteDeploymentDirectoryAfterTestRunIsComplete": true
+    },
+    "assemblyResolution": [
+      {
+        "path": "C:\\project\\dependencies",
+        "includeSubDirectories": "true"
+      },
+      {
+        "path": "C:\\project\\libs",
+        "includeSubDirectories": "true"
+      },
+      {
+        "path": "C:\\project\\plugins",
+        "includeSubDirectories": "true"
+      }
+    ]
+  }
+}
+
+
+
+#file UnitTest1.cs
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod()
+    {
+    }
+}
+""";
+    }
+}
