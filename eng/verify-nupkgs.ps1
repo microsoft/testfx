2dac40dfb2f7e2af299a55a79822879c16bac614
Jakub Jareš <me@jakubjares.com>
2024-11-06 09:16:47 +0100

    Only ship TestAdapter related resources

--- eng/verify-nupkgs.ps1
+++ eng/verify-nupkgs.ps1
@@ -22,7 +22,7 @@ function Confirm-NugetPackages {
         "MSTest.Sdk"                            = 15;
         "MSTest.Internal.TestFx.Documentation"  = 10;
         "MSTest.TestFramework"                  = 130;
-        "MSTest.TestAdapter"                    = 102;
+        "MSTest.TestAdapter"                    = 76;
         "MSTest"                                = 6;
         "MSTest.Analyzers"                      = 10;
     }
