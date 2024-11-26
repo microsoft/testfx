5b1a36cd06424bad0aafb02ad5d2586bf7bc3dd6
Youssef1313 <youssefvictor00@gmail.com>
2024-11-26 14:40:06 +0100

    Implement codefix for MSTEST0037: Use proper 'Assert' methods

---
+++ src/Analyzers/MSTest.Analyzers.CodeFixes/UseProperAssertMethodsFixer.cs
@@ -0,0 +1,182 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Immutable;
+using System.Composition;
+using System.Diagnostics;
+using System.Globalization;
+
+using Analyzer.Utilities;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Formatting;
+
+using MSTest.Analyzers.Helpers;
+
+namespace MSTest.Analyzers;
+
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(UseProperAssertMethodsFixer))]
+[Shared]
+public sealed class UseProperAssertMethodsFixer : CodeFixProvider
+{
+    public sealed override ImmutableArray<string> FixableDiagnosticIds { get; }
+        = ImmutableArray.Create(DiagnosticIds.UseProperAssertMethodsRuleId);
+
+    public override FixAllProvider GetFixAllProvider()
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+        => WellKnownFixAllProviders.BatchFixer;
+
+    public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        SyntaxNode root = await context.Document.GetRequiredSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+        Diagnostic diagnostic = context.Diagnostics[0];
+        string? mode = diagnostic.Properties[UseProperAssertMethodsAnalyzer.CodeFixModeKey];
+        string? properAssertMethodName = diagnostic.Properties[UseProperAssertMethodsAnalyzer.ProperAssertMethodNameKey];
+        if (mode is null || properAssertMethodName is null)
+        {
+            Debug.Fail($"Both '{nameof(mode)}' and '{properAssertMethodName}' are expected to be non-null.");
+            return;
+        }
+
+        SyntaxNode diagnosticNode = root.FindNode(diagnostic.Location.SourceSpan, getInnermostNodeForTie: true);
+        if (diagnosticNode is not InvocationExpressionSyntax invocation)
+        {
+            Debug.Fail($"Is this an interesting scenario where IInvocationOperation for Assert call isn't associated with InvocationExpressionSyntax? SyntaxNode type: '{diagnosticNode.GetType()}', Text: '{diagnosticNode.GetText()}'");
+            return;
+        }
+
+        SyntaxNode methodNameIdentifier = invocation.Expression;
+        if (methodNameIdentifier is MemberAccessExpressionSyntax memberAccess)
+        {
+            methodNameIdentifier = memberAccess.Name;
+        }
+
+        if (methodNameIdentifier is not IdentifierNameSyntax identifierNameSyntax)
+        {
+            Debug.Fail($"Is this an interesting scenario where we are unable to retrieve IdentifierNameSyntax corresponding to the assert method? SyntaxNode type: '{methodNameIdentifier}', Text: '{methodNameIdentifier.GetText()}'.");
+            return;
+        }
+
+        Func<CancellationToken, Task<Document>>? createChangedDocument = null;
+        switch (mode)
+        {
+            case UseProperAssertMethodsAnalyzer.CodeFixModeSimple:
+                createChangedDocument = ct => FixAssertMethodForSimpleModeAsync(context.Document, diagnostic.AdditionalLocations[0], diagnostic.AdditionalLocations[1], root, identifierNameSyntax, properAssertMethodName, ct);
+                break;
+            case UseProperAssertMethodsAnalyzer.CodeFixModeAddArgument:
+                createChangedDocument = ct => FixAssertMethodForAddArgumentModeAsync(context.Document, diagnostic.AdditionalLocations[0], diagnostic.AdditionalLocations[1], diagnostic.AdditionalLocations[2], root, identifierNameSyntax, properAssertMethodName, ct);
+                break;
+            case UseProperAssertMethodsAnalyzer.CodeFixModeRemoveArgument:
+                createChangedDocument = ct => FixAssertMethodForRemoveArgumentModeAsync(context.Document, diagnostic.AdditionalLocations[0], root, identifierNameSyntax, properAssertMethodName, ct);
+                break;
+            default:
+                break;
+        }
+
+        if (createChangedDocument is not null)
+        {
+            context.RegisterCodeFix(
+            CodeAction.Create(
+                title: string.Format(CultureInfo.InvariantCulture, CodeFixResources.UseProperAssertMethodsFix, properAssertMethodName),
+                createChangedDocument,
+                equivalenceKey: nameof(UseProperAssertMethodsFixer)),
+            diagnostic);
+        }
+    }
+
+    private static async Task<Document> FixAssertMethodForSimpleModeAsync(Document document, Location location1, Location location2, SyntaxNode root, IdentifierNameSyntax identifierNameSyntax, string properAssertMethodName, CancellationToken cancellationToken)
+    {
+        // This doesn't properly handle cases like Assert.IsTrue(message: "My message", condition: x == null)
+        // The proper handling of this may be Assert.IsNull(message: "My message", value: x)
+        // Or: Assert.IsNull(x, "My message")
+        // For now this is not handled.
+        if (root.FindNode(location1.SourceSpan) is not ArgumentSyntax node1)
+        {
+            return document;
+        }
+
+        if (root.FindNode(location2.SourceSpan) is not ExpressionSyntax node2)
+        {
+            return document;
+        }
+
+        DocumentEditor editor = await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(false);
+        FixInvocationMethodName(editor, identifierNameSyntax, properAssertMethodName);
+        editor.ReplaceNode(node1, SyntaxFactory.Argument(node2).WithAdditionalAnnotations(Formatter.Annotation));
+
+        return editor.GetChangedDocument();
+    }
+
+    private static async Task<Document> FixAssertMethodForAddArgumentModeAsync(Document document, Location location1, Location location2, Location location3, SyntaxNode root, IdentifierNameSyntax identifierNameSyntax, string properAssertMethodName, CancellationToken cancellationToken)
+    {
+        // This doesn't properly handle cases like Assert.IsTrue(message: "My message", condition: x == y)
+        // The proper handling of this may be Assert.AreEqual(message: "My message", expected: x, actual: y)
+        // Or: Assert.AreEqual(x, y, "My message")
+        // For now this is not handled.
+        if (root.FindNode(location1.SourceSpan) is not ArgumentSyntax node1)
+        {
+            return document;
+        }
+
+        if (node1.Parent is not ArgumentListSyntax argumentList)
+        {
+            return document;
+        }
+
+        if (root.FindNode(location2.SourceSpan) is not ExpressionSyntax node2)
+        {
+            return document;
+        }
+
+        if (root.FindNode(location3.SourceSpan) is not ExpressionSyntax node3)
+        {
+            return document;
+        }
+
+        DocumentEditor editor = await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(false);
+        FixInvocationMethodName(editor, identifierNameSyntax, properAssertMethodName);
+
+        ArgumentListSyntax newArgumentList = argumentList;
+        newArgumentList = newArgumentList.ReplaceNode(node1, SyntaxFactory.Argument(node2).WithAdditionalAnnotations(Formatter.Annotation));
+        int insertionIndex = argumentList.Arguments.IndexOf(node1) + 1;
+        newArgumentList = newArgumentList.WithArguments(newArgumentList.Arguments.Insert(insertionIndex, SyntaxFactory.Argument(node3).WithAdditionalAnnotations(Formatter.Annotation)));
+
+        editor.ReplaceNode(argumentList, newArgumentList);
+
+        return editor.GetChangedDocument();
+    }
+
+    private static async Task<Document> FixAssertMethodForRemoveArgumentModeAsync(Document document, Location location1, SyntaxNode root, IdentifierNameSyntax identifierNameSyntax, string properAssertMethodName, CancellationToken cancellationToken)
+    {
+        // This doesn't properly handle cases like Assert.AreEqual(message: "My message", expected: true, actual: x)
+        // The proper handling of this may be Assert.IsTrue(message: "My message", condition: x)
+        // Or: Assert.IsTrue(x, "My message")
+        // For now this is not handled.
+        if (root.FindNode(location1.SourceSpan) is not ArgumentSyntax node1)
+        {
+            return document;
+        }
+
+        if (node1.Parent is not ArgumentListSyntax argumentList)
+        {
+            return document;
+        }
+
+        DocumentEditor editor = await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(false);
+        FixInvocationMethodName(editor, identifierNameSyntax, properAssertMethodName);
+        editor.ReplaceNode(argumentList, argumentList.WithArguments(argumentList.Arguments.Remove(node1)));
+
+        return editor.GetChangedDocument();
+    }
+
+    private static void FixInvocationMethodName(DocumentEditor editor, IdentifierNameSyntax identifierNameSyntax, string properAssertMethodName)
+    {
+        IdentifierNameSyntax updatedIdentifier = identifierNameSyntax.WithIdentifier(SyntaxFactory.Identifier(identifierNameSyntax.Identifier.LeadingTrivia, properAssertMethodName, identifierNameSyntax.Identifier.TrailingTrivia));
+        editor.ReplaceNode(identifierNameSyntax, updatedIdentifier);
+    }
+}
--- src/Analyzers/MSTest.Analyzers/UseProperAssertMethodsAnalyzer.cs
+++ src/Analyzers/MSTest.Analyzers/UseProperAssertMethodsAnalyzer.cs
@@ -5,6 +5,7 @@ using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 
+using Analyzer.Utilities;
 using Analyzer.Utilities.Extensions;
 
 using Microsoft.CodeAnalysis;
@@ -52,6 +53,56 @@ internal sealed class UseProperAssertMethodsAnalyzer : DiagnosticAnalyzer
         Equals,
         NotEquals,
     }
+
+    internal const string ProperAssertMethodNameKey = nameof(ProperAssertMethodNameKey);
+
+    /// <summary>
+    /// Key in the properties bag that has value one of CodeFixModeSimple, CodeFixModeAddArgument, or CodeFixModeRemoveArgument.
+    /// </summary>
+    internal const string CodeFixModeKey = nameof(CodeFixModeKey);
+
+    /// <summary>
+    /// This mode means the codefix operation is as follows:
+    /// <list type="number">
+    /// <item>Find the right assert method name from the properties bag using <see cref="ProperAssertMethodNameKey"/>.</item>
+    /// <item>Replace the identifier syntax for the invocation with the right assert method name. The identifier syntax is calculated by the codefix.</item>
+    /// <item>Replace the syntax node from the first additional locations with the node from second additional locations.</item>
+    /// </list>
+    /// <para>Example: For <c>Assert.IsTrue(x == null)</c>, it will become <c>Assert.IsNull(x)</c>.</para>
+    /// <para>The value for ProperAssertMethodNameKey is "IsNull".</para>
+    /// <para>The first additional location will point to the "x == null" node.</para>
+    /// <para>The second additional location will point to the "x" node.</para>
+    /// </summary>
+    internal const string CodeFixModeSimple = nameof(CodeFixModeSimple);
+
+    /// <summary>
+    /// This mode means the codefix operation is as follows:
+    /// <list type="number">
+    /// <item>Find the right assert method name from the properties bag using <see cref="ProperAssertMethodNameKey"/>.</item>
+    /// <item>Replace the identifier syntax for the invocation with the right assert method name. The identifier syntax is calculated by the codefix.</item>
+    /// <item>Replace the syntax node from the first additional locations with the node from second additional locations.</item>
+    /// <item>Add new argument which is identical to the node from third additional locations.</item>
+    /// </list>
+    /// <para>Example: For <c>Assert.IsTrue(x == y)</c>, it will become <c>Assert.AreEqual(y, x)</c>.</para>
+    /// <para>The value for ProperAssertMethodNameKey is "AreEqual".</para>
+    /// <para>The first additional location will point to the "x == y" node.</para>
+    /// <para>The second additional location will point to the "y" node.</para>
+    /// <para>The third additional location will point to the "x" node.</para>
+    /// </summary>
+    internal const string CodeFixModeAddArgument = nameof(CodeFixModeAddArgument);
+
+    /// <summary>
+    /// This mode means the codefix operation is as follows:
+    /// <list type="number">
+    /// <item>Find the right assert method name from the properties bag using <see cref="ProperAssertMethodNameKey"/>.</item>
+    /// <item>Replace the identifier syntax for the invocation with the right assert method name. The identifier syntax is calculated by the codefix.</item>
+    /// <item>Remove the argument which the second additional locations points to.</item>
+    /// </list>
+    /// <para>Example: For <c>Assert.AreEqual(false, x)</c>, it will become <c>Assert.IsFalse(x)</c>.</para>
+    /// <para>The value for ProperAssertMethodNameKey is "IsFalse".</para>
+    /// <para>The first additional location will point to the "false" node.</para>
+    /// </summary>
+    internal const string CodeFixModeRemoveArgument = nameof(CodeFixModeRemoveArgument);
 
     private static readonly LocalizableResourceString Title = new(nameof(Resources.UseProperAssertMethodsTitle), Resources.ResourceManager, typeof(Resources));
     private static readonly LocalizableResourceString MessageFormat = new(nameof(Resources.UseProperAssertMethodsMessageFormat), Resources.ResourceManager, typeof(Resources));
@@ -118,31 +169,67 @@ internal sealed class UseProperAssertMethodsAnalyzer : DiagnosticAnalyzer
         }
     }
 
-    private static bool IsIsNullPattern(IOperation operation)
-        => operation is IIsPatternOperation { Pattern: IConstantPatternOperation { Value: { } constantPatternValue } } &&
-            constantPatternValue.WalkDownConversion() is ILiteralOperation { ConstantValue: { HasValue: true, Value: null } };
+    private static bool IsIsNullPattern(IOperation operation, [NotNullWhen(true)] out SyntaxNode? expressionUnderTest)
+    {
+        if (operation is IIsPatternOperation { Pattern: IConstantPatternOperation { Value: { } constantPatternValue } } isPatternOperation &&
+            constantPatternValue.WalkDownConversion() is ILiteralOperation { ConstantValue: { HasValue: true, Value: null } })
+        {
+            expressionUnderTest = isPatternOperation.Value.Syntax;
+            return true;
+        }
 
-    private static bool IsIsNotNullPattern(IOperation operation)
-        => operation is IIsPatternOperation { Pattern: INegatedPatternOperation { Pattern: IConstantPatternOperation { Value: { } constantPatternValue } } } &&
-        constantPatternValue.WalkDownConversion() is ILiteralOperation { ConstantValue: { HasValue: true, Value: null } };
+        expressionUnderTest = null;
+        return false;
+    }
+
+    private static bool IsIsNotNullPattern(IOperation operation, [NotNullWhen(true)] out SyntaxNode? expressionUnderTest)
+    {
+        if (operation is IIsPatternOperation { Pattern: INegatedPatternOperation { Pattern: IConstantPatternOperation { Value: { } constantPatternValue } } } isPatternOperation &&
+            constantPatternValue.WalkDownConversion() is ILiteralOperation { ConstantValue: { HasValue: true, Value: null } })
+        {
+            expressionUnderTest = isPatternOperation.Value.Syntax;
+            return true;
+        }
+
+        expressionUnderTest = null;
+        return false;
+    }
 
     // TODO: Recognize 'null == something' (i.e, when null is the left operand)
-    private static bool IsEqualsNullBinaryOperator(IOperation operation)
-        => operation is IBinaryOperation { OperatorKind: BinaryOperatorKind.Equals, RightOperand: { } rightOperand } &&
-            rightOperand.WalkDownConversion() is ILiteralOperation { ConstantValue: { HasValue: true, Value: null } };
+    private static bool IsEqualsNullBinaryOperator(IOperation operation, [NotNullWhen(true)] out SyntaxNode? expressionUnderTest)
+    {
+        if (operation is IBinaryOperation { OperatorKind: BinaryOperatorKind.Equals, RightOperand: { } rightOperand } binaryOperation &&
+            rightOperand.WalkDownConversion() is ILiteralOperation { ConstantValue: { HasValue: true, Value: null } })
+        {
+            expressionUnderTest = binaryOperation.LeftOperand.Syntax;
+            return true;
+        }
+
+        expressionUnderTest = null;
+        return false;
+    }
 
     // TODO: Recognize 'null != something' (i.e, when null is the left operand)
-    private static bool IsNotEqualsNullBinaryOperator(IOperation operation)
-        => operation is IBinaryOperation { OperatorKind: BinaryOperatorKind.NotEquals, RightOperand: { } rightOperand } &&
-            rightOperand.WalkDownConversion() is ILiteralOperation { ConstantValue: { HasValue: true, Value: null } };
-
-    private static NullCheckStatus RecognizeNullCheck(IOperation operation)
+    private static bool IsNotEqualsNullBinaryOperator(IOperation operation, [NotNullWhen(true)] out SyntaxNode? expressionUnderTest)
     {
-        if (IsIsNullPattern(operation) || IsEqualsNullBinaryOperator(operation))
+        if (operation is IBinaryOperation { OperatorKind: BinaryOperatorKind.NotEquals, RightOperand: { } rightOperand } binaryOperation &&
+            rightOperand.WalkDownConversion() is ILiteralOperation { ConstantValue: { HasValue: true, Value: null } })
+        {
+            expressionUnderTest = binaryOperation.LeftOperand.Syntax;
+            return true;
+        }
+
+        expressionUnderTest = null;
+        return false;
+    }
+
+    private static NullCheckStatus RecognizeNullCheck(IOperation operation, /*We cannot express this is not null when NullCheckStatis is not Unknown*/ out SyntaxNode? expressionUnderTest)
+    {
+        if (IsIsNullPattern(operation, out expressionUnderTest) || IsEqualsNullBinaryOperator(operation, out expressionUnderTest))
         {
             return NullCheckStatus.IsNull;
         }
-        else if (IsIsNotNullPattern(operation) || IsNotEqualsNullBinaryOperator(operation))
+        else if (IsIsNotNullPattern(operation, out expressionUnderTest) || IsNotEqualsNullBinaryOperator(operation, out expressionUnderTest))
         {
             return NullCheckStatus.IsNotNull;
         }
@@ -150,52 +237,91 @@ internal sealed class UseProperAssertMethodsAnalyzer : DiagnosticAnalyzer
         return NullCheckStatus.Unknown;
     }
 
-    private static EqualityCheckStatus RecognizeEqualityCheck(IOperation operation)
+    private static EqualityCheckStatus RecognizeEqualityCheck(IOperation operation, out SyntaxNode? toBecomeExpected, out SyntaxNode? toBecomeActual)
     {
-        if (operation is IIsPatternOperation { Pattern: IConstantPatternOperation } or
-            IBinaryOperation { OperatorKind: BinaryOperatorKind.Equals })
+        if (operation is IIsPatternOperation { Pattern: IConstantPatternOperation constantPattern1 } isPattern1)
         {
+            toBecomeExpected = constantPattern1.Syntax;
+            toBecomeActual = isPattern1.Value.Syntax;
             return EqualityCheckStatus.Equals;
         }
-        else if (operation is IIsPatternOperation { Pattern: INegatedPatternOperation { Pattern: IConstantPatternOperation } } or
-            IBinaryOperation { OperatorKind: BinaryOperatorKind.NotEquals })
+        else if (operation is IBinaryOperation { OperatorKind: BinaryOperatorKind.Equals } binaryOperation1)
         {
+            // This is quite arbitrary. We can do extra checks to see which one (if any) looks like a "constant" and make it the expected.
+            toBecomeExpected = binaryOperation1.RightOperand.Syntax;
+            toBecomeActual = binaryOperation1.LeftOperand.Syntax;
+            return EqualityCheckStatus.Equals;
+        }
+        else if (operation is IIsPatternOperation { Pattern: INegatedPatternOperation { Pattern: IConstantPatternOperation constantPattern2 } } isPattern2)
+        {
+            toBecomeExpected = constantPattern2.Syntax;
+            toBecomeActual = isPattern2.Value.Syntax;
+            return EqualityCheckStatus.NotEquals;
+        }
+        else if (operation is IBinaryOperation { OperatorKind: BinaryOperatorKind.NotEquals } binaryOperation2)
+        {
+            // This is quite arbitrary. We can do extra checks to see which one (if any) looks like a "constant" and make it the expected.
+            toBecomeExpected = binaryOperation2.RightOperand.Syntax;
+            toBecomeActual = binaryOperation2.LeftOperand.Syntax;
             return EqualityCheckStatus.NotEquals;
         }
 
+        toBecomeExpected = null;
+        toBecomeActual = null;
         return EqualityCheckStatus.Unknown;
     }
 
     private static void AnalyzeIsTrueOrIsFalseInvocation(OperationAnalysisContext context, IOperation conditionArgument, bool isTrueInvocation)
     {
-        NullCheckStatus nullCheckStatus = RecognizeNullCheck(conditionArgument);
+        RoslynDebug.Assert(context.Operation is IInvocationOperation, "Expected IInvocationOperation.");
+
+        NullCheckStatus nullCheckStatus = RecognizeNullCheck(conditionArgument, out SyntaxNode? expressionUnderTest);
         if (nullCheckStatus != NullCheckStatus.Unknown)
         {
-            Debug.Assert(nullCheckStatus is NullCheckStatus.IsNull or NullCheckStatus.IsNotNull, "Unexpected NullCheckStatus value.");
+            RoslynDebug.Assert(expressionUnderTest is not null, $"Unexpected null for '{nameof(expressionUnderTest)}'.");
+            RoslynDebug.Assert(nullCheckStatus is NullCheckStatus.IsNull or NullCheckStatus.IsNotNull, "Unexpected NullCheckStatus value.");
             bool shouldUseIsNull = isTrueInvocation
                 ? nullCheckStatus == NullCheckStatus.IsNull
                 : nullCheckStatus == NullCheckStatus.IsNotNull;
 
+            // Here, the codefix will want to switch something like Assert.IsTrue(x == null) with Assert.IsNull(x)
+            // This is the "simple" mode.
+
             // The message is: Use 'Assert.{0}' instead of 'Assert.{1}'.
+            string properAssertMethod = shouldUseIsNull ? "IsNull" : "IsNotNull";
             context.ReportDiagnostic(context.Operation.CreateDiagnostic(
                 Rule,
-                shouldUseIsNull ? "IsNull" : "IsNotNull",
+                additionalLocations: ImmutableArray.Create(conditionArgument.Syntax.GetLocation(), expressionUnderTest.GetLocation()),
+                properties: ImmutableDictionary<string, string?>.Empty
+                    .Add(ProperAssertMethodNameKey, properAssertMethod)
+                    .Add(CodeFixModeKey, CodeFixModeSimple),
+                properAssertMethod,
                 isTrueInvocation ? "IsTrue" : "IsFalse"));
             return;
         }
 
-        EqualityCheckStatus equalityCheckStatus = RecognizeEqualityCheck(conditionArgument);
+        EqualityCheckStatus equalityCheckStatus = RecognizeEqualityCheck(conditionArgument, out SyntaxNode? toBecomeExpected, out SyntaxNode? toBecomeActual);
         if (equalityCheckStatus != EqualityCheckStatus.Unknown)
         {
-            Debug.Assert(equalityCheckStatus is EqualityCheckStatus.Equals or EqualityCheckStatus.NotEquals, "Unexpected EqualityCheckStatus value.");
+            RoslynDebug.Assert(toBecomeExpected is not null, $"Unexpected null for '{nameof(toBecomeExpected)}'.");
+            RoslynDebug.Assert(toBecomeActual is not null, $"Unexpected null for '{nameof(toBecomeActual)}'.");
+            RoslynDebug.Assert(equalityCheckStatus is EqualityCheckStatus.Equals or EqualityCheckStatus.NotEquals, "Unexpected EqualityCheckStatus value.");
             bool shouldUseAreEqual = isTrueInvocation
                 ? equalityCheckStatus == EqualityCheckStatus.Equals
                 : equalityCheckStatus == EqualityCheckStatus.NotEquals;
 
+            // Here, the codefix will want to switch something like Assert.IsTrue(x == y) with Assert.AreEqual(x, y)
+            // This is the "add argument" mode.
+
             // The message is: Use 'Assert.{0}' instead of 'Assert.{1}'.
+            string properAssertMethod = shouldUseAreEqual ? "AreEqual" : "AreNotEqual";
             context.ReportDiagnostic(context.Operation.CreateDiagnostic(
                 Rule,
-                shouldUseAreEqual ? "AreEqual" : "AreNotEqual",
+                additionalLocations: ImmutableArray.Create(conditionArgument.Syntax.GetLocation(), toBecomeExpected.GetLocation(), toBecomeActual.GetLocation()),
+                properties: ImmutableDictionary<string, string?>.Empty
+                    .Add(ProperAssertMethodNameKey, properAssertMethod)
+                    .Add(CodeFixModeKey, CodeFixModeAddArgument),
+                properAssertMethod,
                 isTrueInvocation ? "IsTrue" : "IsFalse"));
             return;
         }
@@ -209,20 +335,35 @@ internal sealed class UseProperAssertMethodsAnalyzer : DiagnosticAnalyzer
         {
             bool shouldUseIsTrue = expectedLiteralBoolean;
 
+            // Here, the codefix will want to switch something like Assert.AreEqual(true, x) with Assert.IsTrue(x)
+            // This is the "remove argument" mode.
+
             // The message is: Use 'Assert.{0}' instead of 'Assert.{1}'.
+            string properAssertMethod = shouldUseIsTrue ? "IsTrue" : "IsFalse";
             context.ReportDiagnostic(context.Operation.CreateDiagnostic(
                 Rule,
-                shouldUseIsTrue ? "IsTrue" : "IsFalse",
+                additionalLocations: ImmutableArray.Create(expectedArgument.Syntax.GetLocation()),
+                properties: ImmutableDictionary<string, string?>.Empty
+                    .Add(ProperAssertMethodNameKey, properAssertMethod)
+                    .Add(CodeFixModeKey, CodeFixModeRemoveArgument),
+                properAssertMethod,
                 isAreEqualInvocation ? "AreEqual" : "AreNotEqual"));
         }
         else if (expectedArgument is ILiteralOperation { ConstantValue: { HasValue: true, Value: null } })
         {
             bool shouldUseIsNull = isAreEqualInvocation;
 
+            // Here, the codefix will want to switch something like Assert.AreEqual(null, x) with Assert.IsNull(x)
+            // This is the "remove argument" mode.
+
             // The message is: Use 'Assert.{0}' instead of 'Assert.{1}'.
+            string properAssertMethod = shouldUseIsNull ? "IsNull" : "IsNotNull";
             context.ReportDiagnostic(context.Operation.CreateDiagnostic(
                 Rule,
-                shouldUseIsNull ? "IsNull" : "IsNotNull",
+                additionalLocations: ImmutableArray.Create(expectedArgument.Syntax.GetLocation()),
+                properties: ImmutableDictionary<string, string?>.Empty
+                    .Add(ProperAssertMethodNameKey, properAssertMethod)
+                    .Add(CodeFixModeKey, CodeFixModeRemoveArgument), properAssertMethod,
                 isAreEqualInvocation ? "AreEqual" : "AreNotEqual"));
         }
     }
