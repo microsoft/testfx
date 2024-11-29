be2a1a0902361fc274de357714535358d4061101
Amaury Levé <amauryleve@microsoft.com>
2024-11-29 23:04:02 +0100

    Add sample test app

---
+++ samples/public/BlankUwpNet9App/UnitTests.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+
+using Windows.UI.Xaml.Controls;
+
+namespace App1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, 0);
+        }
+
+        // Use the UITestMethod attribute for tests that need to run on the UI thread.
+        [UITestMethod]
+        public void TestMethod2()
+        {
+            var grid = new Grid();
+            Assert.AreEqual(0, grid.MinWidth);
+        }
+    }
+}
