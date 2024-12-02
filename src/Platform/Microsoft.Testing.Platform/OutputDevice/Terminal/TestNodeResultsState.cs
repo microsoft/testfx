3805a59fca30c0af47d6c2370fa21ab590a159f0
Artur Spychaj <drognanar@gmail.com>
2024-12-02 19:11:30 +0100

    Fixup small code style issues

--- src/Platform/Microsoft.Testing.Platform/OutputDevice/Terminal/TestNodeResultsState.cs
+++ src/Platform/Microsoft.Testing.Platform/OutputDevice/Terminal/TestNodeResultsState.cs
@@ -20,31 +20,29 @@ internal sealed class TestNodeResultsState
     public long Id { get; }
 
     private readonly TestDetailState _summaryDetail;
-
-    public ConcurrentDictionary<string, TestDetailState> TestNodeProgressStates { get; internal set; }
-        = new();
+    private readonly ConcurrentDictionary<string, TestDetailState> _testNodeProgressStates = new();
 
     public void AddRunningTestNode(int id, string uid, string name, IStopwatch stopwatch)
     {
-        TestNodeProgressStates[uid] = new TestDetailState(id, stopwatch, name);
+        _testNodeProgressStates[uid] = new TestDetailState(id, stopwatch, name);
         UpdateSummaryDetail();
     }
 
     public void RemoveRunningTestNode(string uid)
     {
-        TestNodeProgressStates.TryRemove(uid, out _);
+        _testNodeProgressStates.TryRemove(uid, out _);
         UpdateSummaryDetail();
     }
 
     private void UpdateSummaryDetail()
-        => _summaryDetail.Text = TestNodeProgressStates.Count > 5
-            ? $"... {string.Format(CultureInfo.CurrentCulture, PlatformResources.MoreTestsRunning, TestNodeProgressStates.Count)}"
+        => _summaryDetail.Text = _testNodeProgressStates.Count > 5
+            ? $"... {string.Format(CultureInfo.CurrentCulture, PlatformResources.MoreTestsRunning, _testNodeProgressStates.Count)}"
             : string.Empty;
 
     // Note: Show up to 5 long running tests per project.
     public IEnumerable<TestDetailState> GetRunningTasks()
     {
-        var sortedDetails = TestNodeProgressStates
+        IEnumerable<TestDetailState> sortedDetails = _testNodeProgressStates
             .Select(d => d.Value)
             .OrderBy(d => d.Stopwatch?.Elapsed ?? TimeSpan.Zero)
             .Take(5);
