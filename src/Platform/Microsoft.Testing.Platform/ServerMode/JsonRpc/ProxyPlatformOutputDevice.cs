adb40be6c3d9881689ab7890d162c8790baf7002
Youssef1313 <youssefvictor00@gmail.com>
2024-12-03 09:14:44 +0100

    Progress

--- src/Platform/Microsoft.Testing.Platform/ServerMode/JsonRpc/ProxyPlatformOutputDevice.cs
+++ src/Platform/Microsoft.Testing.Platform/ServerMode/JsonRpc/ProxyPlatformOutputDevice.cs
@@ -2,14 +2,25 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Microsoft.Testing.Platform.Extensions;
+using Microsoft.Testing.Platform.Extensions.Messages;
 using Microsoft.Testing.Platform.Extensions.OutputDevice;
+using Microsoft.Testing.Platform.Extensions.TestHost;
 using Microsoft.Testing.Platform.Helpers;
 using Microsoft.Testing.Platform.Hosts;
 using Microsoft.Testing.Platform.OutputDevice;
+using Microsoft.Testing.Platform.TestHost;
 
 namespace Microsoft.Testing.Platform.ServerMode;
 
-internal sealed class ProxyPlatformOutputDevice : IPlatformOutputDevice, IAsyncInitializableExtension
+// Any interfaces that can have special treatment for any output device should be implemented by
+// this proxy class and be forwarded properly.
+// This is not so good. How can we make sure we are not missing any interfaces that may be implemented by external output devices?
+internal sealed class ProxyPlatformOutputDevice : IHotReloadPlatformOutputDevice,
+    IDataConsumer,
+    IOutputDeviceDataProducer,
+    ITestSessionLifetimeHandler,
+    IDisposable,
+    IAsyncInitializableExtension
 {
     private readonly IPlatformOutputDevice _originalOutputDevice;
     private readonly ServerModePerCallOutputDevice? _serverModeOutputDevice;
@@ -27,6 +38,24 @@ internal sealed class ProxyPlatformOutputDevice : IPlatformOutputDevice, IAsyncI
     public string DisplayName => nameof(ProxyPlatformOutputDevice);
 
     public string Description => nameof(ProxyPlatformOutputDevice);
+
+    public Type[] DataTypesConsumed => (_originalOutputDevice as IDataConsumer)?.DataTypesConsumed ?? Array.Empty<Type>();
+
+    public async Task DisplayBeforeHotReloadSessionStartAsync()
+    {
+        if (_originalOutputDevice is IHotReloadPlatformOutputDevice hotReloadOutputDevice)
+        {
+            await hotReloadOutputDevice.DisplayBeforeHotReloadSessionStartAsync();
+        }
+    }
+
+    public async Task DisplayAfterHotReloadSessionEndAsync()
+    {
+        if (_originalOutputDevice is IHotReloadPlatformOutputDevice hotReloadOutputDevice)
+        {
+            await hotReloadOutputDevice.DisplayAfterHotReloadSessionEndAsync();
+        }
+    }
 
     public async Task DisplayAfterSessionEndRunAsync()
     {
@@ -70,6 +99,22 @@ internal sealed class ProxyPlatformOutputDevice : IPlatformOutputDevice, IAsyncI
     public async Task<bool> IsEnabledAsync()
         => (_serverModeOutputDevice is not null && await _serverModeOutputDevice.IsEnabledAsync()) || await _originalOutputDevice.IsEnabledAsync();
 
+    public async Task OnTestSessionFinishingAsync(SessionUid sessionUid, CancellationToken cancellationToken)
+    {
+        if (_originalOutputDevice is ITestSessionLifetimeHandler originalLifetimeHandler)
+        {
+            await originalLifetimeHandler.OnTestSessionFinishingAsync(sessionUid, cancellationToken);
+        }
+    }
+
+    public async Task OnTestSessionStartingAsync(SessionUid sessionUid, CancellationToken cancellationToken)
+    {
+        if (_originalOutputDevice is ITestSessionLifetimeHandler originalLifetimeHandler)
+        {
+            await originalLifetimeHandler.OnTestSessionStartingAsync(sessionUid, cancellationToken);
+        }
+    }
+
     internal async Task InitializeAsync(ServerTestHost serverTestHost)
     {
         if (_serverModeOutputDevice is not null)
@@ -77,4 +122,15 @@ internal sealed class ProxyPlatformOutputDevice : IPlatformOutputDevice, IAsyncI
             await _serverModeOutputDevice.InitializeAsync(serverTestHost);
         }
     }
+
+    public async Task ConsumeAsync(IDataProducer dataProducer, IData value, CancellationToken cancellationToken)
+    {
+        if (_originalOutputDevice is IDataConsumer dataConsumer)
+        {
+            await dataConsumer.ConsumeAsync(dataProducer, value, cancellationToken);
+        }
+    }
+
+    public void Dispose()
+        => (_originalOutputDevice as IDisposable)?.Dispose();
 }
