099da3a7b2c009ec5800f7e877ff0116ea52f300
Youssef Victor <youssefvictor00@gmail.com>
2024-12-08 08:00:10 +0100

    Fix build

--- src/Adapter/MSTestAdapter.PlatformServices/Services/ReflectionOperations.cs
+++ src/Adapter/MSTestAdapter.PlatformServices/Services/ReflectionOperations.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#if !NETFRAMEWORK
 using System.Diagnostics;
+#endif
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
