06f8daa368ceed22cb5de38f485be9c565f1a150
Youssef1313 <youssefvictor00@gmail.com>
2024-12-08 07:21:21 +0100

    [Performance] Avoid System.Action allocations for test context cancellation

--- src/Adapter/MSTest.TestAdapter/PlatformServiceProvider.cs
+++ src/Adapter/MSTest.TestAdapter/PlatformServiceProvider.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 
 #if !WINDOWS_UWP
@@ -17,6 +18,8 @@ namespace Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter;
 /// </summary>
 internal sealed class PlatformServiceProvider : IPlatformServiceProvider
 {
+    private static readonly Action<object?> CancelDelegate = static s => ((CancellationTokenSource)s!).Cancel();
+
     /// <summary>
     /// Initializes a new instance of the <see cref="PlatformServiceProvider"/> class - a singleton.
     /// </summary>
@@ -223,7 +226,7 @@ internal sealed class PlatformServiceProvider : IPlatformServiceProvider
     public ITestContext GetTestContext(ITestMethod testMethod, StringWriter writer, IDictionary<string, object?> properties)
     {
         var testContextImplementation = new TestContextImplementation(testMethod, writer, properties);
-        TestRunCancellationToken?.Register(testContextImplementation.Context.CancellationTokenSource.Cancel);
+        TestRunCancellationToken?.Register(static state => ((TestContextImplementation)state!).Context.CancellationTokenSource.Cancel(), testContextImplementation);
         return testContextImplementation;
     }
 }
