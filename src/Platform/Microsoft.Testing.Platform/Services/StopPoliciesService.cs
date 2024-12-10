264eb26b6291da468788eda28fc33457be813ddd
Youssef1313 <youssefvictor00@gmail.com>
2024-12-10 05:30:40 +0100

    Cleanup

--- src/Platform/Microsoft.Testing.Platform/Services/StopPoliciesService.cs
+++ src/Platform/Microsoft.Testing.Platform/Services/StopPoliciesService.cs
@@ -9,33 +9,11 @@ namespace Microsoft.Testing.Platform.Services;
 
 internal sealed class StopPoliciesService : IStopPoliciesService
 {
-    private readonly Lock _abortLock = new();
-
-    public StopPoliciesService(ITestApplicationCancellationTokenSource testApplicationCancellationTokenSource)
-    {
+    public StopPoliciesService(ITestApplicationCancellationTokenSource testApplicationCancellationTokenSource) =>
 #pragma warning disable VSTHRD101 // Avoid unsupported async delegates
+        // Note: If cancellation already requested, Register will still invoke the callback.
         testApplicationCancellationTokenSource.CancellationToken.Register(async () => await ExecuteAbortCallbacksAsync());
 #pragma warning restore VSTHRD101 // Avoid unsupported async delegates
-
-        // This happens in mocked tests because the test will do the Cancel on the first CancellationToken property access.
-        // In theory, cancellation may happen in practice fast enough before StopPoliciesService is created.
-        if (testApplicationCancellationTokenSource.CancellationToken.IsCancellationRequested)
-        {
-            _ = ExecuteAbortCallbacksAsync();
-        }
-
-        // NOTE::: Don't move the CancellationToken.Register call to an "else" here.
-        // If the call is moved to else, then this race may happen:
-        // 1. Check IsCancellationRequested -> false
-        // 2. Cancellation happens
-        // 3. Go to the "else" and do CancellationToken.Register which will never happen because
-        //    cancellation already happened after IsCancellationRequested check and before CancellationToken.Register.
-        //
-        // With the current implementation above, we always do the Register first.
-        // Then, if IsCancellationRequested was false, we are sure the register already happened and we get the callback when cancelled.
-        // However, if we found IsCancellationRequested to be true, we are not sure if cancellation happened before Register or not.
-        // So, we may call ExecuteAbortCallbacksAsync twice. This is fine, we handle that with a lock inside ExecuteAbortCallbacksAsync.
-    }
 
     private BlockingCollection<Func<int, CancellationToken, Task>>? _maxFailedTestsCallbacks;
     private BlockingCollection<Func<Task>>? _abortCallbacks;
@@ -65,15 +43,7 @@ internal sealed class StopPoliciesService : IStopPoliciesService
 
     public async Task ExecuteAbortCallbacksAsync()
     {
-        lock (_abortLock)
-        {
-            if (IsAbortTriggered)
-            {
-                return;
-            }
-
-            IsAbortTriggered = true;
-        }
+        IsAbortTriggered = true;
 
         if (_abortCallbacks is null)
         {
