6c918b2ad90113e82f539065ba5b24d244ba7ecb
Simon Cropp <simon.cropp@gmail.com>
2024-12-17 17:17:37 +1100

    Update ExceptionHelper.cs

--- src/Adapter/MSTest.TestAdapter/Execution/ExceptionHelper.cs
+++ src/Adapter/MSTest.TestAdapter/Execution/ExceptionHelper.cs
@@ -79,7 +79,7 @@ internal static class ExceptionHelper
         {
             if (!first)
             {
-                result.Append(UtaEndOfInnerExceptionTrace);
+                result.AppendLine(Resource.UTA_EndOfInnerExceptionTrace);
             }
 
             result.Append(' ');
@@ -90,8 +90,6 @@ internal static class ExceptionHelper
 
         return CreateStackTraceInformation(ex, true, result.ToString());
     }
-
-    private static readonly string UtaEndOfInnerExceptionTrace = Resource.UTA_EndOfInnerExceptionTrace + Environment.NewLine;
 
     /// <summary>
     /// Removes all stack frames that refer to Microsoft.VisualStudio.TestTools.UnitTesting.Assertion.
