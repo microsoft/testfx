ff3ff42d799ef748c55c57337f0d82fc323b566c
Youssef Victor <youssefvictor00@gmail.com>
2024-12-20 12:23:34 +0100

    Remove unused using

--- test/IntegrationTests/MSTest.IntegrationTests/Parameterized tests/DynamicDataTests.cs
+++ test/IntegrationTests/MSTest.IntegrationTests/Parameterized tests/DynamicDataTests.cs
@@ -1,8 +1,7 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Immutable;
-using System.Diagnostics;
 
 using Microsoft.MSTestV2.CLIAutomation;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
