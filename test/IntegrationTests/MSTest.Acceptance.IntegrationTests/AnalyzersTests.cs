6302900cb7e6faf68d0e0075b4c633ff37256574
Youssef1313 <youssefvictor00@gmail.com>
2024-12-23 13:00:42 +0100

    Fix analyzer messages not localized

---
+++ test/IntegrationTests/MSTest.Acceptance.IntegrationTests/AnalyzersTests.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Testing.Platform.Acceptance.IntegrationTests;
+using Microsoft.Testing.Platform.Acceptance.IntegrationTests.Helpers;
+
+namespace MSTest.Acceptance.IntegrationTests;
+
+[TestClass]
+public sealed class AnalyzersTests : AcceptanceTestBase<AnalyzersTests.TestAssetFixture>
+{
+    [TestMethod]
+    public async Task AnalyzerMessagesShouldBeLocalized()
+    {
+        var testHost = TestHost.LocateFrom(AssetFixture.ProjectPath, TestAssetFixture.ProjectName, TargetFrameworks.NetCurrent);
+        TestHostResult testHostResult = await testHost.ExecuteAsync(environmentVariables: new()
+        {
+            // This is fr-FR
+            ["VSLang"] = "1036",
+        });
+
+        testHostResult.AssertOutputContains("DataRow ne doit être défini que sur une méthode de test");
+    }
+
+    public sealed class TestAssetFixture() : TestAssetFixtureBase(AcceptanceFixture.NuGetGlobalPackagesFolder)
+    {
+        public const string ProjectName = "Analyzers";
+
+        public string ProjectPath => GetAssetPath(ProjectName);
+
+        public override IEnumerable<(string ID, string Name, string Code)> GetAssetsToGenerate()
+        {
+            yield return (ProjectName, ProjectName,
+                SourceCode
+                .PatchTargetFrameworks(TargetFrameworks.NetCurrent)
+                .PatchCodeWithReplace("$MSTestVersion$", MSTestVersion));
+        }
+
+        private const string SourceCode = """
+#file Analyzers.csproj
+<Project Sdk="MSTest.Sdk/$MSTestVersion$">
+
+  <PropertyGroup>
+    <TargetFrameworks>$TargetFrameworks$</TargetFrameworks>
+  </PropertyGroup>
+
+</Project>
+
+#file UnitTest1.cs
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class UnitTest1
+{
+    [DataRow(0)]
+    public void TestMethod()
+    {
+    }
+}
+""";
+    }
+}
