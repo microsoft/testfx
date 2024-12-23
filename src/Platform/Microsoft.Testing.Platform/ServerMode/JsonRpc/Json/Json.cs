b8f756fbe7c46ea286018adf6b60bcf0f5942f1d
Simon Cropp <simon.cropp@gmail.com>
2024-12-23 22:13:02 +1100

    .

--- src/Platform/Microsoft.Testing.Platform/ServerMode/JsonRpc/Json/Json.cs
+++ src/Platform/Microsoft.Testing.Platform/ServerMode/JsonRpc/Json/Json.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Collections;
-using System.Text;
 using System.Text.Json;
 
 using Microsoft.Testing.Platform.Extensions.Messages;
