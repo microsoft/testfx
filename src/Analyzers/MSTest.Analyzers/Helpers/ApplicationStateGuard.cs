2067937bc45ff3a7f9fe6d530dc49a90b522c1ee
Simon Cropp <simon.cropp@gmail.com>
2024-12-23 22:23:49 +1100

    UnreachableException is more correct

--- src/Analyzers/MSTest.Analyzers/Helpers/ApplicationStateGuard.cs
+++ src/Analyzers/MSTest.Analyzers/Helpers/ApplicationStateGuard.cs
@@ -1,12 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace MSTest.Analyzers.Helpers;
 
 internal static class ApplicationStateGuard
 {
-    public static InvalidOperationException Unreachable([CallerFilePath] string? path = null, [CallerLineNumber] int line = 0)
+    public static UnreachableException Unreachable([CallerFilePath] string? path = null, [CallerLineNumber] int line = 0)
         => new($"This program location is thought to be unreachable. File='{path}' Line={line}");
 }
