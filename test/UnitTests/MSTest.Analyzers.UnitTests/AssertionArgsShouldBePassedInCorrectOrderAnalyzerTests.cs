cbcc9c91a1063ee2c4bf61759b78895ee0055ef1
Youssef Victor <youssefvictor00@gmail.com>
2024-12-28 14:48:32 +0100

    Fix MSTEST0017 false positive when both actual/expected are constants (#4460)
    Co-authored-by: Amaury Levé <amauryleve@microsoft.com>

--- test/UnitTests/MSTest.Analyzers.UnitTests/AssertionArgsShouldBePassedInCorrectOrderAnalyzerTests.cs
+++ test/UnitTests/MSTest.Analyzers.UnitTests/AssertionArgsShouldBePassedInCorrectOrderAnalyzerTests.cs
@@ -163,6 +163,28 @@ public sealed class AssertionArgsShouldBePassedInCorrectOrderAnalyzerTests
     }
 
     [TestMethod]
+    public async Task WhenBothAreLiterals_NoDiagnostic()
+    {
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+            using System.Collections.Generic;
+
+            [TestClass]
+            public class MyTestClass
+            {
+                [TestMethod]
+                public void NonCompliant()
+                {
+                    Assert.AreEqual(0, 0);
+                    Assert.AreEqual(0, 1);
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyCodeFixAsync(code, code);
+    }
+
+    [TestMethod]
     public async Task LiteralUsingNamedArgument()
     {
         string code = """
