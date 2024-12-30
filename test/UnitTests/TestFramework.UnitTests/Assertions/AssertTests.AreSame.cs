409afc169dbda4df281202369b9a7bf0726b6791
Youssef1313 <youssefvictor00@gmail.com>
2024-12-30 18:21:27 +0100

    Test coverage for AreSame/AreNotSame

---
+++ test/UnitTests/TestFramework.UnitTests/Assertions/AssertTests.AreSame.cs
@@ -0,0 +1,118 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#nullable enable
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.VisualStudio.TestPlatform.TestFramework.UnitTests;
+
+public partial class AssertTests
+{
+    private sealed class DummyClassTrackingToStringCalls
+    {
+        public bool WasToStringCalled { get; private set; }
+
+        public override string ToString()
+        {
+            WasToStringCalled = true;
+            return nameof(DummyClassTrackingToStringCalls);
+        }
+    }
+
+    public void AreSame_PassSameObject_ShouldPass()
+    {
+        object o = new();
+        Assert.AreSame(o, o);
+    }
+
+    public void AreSame_PassDifferentObject_ShouldFail()
+    {
+        Exception ex = VerifyThrows(() => Assert.AreSame(new object(), new object()));
+        Verify(ex.Message == "Assert.AreSame failed. ");
+    }
+
+    public void AreSame_StringMessage_PassSameObject_ShouldPass()
+    {
+        object o = new();
+        Assert.AreSame(o, o, "User-provided message");
+    }
+
+    public void AreSame_StringMessage_PassDifferentObject_ShouldFail()
+    {
+        Exception ex = VerifyThrows(() => Assert.AreSame(new object(), new object(), "User-provided message"));
+        Verify(ex.Message == "Assert.AreSame failed. User-provided message");
+    }
+
+    public void AreSame_InterpolatedString_PassSameObject_ShouldPass()
+    {
+        DummyClassTrackingToStringCalls o = new();
+        Assert.AreSame(o, o, $"User-provided message: {o}");
+        Verify(!o.WasToStringCalled);
+    }
+
+    public void AreSame_InterpolatedString_PassDifferentObject_ShouldFail()
+    {
+        DummyClassTrackingToStringCalls o = new();
+        Exception ex = VerifyThrows(() => Assert.AreSame(new object(), new object(), $"User-provided message. {o}"));
+        Verify(ex.Message == "Assert.AreSame failed. User-provided message. DummyClassTrackingToStringCalls");
+        Verify(o.WasToStringCalled);
+    }
+
+    public void AreSame_MessageArgs_PassSameObject_ShouldPass()
+    {
+        object o = new();
+        Assert.AreSame(o, o, "User-provided message: {0}", new object().GetType());
+    }
+
+    public void AreSame_MessageArgs_PassDifferentObject_ShouldFail()
+    {
+        Exception ex = VerifyThrows(() => Assert.AreSame(new object(), new object(), "User-provided message: System.Object type: {0}", new object().GetType()));
+        Verify(ex.Message == "Assert.AreSame failed. User-provided message: System.Object type: System.Object");
+    }
+
+    public void AreNotSame_PassDifferentObject_ShouldPass()
+        => Assert.AreNotSame(new object(), new object());
+
+    public void AreNotSame_PassSameObject_ShouldFail()
+    {
+        object o = new();
+        Exception ex = VerifyThrows(() => Assert.AreNotSame(o, o));
+        Verify(ex.Message == "Assert.AreNotSame failed. ");
+    }
+
+    public void AreNotSame_StringMessage_PassDifferentObject_ShouldPass()
+        => Assert.AreNotSame(new object(), new object(), "User-provided message");
+
+    public void AreNotSame_StringMessage_PassSameObject_ShouldFail()
+    {
+        object o = new();
+        Exception ex = VerifyThrows(() => Assert.AreNotSame(o, o, "User-provided message"));
+        Verify(ex.Message == "Assert.AreNotSame failed. User-provided message");
+    }
+
+    public void AreNotSame_InterpolatedString_PassDifferentObject_ShouldPass()
+    {
+        DummyClassTrackingToStringCalls o = new();
+        Assert.AreNotSame(new object(), new object(), $"User-provided message: {o}");
+        Verify(!o.WasToStringCalled);
+    }
+
+    public void AreNotSame_InterpolatedString_PassSameObject_ShouldFail()
+    {
+        DummyClassTrackingToStringCalls o = new();
+        Exception ex = VerifyThrows(() => Assert.AreNotSame(o, o, $"User-provided message. {o}"));
+        Verify(ex.Message == "Assert.AreNotSame failed. User-provided message. DummyClassTrackingToStringCalls");
+        Verify(o.WasToStringCalled);
+    }
+
+    public void AreNotSame_MessageArgs_PassDifferentObject_ShouldPass()
+        => Assert.AreNotSame(new object(), new object(), "User-provided message: {0}", new object().GetType());
+
+    public void AreNotSame_MessageArgs_PassSameObject_ShouldFail()
+    {
+        object o = new();
+        Exception ex = VerifyThrows(() => Assert.AreNotSame(o, o, "User-provided message: System.Object type: {0}", new object().GetType()));
+        Verify(ex.Message == "Assert.AreNotSame failed. User-provided message: System.Object type: System.Object");
+    }
+}
