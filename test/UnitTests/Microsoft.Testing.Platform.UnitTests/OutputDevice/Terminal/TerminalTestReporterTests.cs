d2ae877d9c7fcecd60d17c6bc7b39e513184e2ba
Amaury Levé <amauryleve@microsoft.com>
2024-12-31 16:37:53 +0100

    Test codecov PR comment

--- test/UnitTests/Microsoft.Testing.Platform.UnitTests/OutputDevice/Terminal/TerminalTestReporterTests.cs
+++ test/UnitTests/Microsoft.Testing.Platform.UnitTests/OutputDevice/Terminal/TerminalTestReporterTests.cs
@@ -32,7 +32,7 @@ public sealed class TerminalTestReporterTests
         StringAssert.Contains(terminal.Output, "    at Microsoft.Testing.Platform.UnitTests.TerminalTestReporterTests.AppendStackFrameFormatsStackTraceLineCorrectly()");
 #endif
         // Line number without the respective file
-        Assert.IsFalse(terminal.Output.ToString().Contains(" :0"));
+        Assert.Fail("test");
     }
 
     // Code with line when we have symbols
