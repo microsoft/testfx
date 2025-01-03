46e463761ed029fde691c99cc571682b178f06bf
Youssef1313 <youssefvictor00@gmail.com>
2025-01-03 17:18:04 +0100

    Remove unused usings

--- test/UnitTests/MSTestAdapter.UnitTests/Helpers/ReflectHelperTests.cs
+++ test/UnitTests/MSTestAdapter.UnitTests/Helpers/ReflectHelperTests.cs
@@ -3,7 +3,6 @@
 
 using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter;
 using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.Helpers;
-using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.MSTestAdapter.PlatformServices.Interface;
 using Microsoft.VisualStudio.TestPlatform.MSTestAdapter.UnitTests.TestableImplementations;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
