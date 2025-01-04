0a38c6cd1cbc484ffcdc4c8e0fcf50a4c63bfd87
Amaury Levé <amauryleve@microsoft.com>
2025-01-04 10:59:43 +0100

    Don't report MSTEST0001 when TestAdapter isn't referenced (#4481) (#4509)

--- test/UnitTests/MSTest.Analyzers.UnitTests/UseParallelizeAttributeAnalyzerTests.cs
+++ test/UnitTests/MSTest.Analyzers.UnitTests/UseParallelizeAttributeAnalyzerTests.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter;
+
 using VerifyCS = MSTest.Analyzers.Test.CSharpCodeFixVerifier<
     MSTest.Analyzers.UseParallelizeAttributeAnalyzer,
     Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
@@ -10,9 +14,28 @@ namespace MSTest.Analyzers.Test;
 [TestGroup]
 public class UseParallelizeAttributeAnalyzerTests(ITestExecutionContext testExecutionContext) : TestBase(testExecutionContext)
 {
-    public async Task WhenNoAttributeSpecified_Diagnostic() => await VerifyCS.VerifyAnalyzerAsync(
-            string.Empty,
-            VerifyCS.Diagnostic(UseParallelizeAttributeAnalyzer.Rule).WithNoLocation());
+    private static async Task VerifyAsync(string code, bool includeTestAdapter, params DiagnosticResult[] expected)
+    {
+        var test = new VerifyCS.Test
+        {
+            TestCode = code,
+        };
+
+        if (includeTestAdapter)
+        {
+            // NOTE: Test constructor already adds TestFramework refs.
+            test.TestState.AdditionalReferences.Add(MetadataReference.CreateFromFile(typeof(MSTestExecutor).Assembly.Location));
+        }
+
+        test.ExpectedDiagnostics.AddRange(expected);
+        await test.RunAsync();
+    }
+
+    public async Task WhenNoAttributeSpecified_TestAdapterNotReferenced_NoDiagnostic()
+        => await VerifyAsync(string.Empty, includeTestAdapter: false);
+
+    public async Task WhenNoAttributeSpecified_TestAdapterReferenced_Diagnostic()
+        => await VerifyAsync(string.Empty, includeTestAdapter: true, VerifyCS.Diagnostic(UseParallelizeAttributeAnalyzer.Rule).WithNoLocation());
 
     public async Task WhenParallelizeAttributeSet_NoDiagnostic()
     {
@@ -22,7 +45,8 @@ public class UseParallelizeAttributeAnalyzerTests(ITestExecutionContext testExec
             [assembly: Parallelize(Workers = 2, Scope = ExecutionScope.MethodLevel)]
             """;
 
-        await VerifyCS.VerifyAnalyzerAsync(code);
+        await VerifyAsync(code, includeTestAdapter: true);
+        await VerifyAsync(code, includeTestAdapter: false);
     }
 
     public async Task WhenDoNotParallelizeAttributeSet_NoDiagnostic()
@@ -33,6 +57,7 @@ public class UseParallelizeAttributeAnalyzerTests(ITestExecutionContext testExec
             [assembly: DoNotParallelize]
             """;
 
-        await VerifyCS.VerifyAnalyzerAsync(code);
+        await VerifyAsync(code, includeTestAdapter: true);
+        await VerifyAsync(code, includeTestAdapter: false);
     }
 }
