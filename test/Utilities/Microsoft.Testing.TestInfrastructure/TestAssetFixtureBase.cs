8c025c4320032e6ccf1e802151f7082184ea2be5
Simon Cropp <simon.cropp@gmail.com>
2025-01-08 10:34:31 +1100

    missing temp dir cleanup in TestAssetFixtureBase

--- test/Utilities/Microsoft.Testing.TestInfrastructure/TestAssetFixtureBase.cs
+++ test/Utilities/Microsoft.Testing.TestInfrastructure/TestAssetFixtureBase.cs
@@ -63,6 +63,8 @@ public abstract class TestAssetFixtureBase : ITestAssetFixture
 
             _disposedValue = true;
         }
+
+        _nugetGlobalPackagesDirectory.Dispose();
     }
 
     public void Dispose()
