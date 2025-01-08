cbb45b8678b865cad73635c11c94fe8e1b2a3309
Simon Cropp <simon.cropp@gmail.com>
2025-01-08 12:05:51 +1100

    remove redundant finalArguments.Clear

--- src/Platform/Microsoft.Testing.Extensions.Retry/RetryOrchestrator.cs
+++ src/Platform/Microsoft.Testing.Extensions.Retry/RetryOrchestrator.cs
@@ -275,7 +275,6 @@ internal sealed class RetryOrchestrator : ITestHostOrchestrator, IOutputDeviceDa
                     }
                 }
 
-                finalArguments.Clear();
                 lastListOfFailedId = retryFailedTestsPipeServer.FailedUID?.ToArray();
                 continue;
             }
