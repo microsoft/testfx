887175d1e2504f7bffae21b56e213bed3dd98284
Youssef Victor <youssefvictor00@gmail.com>
2025-01-09 14:31:52 +0100

    Implement codefix for 'Are[Not]Same' usage with value types (#4584)

--- test/UnitTests/MSTest.Analyzers.UnitTests/AvoidAssertAreSameWithValueTypesAnalyzerTests.cs
+++ test/UnitTests/MSTest.Analyzers.UnitTests/AvoidAssertAreSameWithValueTypesAnalyzerTests.cs
@@ -3,7 +3,7 @@
 
 using VerifyCS = MSTest.Analyzers.Test.CSharpCodeFixVerifier<
     MSTest.Analyzers.AvoidAssertAreSameWithValueTypesAnalyzer,
-    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+    MSTest.Analyzers.AvoidAssertAreSameWithValueTypesFixer>;
 
 namespace MSTest.Analyzers.UnitTests;
 
@@ -55,7 +55,49 @@ public sealed class AvoidAssertAreSameWithValueTypesAnalyzerTests
             }
             """;
 
-        await VerifyCS.VerifyCodeFixAsync(code, code);
+        string fixedCode = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+            using System.Collections.Generic;
+
+            [TestClass]
+            public class MyTestClass
+            {
+                [TestMethod]
+                public void TestMethod()
+                {
+                    // Both are value types
+                    Assert.AreEqual(0, 0);
+                    Assert.AreEqual(0, 0, "Message");
+                    Assert.AreEqual(0, 0, "Message {0}", "Arg");
+                    Assert.AreEqual(message: "Message", expected: 0, actual: 0);
+
+                    Assert.AreEqual<object>(0, 0);
+                    Assert.AreEqual<object>(0, 0, "Message");
+                    Assert.AreEqual<object>(0, 0, "Message {0}", "Arg");
+                    Assert.AreEqual<object>(message: "Message", expected: 0, actual: 0);
+
+                    // Expected is value type. This is always-failing assert.
+                    Assert.AreEqual<object>("0", 0);
+                    Assert.AreEqual<object>("0", 0, "Message");
+                    Assert.AreEqual<object>("0", 0, "Message {0}", "Arg");
+                    Assert.AreEqual<object>(message: "Message", expected: "0", actual: 0);
+
+                    // Actual is value type. This is always-failing assert.
+                    Assert.AreEqual<object>(0, "0");
+                    Assert.AreEqual<object>(0, "0", "Message");
+                    Assert.AreEqual<object>(0, "0", "Message {0}", "Arg");
+                    Assert.AreEqual<object>(message: "Message", expected: 0, actual: "0");
+
+                    // Both are reference types. No diagnostic.
+                    Assert.AreSame("0", "0");
+                    Assert.AreSame("0", "0", "Message");
+                    Assert.AreSame("0", "0", "Message {0}", "Arg");
+                    Assert.AreSame(message: "Message", expected: "0", actual: "0");
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyCodeFixAsync(code, fixedCode);
     }
 
     [TestMethod]
@@ -103,6 +145,48 @@ public sealed class AvoidAssertAreSameWithValueTypesAnalyzerTests
             }
             """;
 
-        await VerifyCS.VerifyCodeFixAsync(code, code);
+        string fixedCode = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+            using System.Collections.Generic;
+
+            [TestClass]
+            public class MyTestClass
+            {
+                [TestMethod]
+                public void TestMethod()
+                {
+                    // Both are value types
+                    Assert.AreNotEqual(0, 1);
+                    Assert.AreNotEqual(0, 1, "Message");
+                    Assert.AreNotEqual(0, 1, "Message {0}", "Arg");
+                    Assert.AreNotEqual(message: "Message", notExpected: 0, actual: 1);
+
+                    Assert.AreNotEqual<object>(0, 1);
+                    Assert.AreNotEqual<object>(0, 1, "Message");
+                    Assert.AreNotEqual<object>(0, 1, "Message {0}", "Arg");
+                    Assert.AreNotEqual<object>(message: "Message", notExpected: 0, actual: 1);
+
+                    // Expected is value type. This is always-failing assert.
+                    Assert.AreNotEqual<object>("0", 1);
+                    Assert.AreNotEqual<object>("0", 1, "Message");
+                    Assert.AreNotEqual<object>("0", 1, "Message {0}", "Arg");
+                    Assert.AreNotEqual<object>(message: "Message", notExpected: "0", actual: 1);
+
+                    // Actual is value type. This is always-failing assert.
+                    Assert.AreNotEqual<object>(0, "1");
+                    Assert.AreNotEqual<object>(0, "1", "Message");
+                    Assert.AreNotEqual<object>(0, "1", "Message {0}", "Arg");
+                    Assert.AreNotEqual<object>(message: "Message", notExpected: 0, actual: "1");
+
+                    // Both are reference types. No diagnostic.
+                    Assert.AreNotSame("0", "1");
+                    Assert.AreNotSame("0", "1", "Message");
+                    Assert.AreNotSame("0", "1", "Message {0}", "Arg");
+                    Assert.AreNotSame(message: "Message", notExpected: "0", actual: "1");
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyCodeFixAsync(code, fixedCode);
     }
 }
