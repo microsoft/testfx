3081df6712372618ca7bc8f348cfc6528a2dc0e6
Youssef1313 <youssefvictor00@gmail.com>
2025-01-17 06:27:58 +0100

    Remove unused using

--- test/UnitTests/MSTestAdapter.UnitTests/Execution/UnitTestResultTest.cs
+++ test/UnitTests/MSTestAdapter.UnitTests/Execution/UnitTestResultTest.cs
@@ -1,16 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter;
 using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.ObjectModel;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
-
-using Moq;
 
 using TestFramework.ForTestingMSTest;
-
-using UnitTestOutcome = Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.ObjectModel.UnitTestOutcome;
 
 namespace Microsoft.VisualStudio.TestPlatform.MSTestAdapter.UnitTests.Execution;
 
