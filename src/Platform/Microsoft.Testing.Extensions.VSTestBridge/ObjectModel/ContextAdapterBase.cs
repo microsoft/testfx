0aa1d688fb8f6219cbbdcac31e047214642bf698
Youssef1313 <youssefvictor00@gmail.com>
2025-02-05 14:13:11 +0100

    Better filter support in VSTestBridge

--- src/Platform/Microsoft.Testing.Extensions.VSTestBridge/ObjectModel/ContextAdapterBase.cs
+++ src/Platform/Microsoft.Testing.Extensions.VSTestBridge/ObjectModel/ContextAdapterBase.cs
@@ -4,6 +4,8 @@
 using Microsoft.Testing.Extensions.VSTestBridge.CommandLine;
 using Microsoft.Testing.Platform;
 using Microsoft.Testing.Platform.CommandLine;
+using Microsoft.Testing.Platform.Extensions.Messages;
+using Microsoft.Testing.Platform.Requests;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 
@@ -11,17 +13,27 @@ namespace Microsoft.Testing.Extensions.VSTestBridge.ObjectModel;
 
 internal abstract class ContextAdapterBase
 {
-    protected ContextAdapterBase(ICommandLineOptions commandLineOptions)
+    protected ContextAdapterBase(ICommandLineOptions commandLineOptions, IRunSettings runSettings, ITestExecutionFilter filter)
     {
+        RunSettings = runSettings;
+
+        RoslynDebug.Assert(runSettings.SettingsXml is not null);
+
+        string? filterFromRunsettings = XElement.Parse(runSettings.SettingsXml).Descendants("TestCaseFilter").SingleOrDefault()?.Value;
+        string? filterFromCommandLineOption = null;
         if (commandLineOptions.TryGetOptionArgumentList(
             TestCaseFilterCommandLineOptionsProvider.TestCaseFilterOptionName,
             out string[]? filterExpressions)
             && filterExpressions is not null
             && filterExpressions.Length == 1)
         {
-            FilterExpressionWrapper = new(filterExpressions[0]);
+            filterFromCommandLineOption = filterExpressions[0];
         }
+
+        HandleFilter(filter, filterFromRunsettings, filterFromCommandLineOption);
     }
+
+    public IRunSettings? RunSettings { get; }
 
     protected FilterExpressionWrapper? FilterExpressionWrapper { get; set; }
 
@@ -60,5 +72,118 @@ internal abstract class ContextAdapterBase
         }
 
         return adapterSpecificTestCaseFilter;
+    }
+
+    private void HandleFilter(ITestExecutionFilter? filter, string? filterFromRunsettings, string? filterFromCommandLineOption)
+    {
+        // No filters at all, we can return immediately as there is nothing to do.
+        if (filter is null or NopFilter && filterFromRunsettings is null && filterFromCommandLineOption is null)
+        {
+            return;
+        }
+
+        var filterBuilder = new StringBuilder();
+
+        AppendFilter(filterFromRunsettings, filterBuilder);
+        AppendFilter(filterFromCommandLineOption, filterBuilder);
+
+        // TODO: Handle TreeNodeFilter. Note that this is unlikely to be handled here.
+        // This code path tries to construct a VSTest compatible filter.
+        // This is probably not possible for TreeNodeFilter.
+        switch (filter)
+        {
+            case TestNodeUidListFilter testNodeUidListFilter:
+                StartFilter(filterBuilder);
+                BuildFilter(testNodeUidListFilter.TestNodeUids, filterBuilder);
+                EndFilter(filterBuilder);
+
+                break;
+
+            default:
+                break;
+        }
+
+        if (filterBuilder.Length > 0)
+        {
+            FilterExpressionWrapper = new(filterBuilder.ToString());
+        }
+
+        static void AppendFilter(string? filter, StringBuilder builder)
+        {
+            if (filter is null)
+            {
+                return;
+            }
+
+            StartFilter(builder);
+            builder.Append(filter);
+            EndFilter(builder);
+        }
+
+        static void StartFilter(StringBuilder builder)
+        {
+            if (builder.Length > 0)
+            {
+                builder.Append(" & (");
+            }
+            else
+            {
+                builder.Append('(');
+            }
+        }
+
+        static void EndFilter(StringBuilder builder)
+            => builder.Append(')');
+    }
+
+    // We use heuristic to understand if the filter should be a TestCaseId or FullyQualifiedName.
+    // We know that in VSTest TestCaseId is a GUID and FullyQualifiedName is a string.
+    private static void BuildFilter(TestNodeUid[] testNodesUid, StringBuilder filter)
+    {
+        for (int i = 0; i < testNodesUid.Length; i++)
+        {
+            if (Guid.TryParse(testNodesUid[i].Value, out Guid guid))
+            {
+                filter.Append("Id=");
+                filter.Append(guid.ToString());
+            }
+            else
+            {
+                TestNodeUid currentTestNodeUid = testNodesUid[i];
+                filter.Append("FullyQualifiedName=");
+                for (int k = 0; k < currentTestNodeUid.Value.Length; k++)
+                {
+                    char currentChar = currentTestNodeUid.Value[k];
+                    switch (currentChar)
+                    {
+                        case '\\':
+                        case '(':
+                        case ')':
+                        case '&':
+                        case '|':
+                        case '=':
+                        case '!':
+                        case '~':
+                            // If the symbol is not escaped, add an escape character.
+                            if (i - 1 < 0 || currentTestNodeUid.Value[k - 1] != '\\')
+                            {
+                                filter.Append('\\');
+                            }
+
+                            filter.Append(currentChar);
+                            break;
+
+                        default:
+                            filter.Append(currentChar);
+                            break;
+                    }
+                }
+            }
+
+            if (i != testNodesUid.Length - 1)
+            {
+                filter.Append('|');
+            }
+        }
     }
 }
