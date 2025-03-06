fe77fea1883b42199bd05bf17c51a7d5c5a083b1
Youssef Victor <youssefvictor00@gmail.com>
2025-03-06 13:41:07 +0100

    Add missing paren

--- test/Utilities/Microsoft.Testing.TestInfrastructure/TempDirectory.cs
+++ test/Utilities/Microsoft.Testing.TestInfrastructure/TempDirectory.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Microsoft.Testing.Platform.Helpers;
@@ -180,7 +180,7 @@ public class TempDirectory : IDisposable
         File.WriteAllText(dotnetConfig, """
             [dotnet.test:runner]
             name= "VSTest"
-            """;
+            """);
 
         string directoryBuildProps = System.IO.Path.Combine(directoryPath, "Directory.Build.props");
         File.WriteAllText(directoryBuildProps, $"""
