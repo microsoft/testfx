187452c9b0474166f0b21c6258d804d00e22b055
Jakub Jareš <me@jakubjares.com>
2025-04-24 16:46:55 +0200

    Basic unit test

---
+++ test/UnitTests/Microsoft.Testing.Extensions.UnitTests/AzureDevOpsTests.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Testing.Extensions.AzureDevOps;
+using Microsoft.Testing.Platform.Helpers;
+
+namespace Microsoft.Testing.Extensions.UnitTests;
+
+[TestClass]
+public sealed class AzureDevOpsTests
+{
+    [TestMethod]
+    public async Task ReportsTheFirstExistingFileInStackTraceWithTheRightLineNumberAndEscaping()
+    {
+        Exception error;
+        try
+        {
+            throw new Exception("this is an error\nwith\rnewline");
+        }
+        catch (Exception ex)
+        {
+            error = ex;
+        }
+
+        string? text = AzureDevOpsReporter.GetErrorText(null, error, "severity", new SystemFileSystem());
+        Assert.AreEqual("##vso[task.logissue type=severity;sourcepath=test/UnitTests/Microsoft.Testing.Extensions.UnitTests/AzureDevOpsTests.cs;linenumber=18;columnnumber=1]this is an error%0Awith%0Dnewline", text);
+    }
+}
