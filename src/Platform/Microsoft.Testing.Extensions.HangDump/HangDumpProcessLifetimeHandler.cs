79d2006d4ba9e6e3e7d4347a6852d20eb23c2cdf
Youssef Victor <youssefvictor00@gmail.com>
2025-04-29 05:50:08 +0200

    Fix formatting

--- src/Platform/Microsoft.Testing.Extensions.HangDump/HangDumpProcessLifetimeHandler.cs
+++ src/Platform/Microsoft.Testing.Extensions.HangDump/HangDumpProcessLifetimeHandler.cs
@@ -391,7 +391,7 @@ internal sealed class HangDumpProcessLifetimeHandler : ITestHostProcessLifetimeH
                 "full" => DumpType.Full,
                 _ => throw ApplicationStateGuard.Unreachable(),
             };
-    
+
             // Wrap the dump path into "" when it has space in it, this is a workaround for this runtime issue: https://github.com/dotnet/diagnostics/issues/5020
             // It only affects windows. Otherwise the dump creation fails with: [createdump] The pid argument is no longer supported
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && finalDumpFileName.Contains(' '))
@@ -408,7 +408,7 @@ internal sealed class HangDumpProcessLifetimeHandler : ITestHostProcessLifetimeH
                 "full" => MiniDumpWriteDump.MiniDumpTypeOption.Full,
                 _ => throw ApplicationStateGuard.Unreachable(),
             };
-    
+
             MiniDumpWriteDump.CollectDumpUsingMiniDumpWriteDump(_testHostProcessInformation.PID, finalDumpFileName, miniDumpTypeOption);
 #endif
 
