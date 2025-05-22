0ae4163da1c6f13977d2c0326b7410742ec450c9
Youssef1313 <youssefvictor00@gmail.com>
2025-05-22 09:00:56 +0200

    EC

--- src/Adapter/MSTest.TestAdapter/Helpers/ExecutionContextHelpers.cs
+++ src/Adapter/MSTest.TestAdapter/Helpers/ExecutionContextHelpers.cs
@@ -13,12 +13,6 @@ internal static class ExecutionContextHelpers
         }
         else
         {
-            // CreateCopy doesn't do anything on .NET Core as ExecutionContexts are immutable.
-            // But it's important on .NET Framework.
-            // On .NET Framework, ExecutionContext.Run cannot be called twice with the same ExecutionContext.
-            // Otherwise, it will throw InvalidOperationException with message:
-            // Cannot apply a context that has been marshaled across AppDomains, that was not acquired through a Capture operation or that has already been the argument to a Set call.
-            executionContext = executionContext.CreateCopy();
             ExecutionContext.Run(executionContext, static action => ((Action)action!).Invoke(), action);
         }
     }
