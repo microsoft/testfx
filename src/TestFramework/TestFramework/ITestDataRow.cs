6151cd2470eead5d39baf74f30862436474916f6
Youssef1313 <youssefvictor00@gmail.com>
2025-05-22 14:01:59 +0200

    Progress. Too many errors still

--- src/TestFramework/TestFramework/ITestDataRow.cs
+++ src/TestFramework/TestFramework/ITestDataRow.cs
@@ -3,11 +3,24 @@
 
 namespace Microsoft.VisualStudio.TestTools.UnitTesting;
 
-internal interface ITestDataRow
+/// <summary>
+/// Special interface that, when returned from parameterized tests, is recognized specially.
+/// </summary>
+public interface ITestDataRow
 {
+    /// <summary>
+    /// Gets the Value to pass to the test method.
+    /// It can be a tuple to handle the case where the parameterized test method has multiple parameters.
+    /// </summary>
     object? Value { get; }
 
+    /// <summary>
+    /// Gets the ignore message. If non-null, the test case is considered ignored.
+    /// </summary>
     string? IgnoreMessage { get; }
 
+    /// <summary>
+    /// Gets the display name for this specific test case. If null, the display name is calculated normally.
+    /// </summary>
     string? DisplayName { get; }
 }
