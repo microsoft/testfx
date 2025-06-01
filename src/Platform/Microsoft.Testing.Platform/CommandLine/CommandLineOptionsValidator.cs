e1369e785634b6748590845e1f3fd869c6b60a18
Youssef Victor <youssefvictor00@gmail.com>
2025-06-01 13:58:15 +0200

    Update CommandLineOptionsValidator.cs

--- src/Platform/Microsoft.Testing.Platform/CommandLine/CommandLineOptionsValidator.cs
+++ src/Platform/Microsoft.Testing.Platform/CommandLine/CommandLineOptionsValidator.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
@@ -318,7 +317,7 @@ internal static class CommandLineOptionsValidator
         while (end > 0)
         {
             char c = result[end - 1];
-            if (c != '\r' && c != '\n')
+            if (c is not ('\r' or '\n'))
             {
                 break;
             }
