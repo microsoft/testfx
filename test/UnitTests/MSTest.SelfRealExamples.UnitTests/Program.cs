1256ba85eaec251f6828d56185b3d05b4ae5ac46
Youssef1313 <youssefvictor00@gmail.com>
2025-06-04 17:12:54 +0200

    Add test

---
+++ test/UnitTests/MSTest.SelfRealExamples.UnitTests/Program.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Testing.Extensions;
+using Microsoft.Testing.Platform.Builder;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+ITestApplicationBuilder testApplicationBuilder = await TestApplication.CreateBuilderAsync(args);
+
+testApplicationBuilder.AddMSTest(() => [Assembly.GetEntryAssembly()!]);
+testApplicationBuilder.AddTrxReportProvider();
+testApplicationBuilder.AddAppInsightsTelemetryProvider();
+testApplicationBuilder.AddCodeCoverageProvider();
+testApplicationBuilder.AddAzureDevOpsProvider();
+using ITestApplication testApplication = await testApplicationBuilder.BuildAsync();
+return await testApplication.RunAsync();
