4947abf2ba0ac8928a3e2b6f2d59ff4c1643634f
Youssef1313 <youssefvictor00@gmail.com>
2025-06-07 09:46:30 +0200

    Error if framework/adapter versions are mismatched at runtime

--- src/Adapter/MSTest.TestAdapter/VSTestAdapter/MSTestExecutor.cs
+++ src/Adapter/MSTest.TestAdapter/VSTestAdapter/MSTestExecutor.cs
@@ -6,6 +6,7 @@ using Microsoft.VisualStudio.TestPlatform.MSTestAdapter.PlatformServices.Interfa
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter;
 
@@ -46,6 +47,20 @@ public class MSTestExecutor : ITestExecutor
     /// Gets or sets the ms test execution manager.
     /// </summary>
     public TestExecutionManager TestExecutionManager { get; protected set; }
+
+#pragma warning disable CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+    [ModuleInitializer]
+#pragma warning restore CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+    internal static void EnsureAdapterAndFrameworkVersions()
+    {
+        string? adapterVersion = typeof(MSTestExecutor).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+        string? frameworkVersion = typeof(TestMethodAttribute).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+        if (adapterVersion is not null && frameworkVersion is not null
+            && adapterVersion != frameworkVersion)
+        {
+            throw new InvalidOperationException($"MSTest.TestAdapter and MSTest.TestFramework must have the same version. Found MSTest.TestAdapter version '{adapterVersion}' and MSTest.TestFramework version {frameworkVersion}.");
+        }
+    }
 
     /// <summary>
     /// Runs the tests.
