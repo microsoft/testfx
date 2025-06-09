7711fe1d82869ecc6feb4dabc870e416fff1974b
Amaury Levé <amauryleve@microsoft.com>
2025-06-09 16:28:32 +0200

    Add type forwarding

--- src/Adapter/MSTest.TestAdapter/Properties/AssemblyInfo.cs
+++ src/Adapter/MSTest.TestAdapter/Properties/AssemblyInfo.cs
@@ -3,5 +3,22 @@
 
 using Microsoft.VisualStudio.TestPlatform;
 using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter;
+using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.Execution;
+using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.Extensions;
+using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.ObjectModel;
 
 [assembly: TestExtensionTypes(typeof(MSTestDiscoverer), typeof(MSTestExecutor))]
+
+[assembly: TypeForwardedTo(typeof(LogMessageListener))]
+[assembly: TypeForwardedTo(typeof(MSTestSettings))]
+[assembly: TypeForwardedTo(typeof(RunConfigurationSettings))]
+[assembly: TypeForwardedTo(typeof(TestAssemblyInfo))]
+[assembly: TypeForwardedTo(typeof(TestClassInfo))]
+[assembly: TypeForwardedTo(typeof(TestExecutionManager))]
+[assembly: TypeForwardedTo(typeof(TestMethod))]
+[assembly: TypeForwardedTo(typeof(TestMethodInfo))]
+[assembly: TypeForwardedTo(typeof(TestResultExtensions))]
+[assembly: TypeForwardedTo(typeof(TestRunCancellationToken))]
+[assembly: TypeForwardedTo(typeof(UnitTestOutcome))]
+[assembly: TypeForwardedTo(typeof(UnitTestOutcomeExtensions))]
+[assembly: TypeForwardedTo(typeof(UnitTestResult))]
