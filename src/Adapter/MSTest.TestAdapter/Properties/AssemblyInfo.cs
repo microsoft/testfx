e6c37addf264b1aae5103158ccd754ae13000a9e
Amaury Levé <amauryleve@microsoft.com>
2025-06-10 13:09:21 +0200

    Disable RS0016 on type forwarding

--- src/Adapter/MSTest.TestAdapter/Properties/AssemblyInfo.cs
+++ src/Adapter/MSTest.TestAdapter/Properties/AssemblyInfo.cs
@@ -9,6 +9,7 @@ using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.ObjectModel;
 
 [assembly: TestExtensionTypes(typeof(MSTestDiscoverer), typeof(MSTestExecutor))]
 
+#pragma warning disable RS0016 // Add public types and members to the declared API (type forwarding is not public API)
 [assembly: TypeForwardedTo(typeof(LogMessageListener))]
 [assembly: TypeForwardedTo(typeof(MSTestSettings))]
 [assembly: TypeForwardedTo(typeof(RunConfigurationSettings))]
@@ -22,3 +23,4 @@ using Microsoft.VisualStudio.TestPlatform.MSTest.TestAdapter.ObjectModel;
 [assembly: TypeForwardedTo(typeof(UnitTestOutcome))]
 [assembly: TypeForwardedTo(typeof(UnitTestOutcomeExtensions))]
 [assembly: TypeForwardedTo(typeof(UnitTestResult))]
+#pragma warning restore RS0016
