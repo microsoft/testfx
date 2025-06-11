1377c41d974b8520f725e127643a5eb4674e3eae
Youssef1313 <youssefvictor00@gmail.com>
2025-06-11 18:49:43 +0200

    Fix tests

--- test/UnitTests/MSTestAdapter.PlatformServices.UnitTests/TestableImplementations/TestablePlatformServiceProvider.cs
+++ test/UnitTests/MSTestAdapter.PlatformServices.UnitTests/TestableImplementations/TestablePlatformServiceProvider.cs
@@ -71,7 +71,7 @@ internal class TestablePlatformServiceProvider : IPlatformServiceProvider
 
     public ITestContext GetTestContext(ITestMethod testMethod, StringWriter writer, IDictionary<string, object?> properties, IMessageLogger messageLogger, UnitTestOutcome outcome)
     {
-        var testContextImpl = new TestContextImplementation(testMethod, writer, properties, messageLogger);
+        var testContextImpl = new TestContextImplementation(testMethod, writer, properties, messageLogger, null);
         testContextImpl.SetOutcome(outcome);
         return testContextImpl;
     }
