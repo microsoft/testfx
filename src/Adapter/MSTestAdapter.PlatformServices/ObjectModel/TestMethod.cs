7b8b50520e5281c9724f807cb77f7e269d7fbe1f
Youssef1313 <youssefvictor00@gmail.com>
2025-06-16 07:58:51 +0200

    app domains........

--- src/Adapter/MSTestAdapter.PlatformServices/ObjectModel/TestMethod.cs
+++ src/Adapter/MSTestAdapter.PlatformServices/ObjectModel/TestMethod.cs
@@ -147,6 +147,10 @@ public sealed class TestMethod : ITestMethod
     /// </summary>
     internal string?[]? SerializedData { get; set; }
 
+    // This holds user types that may not be serializable.
+    // If app domains are enabled, we have no choice other than losing the original data.
+    // In that case, we fallback to deserializing the SerializedData.
+    [field: NonSerialized]
     internal object?[]? ActualData { get; set; }
 
     /// <summary>
