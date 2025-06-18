e197bc0d47944976a21a38e520c57acbd18d76aa
Amaury Levé <amauryleve@microsoft.com>
2025-06-18 10:12:55 +0200

    Deprecates `That` property and suggest `Instance` instead

--- src/TestFramework/TestFramework/FrameworkConstants.cs
+++ src/TestFramework/TestFramework/FrameworkConstants.cs
@@ -9,4 +9,5 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting;
 internal static class FrameworkConstants
 {
     internal const string PublicTypeObsoleteMessage = "We will remove or hide this type starting with v4. If you are using this type, reach out to our team on https://github.com/microsoft/testfx.";
+    internal const string ThatPropertyObsoleteMessage = "The 'That' property is obsolete and will be removed in a future version. Use the 'Instance' property instead.";
 }
