107381a70ccaa306451306d1542947352b4bf650
Youssef1313 <youssefvictor00@gmail.com>
2025-06-20 23:46:01 +0200

    Fix

--- test/UnitTests/MSTestAdapter.PlatformServices.UnitTests/Services/MSTestAdapterSettingsTests.cs
+++ test/UnitTests/MSTestAdapter.PlatformServices.UnitTests/Services/MSTestAdapterSettingsTests.cs
@@ -371,7 +371,7 @@ public class MSTestAdapterSettingsTests : TestContainer
     #endregion
 }
 
-public class TestableMSTestAdapterSettings : MSTestAdapterSettings
+internal class TestableMSTestAdapterSettings : MSTestAdapterSettings
 {
     public TestableMSTestAdapterSettings()
     {
