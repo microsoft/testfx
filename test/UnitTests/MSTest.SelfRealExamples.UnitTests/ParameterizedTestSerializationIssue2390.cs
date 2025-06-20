405b8b409baa0100f44ee067e1cee0c761b4f108
Youssef Victor <youssefvictor00@gmail.com>
2025-06-20 10:41:32 +0200

    Update ParameterizedTestSerializationIssue2390.cs

--- test/UnitTests/MSTest.SelfRealExamples.UnitTests/ParameterizedTestSerializationIssue2390.cs
+++ test/UnitTests/MSTest.SelfRealExamples.UnitTests/ParameterizedTestSerializationIssue2390.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 namespace MSTest.SelfRealExamples.UnitTests;
 
 // Test for https://github.com/microsoft/testfx/issues/2390
