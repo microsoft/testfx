1cf6b3b5061d8487a3c7894a9e31518eac1584cd
Youssef1313 <youssefvictor00@gmail.com>
2025-06-24 15:39:02 +0200

    Revert

--- samples/Playground/Tests.cs
+++ samples/Playground/Tests.cs
@@ -3,6 +3,7 @@
 
 #pragma warning disable VSTHRD200 // Use "Async" suffix for async methods
 
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [assembly: Parallelize(Scope = ExecutionScope.MethodLevel, Workers = 0)]
@@ -12,9 +13,20 @@ namespace Playground;
 [TestClass]
 public class TestClass
 {
+    private sealed class MyDynamicAttribute : Attribute, ITestDataSource
+    {
+        public IEnumerable<object?[]> GetData(MethodInfo methodInfo)
+        {
+            yield return [1, 2];
+            yield return [3, 4];
+        }
+
+        public string? GetDisplayName(MethodInfo methodInfo, object?[]? data) => null;
+    }
+
     [TestMethod]
-    [DynamicData(nameof(Data))]
-    public void Test3(int a, int b)
+    [MyDynamic]
+    public void Test1(int a, int b)
     {
     }
 
