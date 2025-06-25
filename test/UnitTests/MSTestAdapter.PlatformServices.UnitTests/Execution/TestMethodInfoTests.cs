d4a179d07a92eeb163c7a7e52fcae97bccfa88c1
Youssef1313 <youssefvictor00@gmail.com>
2025-06-25 11:23:23 +0200

    Fix

--- test/UnitTests/MSTestAdapter.PlatformServices.UnitTests/Execution/TestMethodInfoTests.cs
+++ test/UnitTests/MSTestAdapter.PlatformServices.UnitTests/Execution/TestMethodInfoTests.cs
@@ -331,11 +331,11 @@ public class TestMethodInfoTests : TestContainer
 
         Verify(exception is not null);
         Verify(exception.StackTraceInformation is not null);
-        // NOTE: On net8.0 and later, the first frame is  System.Reflection.RuntimeConstructorInfo.Invoke(BindingFlags invokeAttr, Binder binder, Object[] parameters, CultureInfo culture)
+        // NOTE: On net8.0 and later, the first frame is System.Reflection.MethodBaseInvoker.ThrowTargetParameterCountException()
         // So, we do a consistent Contains check to check that the stack trace is there.
         Verify(
             exception.StackTraceInformation.ErrorStackTrace.Contains(
-            "   TestMethodInfoInvokeShouldSetStackTraceInformationIfTestClassConstructorThrowsWithoutInnerException", StringComparison.Ordinal));
+            "   at System.Reflection.RuntimeConstructorInfo.Invoke(BindingFlags invokeAttr, Binder binder, Object[] parameters, CultureInfo culture)", StringComparison.Ordinal));
     }
 
     public async Task TestMethodInfoInvokeShouldSetResultFilesIfTestContextHasAttachments()
