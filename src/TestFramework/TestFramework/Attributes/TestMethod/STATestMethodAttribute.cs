9da262ec2af50213ee1dd78c0774b989911fd647
Youssef Victor <youssefvictor00@gmail.com>
2025-06-25 11:22:54 +0200

    Refactor STA attributes logic outside of adapter (#5856)

--- src/TestFramework/TestFramework/Attributes/TestMethod/STATestMethodAttribute.cs
+++ src/TestFramework/TestFramework/Attributes/TestMethod/STATestMethodAttribute.cs
@@ -9,11 +9,58 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting;
 [AttributeUsage(AttributeTargets.Method)]
 public class STATestMethodAttribute : TestMethodAttribute
 {
+    private readonly TestMethodAttribute? _testMethodAttribute;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="STATestMethodAttribute"/> class.
     /// </summary>
     public STATestMethodAttribute([CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = -1)
         : base(callerFilePath, callerLineNumber)
     {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="STATestMethodAttribute"/> class.
+    /// This constructor is intended to be called by <see cref="STATestClassAttribute"/> (or derived) to wrap an existing test method attribute.
+    /// It can also be called by a derived <see cref="STATestMethodAttribute"/> which will be called by a derived <see cref="STATestClassAttribute"/>.
+    /// </summary>
+    /// <param name="testMethodAttribute">The wrapped test method.</param>
+    public STATestMethodAttribute(TestMethodAttribute testMethodAttribute)
+        : base(testMethodAttribute.DeclaringFilePath, testMethodAttribute.DeclaringLineNumber ?? -1)
+        => _testMethodAttribute = testMethodAttribute;
+
+    /// <summary>
+    /// The core execution of STA test method, which happens on the STA thread.
+    /// </summary>
+    /// <param name="testMethod">The test method to execute.</param>
+    /// <returns>The test results of the test method.</returns>
+    protected virtual Task<TestResult[]> ExecuteCoreAsync(ITestMethod testMethod)
+        => _testMethodAttribute is null ? base.ExecuteAsync(testMethod) : _testMethodAttribute.ExecuteAsync(testMethod);
+
+    /// <inheritdoc />
+    public override Task<TestResult[]> ExecuteAsync(ITestMethod testMethod)
+    {
+        if (Thread.CurrentThread.GetApartmentState() == ApartmentState.STA)
+        {
+            return ExecuteCoreAsync(testMethod);
+        }
+
+#if !NETFRAMEWORK
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            // TODO: Throw?
+            return ExecuteCoreAsync(testMethod);
+        }
+#endif
+
+        TestResult[]? results = null;
+        var t = new Thread(() => results = ExecuteCoreAsync(testMethod).GetAwaiter().GetResult())
+        {
+            Name = "STATestMethodAttribute thread",
+        };
+        t.SetApartmentState(ApartmentState.STA);
+        t.Start();
+        t.Join();
+        return Task.FromResult(results!);
     }
 }
