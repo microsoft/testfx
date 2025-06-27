69f22460cc425f940a668e1cc7e1bc80728c8fc1
Youssef1313 <youssefvictor00@gmail.com>
2025-06-27 12:36:54 +0200

    Add test

---
+++ test/UnitTests/MSTest.SelfRealExamples.UnitTests/DynamicDataMethodArgumentsTests.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest.SelfRealExamples.UnitTests;
+
+[TestClass]
+public class DynamicDataMethodArgumentsTests
+{
+    [TestMethod]
+    [DynamicData(nameof(GetData), 4)]
+    public void Test3(int a)
+        => Assert.IsInRange(4, 6, a);
+
+    public static IEnumerable<int> GetData(int i)
+    {
+        yield return i++;
+        yield return i++;
+        yield return i++;
+    }
+}
