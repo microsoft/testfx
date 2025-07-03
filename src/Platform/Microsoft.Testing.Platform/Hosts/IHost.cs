3f020f2309402cb55c7ffcf225b5457eaacd56b9
Youssef Victor <youssefvictor00@gmail.com>
2025-07-03 08:38:26 +0200

    Apply suggestion from code review

--- src/Platform/Microsoft.Testing.Platform/Hosts/IHost.cs
+++ src/Platform/Microsoft.Testing.Platform/Hosts/IHost.cs
@@ -6,7 +6,7 @@ namespace Microsoft.Testing.Platform.Hosts;
 /// <summary>
 /// This represents a host (i.e, a process). It could be a "test host" (the process that actually runs the tests),
 /// a "test host controller" (runs out-of-process extensions like dump extensions which observes the test host), or
-/// a "test host orchestrator" (like Retry extension, which coordinates how test hosts should be run)
+/// a "test host orchestrator" (like Retry extension, which coordinates how test hosts should be run).
 /// </summary>
 internal interface IHost
 {
