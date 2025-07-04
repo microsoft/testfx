c78a63902b10a2b5410fde28b011cbd11176836a
Youssef1313 <youssefvictor00@gmail.com>
2025-07-04 09:12:24 +0200

    Fix more build errors

--- test/IntegrationTests/MSTest.IntegrationTests/TestCategoriesFromTestDataRowTests.cs
+++ test/IntegrationTests/MSTest.IntegrationTests/TestCategoriesFromTestDataRowTests.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Microsoft.MSTestV2.CLIAutomation;
@@ -10,7 +10,7 @@ public class TestCategoriesFromTestDataRowTests : CLITestBase
     private const string TestAssetName = "TestCategoriesFromTestDataRowProject";
 
     [TestMethod]
-    public async Task TestCategoriesFromTestDataRow_ShouldDiscoverTestsWithIntegrationCategory()
+    public void TestCategoriesFromTestDataRow_ShouldDiscoverTestsWithIntegrationCategory()
     {
         // Arrange
         string assemblyPath = GetAssetFullPath(TestAssetName);
@@ -20,13 +20,13 @@ public class TestCategoriesFromTestDataRowTests : CLITestBase
 
         // Assert - Should discover the test that has Integration category from TestDataRow
         Assert.AreEqual(1, testCases.Length, "Should discover exactly 1 test with Integration category");
-        
-        var integrationTest = testCases[0];
+
+        Microsoft.VisualStudio.TestPlatform.ObjectModel.TestCase integrationTest = testCases[0];
         Assert.IsTrue(integrationTest.DisplayName.Contains("Integration and Slow"), "Should be the test with Integration and Slow categories");
     }
 
     [TestMethod]
-    public async Task TestCategoriesFromTestDataRow_ShouldDiscoverTestsWithUnitCategory()
+    public void TestCategoriesFromTestDataRow_ShouldDiscoverTestsWithUnitCategory()
     {
         // Arrange
         string assemblyPath = GetAssetFullPath(TestAssetName);
@@ -36,30 +36,30 @@ public class TestCategoriesFromTestDataRowTests : CLITestBase
 
         // Assert - Should discover the test that has Unit category from TestDataRow
         Assert.AreEqual(1, testCases.Length, "Should discover exactly 1 test with Unit category");
-        
-        var unitTest = testCases[0];
+
+        Microsoft.VisualStudio.TestPlatform.ObjectModel.TestCase unitTest = testCases[0];
         Assert.IsTrue(unitTest.DisplayName.Contains("Unit and Fast"), "Should be the test with Unit and Fast categories");
     }
 
     [TestMethod]
-    public async Task TestCategoriesFromTestDataRow_ShouldCombineMethodAndDataCategories()
+    public void TestCategoriesFromTestDataRow_ShouldCombineMethodAndDataCategories()
     {
         // Arrange
         string assemblyPath = GetAssetFullPath(TestAssetName);
 
         // Act - Filter by "MethodLevel" category (method attribute)
         System.Collections.Immutable.ImmutableArray<Microsoft.VisualStudio.TestPlatform.ObjectModel.TestCase> methodLevelTests = DiscoverTests(assemblyPath, "TestCategory~MethodLevel");
-        
+
         // Act - Filter by "DataLevel" category (from TestDataRow)
         System.Collections.Immutable.ImmutableArray<Microsoft.VisualStudio.TestPlatform.ObjectModel.TestCase> dataLevelTests = DiscoverTests(assemblyPath, "TestCategory~DataLevel");
 
         // Assert - The same test should be found by both filters since categories are merged
         Assert.AreEqual(1, methodLevelTests.Length, "Should discover exactly 1 test with MethodLevel category");
         Assert.AreEqual(1, dataLevelTests.Length, "Should discover exactly 1 test with DataLevel category");
-        
-        var methodTest = methodLevelTests[0];
-        var dataTest = dataLevelTests[0];
-        
+
+        Microsoft.VisualStudio.TestPlatform.ObjectModel.TestCase methodTest = methodLevelTests[0];
+        Microsoft.VisualStudio.TestPlatform.ObjectModel.TestCase dataTest = dataLevelTests[0];
+
         Assert.AreEqual(methodTest.FullyQualifiedName, dataTest.FullyQualifiedName, "Both filters should find the same test");
         Assert.IsTrue(methodTest.DisplayName.Contains("method and data categories"), "Should be the test with combined categories");
     }
