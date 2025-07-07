bb767cfa31ce6de5ead6ed39198359d7472f597e
Youssef1313 <youssefvictor00@gmail.com>
2025-07-07 15:00:11 +0200

    Add test for array

--- test/UnitTests/MSTest.SelfRealExamples.UnitTests/DynamicDataMethodArgumentsTests.cs
+++ test/UnitTests/MSTest.SelfRealExamples.UnitTests/DynamicDataMethodArgumentsTests.cs
@@ -9,14 +9,28 @@ namespace MSTest.SelfRealExamples.UnitTests;
 public class DynamicDataMethodArgumentsTests
 {
     [TestMethod]
-    [DynamicData(nameof(GetData), 4)]
-    public void Test3(int a)
+    [DynamicData(nameof(GetData1), 4)]
+    public void TestMethod(int a)
         => Assert.IsInRange(4, 6, a);
 
-    public static IEnumerable<int> GetData(int i)
+    [TestMethod]
+    [DynamicData(nameof(GetData2), [new int[] { 4, 5, 6 }])]
+    public void TestMethod4(int x)
+    {
+        Assert.IsTrue(x is 5 or 7 or 9);
+    }
+
+    public static IEnumerable<int> GetData1(int i)
     {
         yield return i++;
         yield return i++;
         yield return i++;
+    }
+
+    public static IEnumerable<object[]> GetData2(int[] input)
+    {
+        yield return [1 + input[0]];
+        yield return [2 + input[1]];
+        yield return [3 + input[2]];
     }
 }
