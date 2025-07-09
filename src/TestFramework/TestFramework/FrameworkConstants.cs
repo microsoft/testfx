40ff27c01aade18d551359194c2cd09b32915465
Youssef Victor <youssefvictor00@gmail.com>
2025-07-09 20:50:15 +0200

    Delete InternalTestFailureException (#5956)

--- src/TestFramework/TestFramework/FrameworkConstants.cs
+++ src/TestFramework/TestFramework/FrameworkConstants.cs
@@ -8,7 +8,6 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting;
 /// </summary>
 internal static class FrameworkConstants
 {
-    internal const string PublicTypeObsoleteMessage = "We will remove or hide this type starting with v4. If you are using this type, reach out to our team on https://github.com/microsoft/testfx.";
     internal const string DoNotUseAssertEquals = "Assert.Equals should not be used for Assertions. Please use Assert.AreEqual & overloads instead.";
     internal const string DoNotUseAssertReferenceEquals = "Assert.ReferenceEquals should not be used for Assertions. Please use Assert.AreSame & overloads instead.";
     internal const string DoNotUseStringAssertEquals = "StringAssert.Equals should not be used for Assertions. Please use StringAssert methods or Assert.AreEqual & overloads instead.";
