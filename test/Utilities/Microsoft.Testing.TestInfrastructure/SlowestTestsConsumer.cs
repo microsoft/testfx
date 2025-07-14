50eb90bf3da3f0f6ef1bf52138eefbff34b37916
Youssef1313 <youssefvictor00@gmail.com>
2025-07-14 09:20:25 +0200

    Missing using

--- test/Utilities/Microsoft.Testing.TestInfrastructure/SlowestTestsConsumer.cs
+++ test/Utilities/Microsoft.Testing.TestInfrastructure/SlowestTestsConsumer.cs
@@ -3,6 +3,7 @@
 
 using Microsoft.Testing.Platform.Extensions.Messages;
 using Microsoft.Testing.Platform.Extensions.TestHost;
+using Microsoft.Testing.Platform.Services;
 using Microsoft.Testing.Platform.TestHost;
 
 namespace Microsoft.Testing.TestInfrastructure;
