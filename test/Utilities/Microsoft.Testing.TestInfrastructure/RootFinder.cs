60aecf2d710588ee5d8f2ae345bea8d268b770d1
Amaury Levé <amauryleve@microsoft.com>
2025-07-15 15:16:13 +0200

    Fix RootFinder to handle git worktrees

--- test/Utilities/Microsoft.Testing.TestInfrastructure/RootFinder.cs
+++ test/Utilities/Microsoft.Testing.TestInfrastructure/RootFinder.cs
@@ -3,22 +3,51 @@
 
 namespace Microsoft.Testing.TestInfrastructure;
 
-public static class RootFinder
+/// <summary>
+/// Provides functionality to locate the root directory of a Git repository.
+/// </summary>
+/// <remarks>The <see cref="RootFinder"/> class is used to find the root directory of a Git repository by
+/// searching for a ".git" directory or file starting from the application's base directory and moving up the directory
+/// hierarchy. This is useful for applications that need to determine the root of a project or repository.</remarks>
+#if ROOT_FINDER_PUBLIC
+public
+#else
+internal
+#endif
+    static class RootFinder
 {
+    private static string? s_root;
+
+    /// <summary>
+    /// Finds the root directory of a Git repository starting from the application's base directory.
+    /// </summary>
+    /// <remarks>This method searches for a ".git" directory or file in the application's base directory and
+    /// its parent directories. If a Git repository is found, the path to its root directory is returned. If no Git
+    /// repository is found, an <see cref="InvalidOperationException"/> is thrown.</remarks>
+    /// <returns>The path to the root directory of the Git repository, ending with a directory separator character.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if a Git repository is not found in the application's base directory or any of its parent directories.</exception>
     public static string Find()
     {
-        string path = AppContext.BaseDirectory;
-        string dir = path;
-        while (Directory.GetDirectoryRoot(dir) != dir)
+        if (s_root != null)
         {
-            if (Directory.Exists(Path.Combine(dir, ".git")))
+            return s_root;
+        }
+
+        string path = AppContext.BaseDirectory;
+        string currentDirectory = path;
+        string rootDriveDirectory = Directory.GetDirectoryRoot(currentDirectory);
+        while (rootDriveDirectory != currentDirectory)
+        {
+            string gitPath = Path.Combine(currentDirectory, ".git");
+
+            // When working with git worktrees, the .git is a file not a folder
+            if (Directory.Exists(gitPath) || File.Exists(gitPath))
             {
-                return dir;
+                s_root = currentDirectory + Path.DirectorySeparatorChar;
+                return s_root;
             }
-            else
-            {
-                dir = Directory.GetParent(dir)!.ToString();
-            }
+
+            currentDirectory = Directory.GetParent(currentDirectory)!.ToString();
         }
 
         throw new InvalidOperationException($"Could not find solution root, .git not found in {path} or any parent directory.");
