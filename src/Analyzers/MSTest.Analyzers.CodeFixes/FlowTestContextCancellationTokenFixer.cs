60a28a7b3d9ab6fe19f88b431fbab3bfd7099adc
Youssef Victor <youssefvictor00@gmail.com>
2025-07-22 16:56:39 +0200

    Add analyzer to flow TestContext.CTS.Token (#6126)

---
+++ src/Analyzers/MSTest.Analyzers.CodeFixes/FlowTestContextCancellationTokenFixer.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Immutable;
+using System.Composition;
+
+using Analyzer.Utilities;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Text;
+
+using MSTest.Analyzers.Helpers;
+
+namespace MSTest.Analyzers;
+
+/// <summary>
+/// Code fixer for <see cref="FlowTestContextCancellationTokenAnalyzer"/>.
+/// </summary>
+[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(FlowTestContextCancellationTokenFixer))]
+[Shared]
+public sealed class FlowTestContextCancellationTokenFixer : CodeFixProvider
+{
+    /// <inheritdoc />
+    public sealed override ImmutableArray<string> FixableDiagnosticIds { get; }
+        = ImmutableArray.Create(DiagnosticIds.FlowTestContextCancellationTokenRuleId);
+
+    /// <inheritdoc />
+    public override FixAllProvider GetFixAllProvider()
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+        => WellKnownFixAllProviders.BatchFixer;
+
+    /// <inheritdoc />
+    public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        SyntaxNode root = await context.Document.GetRequiredSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+        Diagnostic diagnostic = context.Diagnostics[0];
+        TextSpan diagnosticSpan = diagnostic.Location.SourceSpan;
+
+        // Find the invocation expression identified by the diagnostic
+        SyntaxNode node = root.FindNode(diagnosticSpan, getInnermostNodeForTie: true);
+        if (node is not InvocationExpressionSyntax invocationExpression)
+        {
+            return;
+        }
+
+        diagnostic.Properties.TryGetValue(FlowTestContextCancellationTokenAnalyzer.TestContextMemberNamePropertyKey, out string? testContextMemberName);
+
+        // Register a code action that will invoke the fix
+        context.RegisterCodeFix(
+            CodeAction.Create(
+                title: CodeFixResources.PassCancellationTokenFix,
+                createChangedDocument: c => AddCancellationTokenParameterAsync(context.Document, invocationExpression, testContextMemberName, c),
+                equivalenceKey: "AddTestContextCancellationToken"),
+            diagnostic);
+    }
+
+    private static async Task<Document> AddCancellationTokenParameterAsync(
+        Document document,
+        InvocationExpressionSyntax invocationExpression,
+        string? testContextMemberName,
+        CancellationToken cancellationToken)
+    {
+        DocumentEditor editor = await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(false);
+
+        // Create the TestContext.CancellationTokenSource.Token expression
+        MemberAccessExpressionSyntax testContextExpression = SyntaxFactory.MemberAccessExpression(
+            SyntaxKind.SimpleMemberAccessExpression,
+            SyntaxFactory.MemberAccessExpression(
+                SyntaxKind.SimpleMemberAccessExpression,
+                SyntaxFactory.IdentifierName(testContextMemberName ?? "testContext"),
+                SyntaxFactory.IdentifierName("CancellationTokenSource")),
+            SyntaxFactory.IdentifierName("Token"));
+
+        ArgumentListSyntax currentArguments = invocationExpression.ArgumentList;
+        SeparatedSyntaxList<ArgumentSyntax> newArguments = currentArguments.Arguments.Add(SyntaxFactory.Argument(testContextExpression));
+        InvocationExpressionSyntax newInvocation = invocationExpression.WithArgumentList(currentArguments.WithArguments(newArguments));
+        editor.ReplaceNode(invocationExpression, newInvocation);
+        return editor.GetChangedDocument();
+    }
+}
