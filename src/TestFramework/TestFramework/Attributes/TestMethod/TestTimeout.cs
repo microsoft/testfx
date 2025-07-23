9ea1a015ccb7ab7e4468d0b152bcb6bb42cea95f
Amaury Levé <amauryleve@microsoft.com>
2025-07-23 21:38:43 +0200

    Obsolete `TestTimeout`

--- src/TestFramework/TestFramework/Attributes/TestMethod/TestTimeout.cs
+++ src/TestFramework/TestFramework/Attributes/TestMethod/TestTimeout.cs
@@ -8,6 +8,11 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting;
 /// The type of the enumeration must match.
 /// </summary>
 [SuppressMessage("Microsoft.Design", "CA1008:EnumsShouldHaveZeroValue", Justification = "Compat reasons")]
+#if NET6_0_OR_GREATER
+[Obsolete("Use 'int.MaxValue' for infinite timeout instead. The enum will be dropped in v4.", error: false, DiagnosticId = "MSTESTOBS")]
+#else
+[Obsolete("Use 'int.MaxValue' for infinite timeout instead. The enum will be dropped in v4.", error: false)]
+#endif
 public enum TestTimeout
 {
     /// <summary>
