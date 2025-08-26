57c335fbbeeff729e0ac7a64be85c89f55f56cdc
Amaury Levé <amauryleve@microsoft.com>
2025-08-26 13:50:21 +0200

    Use AwesomeAssertions for framework UTs

--- test/UnitTests/TestFramework.UnitTests/TestResultTests.cs
+++ test/UnitTests/TestFramework.UnitTests/TestResultTests.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using AwesomeAssertions;
+
 using TestFramework.ForTestingMSTest;
 
 namespace Microsoft.VisualStudio.TestPlatform.TestFramework.UnitTests;
@@ -18,20 +20,20 @@ public sealed class TestResultTests : TestContainer
         };
 
         // We use GetType() == typeof(...) to do a strict type match.
-        Verify(testResult.TestFailureException.GetType() == typeof(InvalidOperationException));
-        Verify(testResult.TestFailureException.Message == "Failure1");
+        testResult.TestFailureException.Should().BeOfType<InvalidOperationException>();
+        testResult.TestFailureException.Message.Should().Be("Failure1");
 
         testResult.TestFailureException = new ArgumentException("Failure2");
         var aggregateException = (AggregateException)testResult.TestFailureException;
-        Verify(aggregateException.InnerExceptions.Count == 2);
-        Verify(aggregateException.InnerExceptions[0].Message == "Failure1");
-        Verify(aggregateException.InnerExceptions[1].Message == "Failure2");
+        aggregateException.InnerExceptions.Should().HaveCount(2);
+        aggregateException.InnerExceptions[0].Message.Should().Be("Failure1");
+        aggregateException.InnerExceptions[1].Message.Should().Be("Failure2");
 
         testResult.TestFailureException = new ArgumentException("Failure3");
         aggregateException = (AggregateException)testResult.TestFailureException;
-        Verify(aggregateException.InnerExceptions.Count == 3);
-        Verify(aggregateException.InnerExceptions[0].Message == "Failure1");
-        Verify(aggregateException.InnerExceptions[1].Message == "Failure2");
-        Verify(aggregateException.InnerExceptions[2].Message == "Failure3");
+        aggregateException.InnerExceptions.Should().HaveCount(3);
+        aggregateException.InnerExceptions[0].Message.Should().Be("Failure1");
+        aggregateException.InnerExceptions[1].Message.Should().Be("Failure2");
+        aggregateException.InnerExceptions[2].Message.Should().Be("Failure3");
     }
 }
