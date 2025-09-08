268bd403b62e46b09a4cdd509c161a7ef21ac57b
Youssef1313 <youssefvictor00@gmail.com>
2025-09-08 17:29:33 +0200

    Avoid long path issues for pipes

--- src/Platform/Microsoft.Testing.Platform/IPC/PipeNameDescription.cs
+++ src/Platform/Microsoft.Testing.Platform/IPC/PipeNameDescription.cs
@@ -3,32 +3,11 @@
 
 namespace Microsoft.Testing.Platform.IPC;
 
-internal sealed class PipeNameDescription(string name, bool isDirectory) : IDisposable
+internal sealed class PipeNameDescription(string name) : IDisposable
 {
-    private readonly bool _isDirectory = isDirectory;
-    private bool _disposed;
-
     public string Name { get; } = name;
 
     public void Dispose()
     {
-        if (_disposed)
-        {
-            return;
-        }
-
-        if (_isDirectory)
-        {
-            try
-            {
-                Directory.Delete(Path.GetDirectoryName(Name)!, true);
-            }
-            catch (IOException)
-            {
-                // This folder is created inside the temp directory and will be cleaned up eventually by the OS
-            }
-        }
-
-        _disposed = true;
     }
 }
