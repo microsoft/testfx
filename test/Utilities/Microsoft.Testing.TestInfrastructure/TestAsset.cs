c33dcb01fd430a05606123f6e9e42d1807f1ede1
Youssef Victor <youssefvictor00@gmail.com>
2025-10-06 12:19:12 +0200

    Update TestAsset.cs

--- test/Utilities/Microsoft.Testing.TestInfrastructure/TestAsset.cs
+++ test/Utilities/Microsoft.Testing.TestInfrastructure/TestAsset.cs
@@ -137,6 +137,8 @@ public class TestAsset : IDisposable
         <add key="dotnet-public" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-public/nuget/v3/index.json" />
         <!-- This feed is required for FSharp.Core until preview 1 or 2 is released -->
         <add key="dotnet10" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet10/nuget/v3/index.json" />
+        <!-- This feed is required for Fakes until Fakes 18.1.0 is published to nuget.org -->
+        <add key="test-tools" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/test-tools/nuget/v3/index.json" />
     </packageSources>
     <config>
         <add key="globalPackagesFolder" value=".packages" />
@@ -156,6 +158,9 @@ public class TestAsset : IDisposable
             <package pattern="*" />
         </packageSource>
         <packageSource key="dotnet10">
+            <package pattern="*" />
+        </packageSource>
+        <packageSource key="test-tools">
             <package pattern="*" />
         </packageSource>
     </packageSourceMapping>
