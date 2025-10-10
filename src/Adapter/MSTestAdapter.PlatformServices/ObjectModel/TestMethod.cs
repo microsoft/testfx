20e1314fd8ac5e292da5065060ae475fe857bf54
Youssef1313 <youssefvictor00@gmail.com>
2025-10-10 16:10:05 +0200

    Fix deployment regression

--- src/Adapter/MSTestAdapter.PlatformServices/ObjectModel/TestMethod.cs
+++ src/Adapter/MSTestAdapter.PlatformServices/ObjectModel/TestMethod.cs
@@ -165,4 +165,11 @@ internal sealed class TestMethod : ITestMethod
     internal string DisplayName { get; set; }
 
     internal TestMethod Clone() => (TestMethod)MemberwiseClone();
+
+    internal TestMethod CloneWithUpdatedSource(string source)
+    {
+        var clone = (TestMethod)MemberwiseClone();
+        AssemblyName = source;
+        return clone;
+    }
 }
