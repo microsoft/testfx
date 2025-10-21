059b84b98dc55714415b7cfbbeefa451b2dfe126
Amaury Levé <amauryleve@microsoft.com>
2025-10-21 22:22:22 +0200

    Updates

--- src/Platform/Microsoft.Testing.Platform/Helpers/ExtensionHelper.cs
+++ src/Platform/Microsoft.Testing.Platform/Helpers/ExtensionHelper.cs
@@ -7,12 +7,11 @@ namespace Microsoft.Testing.Platform.Helpers;
 
 internal static class ExtensionHelper
 {
-    public static IEnumerable<KeyValuePair<string, object?>> ToOTelTags(this IExtension extension)
-        => new KeyValuePair<string, object?>[]
-        {
+    public static KeyValuePair<string, object?>[] ToOTelTags(this IExtension extension)
+        => [
             new("Extension.UID", extension.Uid),
             new("Extension.Version", extension.Version),
             new("Extension.DisplayName", extension.DisplayName),
             new("Extension.Description", extension.Description),
-        };
+        ];
 }
