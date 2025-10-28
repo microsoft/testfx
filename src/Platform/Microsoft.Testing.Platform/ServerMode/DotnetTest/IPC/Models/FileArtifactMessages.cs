e0be6b88342792b68d2b6f7c2f051014c9163857
Amaury Levé <amauryleve@microsoft.com>
2025-10-28 10:59:55 +0100

    Use EasyNamedPipes for dotnet test protocol

--- src/Platform/Microsoft.Testing.Platform/ServerMode/DotnetTest/IPC/Models/FileArtifactMessages.cs
+++ src/Platform/Microsoft.Testing.Platform/ServerMode/DotnetTest/IPC/Models/FileArtifactMessages.cs
@@ -1,8 +1,21 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using EasyNamedPipes;
+
 namespace Microsoft.Testing.Platform.IPC.Models;
 
-internal sealed record FileArtifactMessage(string? FullPath, string? DisplayName, string? Description, string? TestUid, string? TestDisplayName, string? SessionUid);
+internal sealed record FileArtifactMessage(
+    [property: PipePropertyId(1)] string? FullPath,
+    [property: PipePropertyId(2)] string? DisplayName,
+    [property: PipePropertyId(3)] string? Description,
+    [property: PipePropertyId(4)] string? TestUid,
+    [property: PipePropertyId(5)] string? TestDisplayName,
+    [property: PipePropertyId(6)] string? SessionUid);
 
-internal sealed record FileArtifactMessages(string? ExecutionId, string? InstanceId, FileArtifactMessage[] FileArtifacts) : IRequest;
+[PipeSerializableMessage("DotNetTestProtocol", 7)]
+internal sealed record FileArtifactMessages(
+    [property: PipePropertyId(1)] string? ExecutionId,
+    [property: PipePropertyId(2)] string? InstanceId,
+    [property: PipePropertyId(3)] FileArtifactMessage[] FileArtifacts)
+    : IRequest;
--- src/Platform/Microsoft.Testing.Platform/ServerMode/DotnetTest/IPC/ObjectFieldIds.cs
+++ src/Platform/Microsoft.Testing.Platform/ServerMode/DotnetTest/IPC/ObjectFieldIds.cs
@@ -22,29 +22,12 @@ internal static class TestHostProcessPIDRequestFieldsId
     public const int MessagesSerializerId = 2;
 }
 
-internal static class CommandLineOptionMessagesFieldsId
-{
-    public const int MessagesSerializerId = 3;
-
-    public const ushort ModulePath = 1;
-    public const ushort CommandLineOptionMessageList = 2;
-}
-
 internal static class CommandLineOptionMessageFieldsId
 {
     public const ushort Name = 1;
     public const ushort Description = 2;
     public const ushort IsHidden = 3;
     public const ushort IsBuiltIn = 4;
-}
-
-internal static class DiscoveredTestMessagesFieldsId
-{
-    public const int MessagesSerializerId = 5;
-
-    public const ushort ExecutionId = 1;
-    public const ushort InstanceId = 2;
-    public const ushort DiscoveredTestMessageList = 3;
 }
 
 internal static class DiscoveredTestMessageFieldsId
@@ -58,85 +41,4 @@ internal static class DiscoveredTestMessageFieldsId
     public const ushort MethodName = 7;
     public const ushort Traits = 8;
     public const ushort ParameterTypeFullNames = 9;
-}
-
-internal static class TraitMessageFieldsId
-{
-    public const ushort Key = 1;
-    public const ushort Value = 2;
-}
-
-internal static class TestResultMessagesFieldsId
-{
-    public const int MessagesSerializerId = 6;
-
-    public const ushort ExecutionId = 1;
-    public const ushort InstanceId = 2;
-    public const ushort SuccessfulTestMessageList = 3;
-    public const ushort FailedTestMessageList = 4;
-}
-
-internal static class SuccessfulTestResultMessageFieldsId
-{
-    public const ushort Uid = 1;
-    public const ushort DisplayName = 2;
-    public const ushort State = 3;
-    public const ushort Duration = 4;
-    public const ushort Reason = 5;
-    public const ushort StandardOutput = 6;
-    public const ushort ErrorOutput = 7;
-    public const ushort SessionUid = 8;
-}
-
-internal static class FailedTestResultMessageFieldsId
-{
-    public const ushort Uid = 1;
-    public const ushort DisplayName = 2;
-    public const ushort State = 3;
-    public const ushort Duration = 4;
-    public const ushort Reason = 5;
-    public const ushort ExceptionMessageList = 6;
-    public const ushort StandardOutput = 7;
-    public const ushort ErrorOutput = 8;
-    public const ushort SessionUid = 9;
-}
-
-internal static class ExceptionMessageFieldsId
-{
-    public const ushort ErrorMessage = 1;
-    public const ushort ErrorType = 2;
-    public const ushort StackTrace = 3;
-}
-
-internal static class FileArtifactMessagesFieldsId
-{
-    public const int MessagesSerializerId = 7;
-
-    public const ushort ExecutionId = 1;
-    public const ushort InstanceId = 2;
-    public const ushort FileArtifactMessageList = 3;
-}
-
-internal static class FileArtifactMessageFieldsId
-{
-    public const ushort FullPath = 1;
-    public const ushort DisplayName = 2;
-    public const ushort Description = 3;
-    public const ushort TestUid = 4;
-    public const ushort TestDisplayName = 5;
-    public const ushort SessionUid = 6;
-}
-
-internal static class TestSessionEventFieldsId
-{
-    public const int MessagesSerializerId = 8;
-
-    public const ushort SessionType = 1;
-    public const ushort SessionUid = 2;
-    public const ushort ExecutionId = 3;
-}
-
-internal static class HandshakeMessageFieldsId
-{
-    public const int MessagesSerializerId = 9;
 }
