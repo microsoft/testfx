29b3d9f772326f29d3cf63bb7fa5f0d69e2495e4
Muiz Atolagbe <atolagbemuiz@gmail.com>
2025-11-12 22:35:15 +0000

    implementation for do not duplicate test method

---
+++ src/Analyzers/MSTest.Analyzers/DoNotDuplicateTestMethodAnalyzer.cs
@@ -0,0 +1,225 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Immutable;
+
+using Analyzer.Utilities.Extensions;
+
+using Microsoft.CodeAnalysis;
+
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using MSTest.Analyzers.Helpers;
+
+namespace MSTest.Analyzers;
+
+/// <summary>
+/// MSTEST0033: <inheritdoc cref="Resources.DoNotDuplicateTestMethodTitle"/>.
+/// </summary>
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+public sealed class DoNotDuplicateTestMethodAnalyzer : DiagnosticAnalyzer
+{
+    private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.DoNotDuplicateTestMethodTitle), Resources.ResourceManager, typeof(Resources));
+    // private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.DoNotDuplicateTestMethodDescription), Resources.ResourceManager, typeof(Resources));
+    private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.DoNotDuplicateTestMethodMessageFormat), Resources.ResourceManager, typeof(Resources));
+
+    internal static readonly DiagnosticDescriptor DoNotDuplicateTestMethodRule = DiagnosticDescriptorHelper.Create(
+        DiagnosticIds.DoNotDuplicateTestMethodRuleId,
+        Title,
+        MessageFormat,
+        null,
+        Category.Design,
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true);
+
+    /// <summary>
+    /// Gets the diagnostic descriptors supported by this analyzer.
+    /// </summary>
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(DoNotDuplicateTestMethodRule);
+
+    /// <summary>
+    /// Initializes the analyzer by registering actions to analyze named type symbols for duplicate test methods.
+    /// </summary>
+    /// <param name="context">The analysis context to register actions with.</param>
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+
+        context.RegisterCompilationStartAction(compilationContext =>
+        {
+            if (compilationContext.Compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.MicrosoftVisualStudioTestToolsUnitTestingTestClassAttribute, out INamedTypeSymbol? testClassAttributeSymbol)
+                && compilationContext.Compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.MicrosoftVisualStudioTestToolsUnitTestingTestMethodAttribute, out INamedTypeSymbol? testMethodAttributeSymbol))
+            {
+                compilationContext.RegisterSymbolAction(
+                    context => AnalyzeNamedTypeSymbol(context, testClassAttributeSymbol, testMethodAttributeSymbol),
+                    SymbolKind.NamedType);
+            }
+        });
+    }
+
+    private static void AnalyzeNamedTypeSymbol(SymbolAnalysisContext context, INamedTypeSymbol testClassAttributeSymbol, INamedTypeSymbol testMethodAttributeSymbol)
+    {
+        var namedType = (INamedTypeSymbol)context.Symbol;
+
+        // Check if the type is a test class
+        if (!namedType.GetAttributes().Any(attr => SymbolEqualityComparer.Default.Equals(attr.AttributeClass, testClassAttributeSymbol)))
+        {
+            return;
+        }
+
+        // Get all test methods in the class (including inherited ones)
+        var testMethods = namedType.GetMembers()
+            .OfType<IMethodSymbol>()
+            .Where(method => IsTestMethod(method, testMethodAttributeSymbol))
+            .ToList();
+
+        // Check for duplicate method names within the same class
+        var duplicatesByName = testMethods
+            .GroupBy(m => m.Name, StringComparer.Ordinal)
+            .Where(g => g.Count() > 1)
+            .ToList();
+
+        foreach (IGrouping<string, IMethodSymbol> duplicateGroup in duplicatesByName)
+        {
+            foreach (IMethodSymbol method in duplicateGroup.Skip(1)) // Report all but the first occurrence
+            {
+                IMethodSymbol firstMethod = duplicateGroup.First();
+                Location? location = method.Locations.FirstOrDefault();
+
+                if (location != null)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(
+                        DoNotDuplicateTestMethodRule,
+                        location,
+                        method.Name,
+                        namedType.Name));
+                }
+            }
+        }
+
+        // Check for duplicate method signatures (overloads)
+        var duplicatesBySignature = testMethods
+            .GroupBy(GetMethodSignature, MethodSignatureComparer.Instance)
+            .Where(g => g.Count() > 1)
+            .ToList();
+
+        foreach (IGrouping<MethodSignature, IMethodSymbol> duplicateGroup in duplicatesBySignature)
+        {
+            // Only report if we haven't already reported by name
+            // (to avoid duplicate diagnostics)
+            if (!duplicatesByName.Any(ng => ng.Key == duplicateGroup.Key.Name))
+            {
+                foreach (IMethodSymbol method in duplicateGroup.Skip(1))
+                {
+                    Location? location = method.Locations.FirstOrDefault();
+
+                    if (location != null)
+                    {
+                        context.ReportDiagnostic(Diagnostic.Create(
+                            DoNotDuplicateTestMethodRule,
+                            location,
+                            method.Name,
+                            namedType.Name));
+                    }
+                }
+            }
+        }
+    }
+
+    private static bool IsTestMethod(IMethodSymbol method, INamedTypeSymbol testMethodAttributeSymbol) =>
+     // Check if the method has [TestMethod] or a derived attribute
+     method.GetAttributes().Any(attr =>
+     {
+         if (attr.AttributeClass == null)
+         {
+             return false;
+         }
+
+         // Check if it's TestMethodAttribute or derives from it
+         INamedTypeSymbol? currentType = attr.AttributeClass;
+         while (currentType != null)
+         {
+             if (SymbolEqualityComparer.Default.Equals(currentType, testMethodAttributeSymbol))
+             {
+                 return true;
+             }
+
+             currentType = currentType.BaseType;
+         }
+
+         return false;
+     });
+
+    private static MethodSignature GetMethodSignature(IMethodSymbol method) =>
+        new(
+            method.Name,
+            method.Parameters.Select(p => p.Type).ToImmutableArray(),
+            method.TypeParameters.Length);
+
+    private sealed class MethodSignature
+    {
+        public string Name { get; }
+
+        public ImmutableArray<ITypeSymbol> ParameterTypes { get; }
+
+        public int TypeParameterCount { get; }
+
+        public MethodSignature(string name, ImmutableArray<ITypeSymbol> parameterTypes, int typeParameterCount)
+        {
+            Name = name;
+            ParameterTypes = parameterTypes;
+            TypeParameterCount = typeParameterCount;
+        }
+    }
+
+    private sealed class MethodSignatureComparer : IEqualityComparer<MethodSignature>
+    {
+        public static readonly MethodSignatureComparer Instance = new();
+
+        public bool Equals(MethodSignature? x, MethodSignature? y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+
+            if (x is null || y is null)
+            {
+                return false;
+            }
+
+            if (x.Name != y.Name || x.TypeParameterCount != y.TypeParameterCount)
+            {
+                return false;
+            }
+
+            if (x.ParameterTypes.Length != y.ParameterTypes.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < x.ParameterTypes.Length; i++)
+            {
+                if (!SymbolEqualityComparer.Default.Equals(x.ParameterTypes[i], y.ParameterTypes[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public int GetHashCode(MethodSignature obj)
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = (hash * 31) + (obj.Name?.GetHashCode() ?? 0);
+                hash = (hash * 31) + obj.TypeParameterCount.GetHashCode();
+                hash = (hash * 31) + obj.ParameterTypes.Length.GetHashCode();
+                return hash;
+            }
+        }
+    }
+}
---
+++ test/UnitTests/MSTest.Analyzers.UnitTests/DoNotDuplicateTestMethodAnalyzerTests.cs
@@ -0,0 +1,398 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using VerifyCS = MSTest.Analyzers.Test.CSharpCodeFixVerifier<
+    MSTest.Analyzers.DoNotNegateBooleanAssertionAnalyzer,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+
+namespace MSTest.Analyzers.Test;
+
+[TestClass]
+public sealed class DoNotDuplicateTestMethodAnalyzerTests
+{
+    [TestMethod]
+    public async Task WhenTestClassHasNoDuplicateMethods_NoDiagnostic()
+    {
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class TestClass1
+            {
+                [TestMethod]
+                public void TestMethod1()
+                {
+                }
+
+                [TestMethod]
+                public void TestMethod2()
+                {
+                }
+
+                [TestMethod]
+                public void TestMethod3()
+                {
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+
+    [TestMethod]
+    public async Task WhenTestClassHasDuplicateMethodNames_Diagnostic()
+    {
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class TestClass1
+            {
+                [TestMethod]
+                public void TestMethod1()
+                {
+                }
+
+                [TestMethod]
+                public void [|TestMethod1|]()
+                {
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+
+    [TestMethod]
+    public async Task WhenTestClassHasMultipleDuplicateMethodNames_DiagnosticForEach()
+    {
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class TestClass1
+            {
+                [TestMethod]
+                public void TestMethod1()
+                {
+                }
+
+                [TestMethod]
+                public void [|TestMethod1|]()
+                {
+                }
+
+                [TestMethod]
+                public void [|TestMethod1|]()
+                {
+                }
+
+                [TestMethod]
+                public void TestMethod2()
+                {
+                }
+
+                [TestMethod]
+                public void [|TestMethod2|]()
+                {
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+
+    [TestMethod]
+    public async Task WhenTestClassHasDuplicateMethodSignatures_Diagnostic()
+    {
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class TestClass1
+            {
+                [TestMethod]
+                public void TestMethod1()
+                {
+                }
+
+                [TestMethod]
+                public void [|TestMethod1|]()
+                {
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+
+    [TestMethod]
+    public async Task WhenTestClassHasMethodsWithSameNameButDifferentParameters_NoDiagnostic()
+    {
+        // Note: While method overloading is technically allowed in C#,
+        // MSTest doesn't support parameterized test methods without DataRow/DynamicData
+        // This test documents current behavior - in practice, overloaded test methods
+        // should be avoided unless using data-driven testing
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class TestClass1
+            {
+                [TestMethod]
+                public void TestMethod1()
+                {
+                }
+
+                // This is not a valid test method pattern, but analyzer focuses on exact duplicates
+                public void TestMethod1(int parameter)
+                {
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+
+    [TestMethod]
+    public async Task WhenTestClassHasDataTestMethod_NoDiagnosticForDifferentDataRows()
+    {
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class TestClass1
+            {
+                [DataTestMethod]
+                [DataRow(1)]
+                [DataRow(2)]
+                public void TestMethod1(int value)
+                {
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+
+    [TestMethod]
+    public async Task WhenNonTestClassHasDuplicateMethods_NoDiagnostic()
+    {
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            public class NotATestClass
+            {
+                public void Method1()
+                {
+                }
+
+                public void Method1()
+                {
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+
+    [TestMethod]
+    public async Task WhenTestClassHasNonTestMethodsDuplicated_NoDiagnostic()
+    {
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class TestClass1
+            {
+                [TestMethod]
+                public void TestMethod1()
+                {
+                }
+
+                // Not a test method, so duplication is a compiler error, not our concern
+                private void HelperMethod()
+                {
+                }
+
+                private void HelperMethod()
+                {
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+
+    [TestMethod]
+    public async Task WhenTestMethodsInheritedFromBase_ChecksAcrossHierarchy()
+    {
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class BaseTestClass
+            {
+                [TestMethod]
+                public void TestMethod1()
+                {
+                }
+            }
+
+            [TestClass]
+            public class DerivedTestClass : BaseTestClass
+            {
+                [TestMethod]
+                public void [|TestMethod1|]() // Hides base method
+                {
+                }
+            }
+            """;
+
+        // Note: This test documents expected behavior for inherited test methods
+        // The analyzer should warn about hiding base test methods
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+
+    [TestMethod]
+    public async Task WhenTestClassHasDuplicateMethodsWithDifferentAttributes_StillDiagnostic()
+    {
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class TestClass1
+            {
+                [TestMethod]
+                [TestCategory("Category1")]
+                public void TestMethod1()
+                {
+                }
+
+                [TestMethod]
+                [TestCategory("Category2")]
+                public void [|TestMethod1|]()
+                {
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+
+    [TestMethod]
+    public async Task WhenTestClassHasDuplicateDataTestMethods_Diagnostic()
+    {
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class TestClass1
+            {
+                [DataTestMethod]
+                [DataRow(1)]
+                public void TestMethod1(int value)
+                {
+                }
+
+                [DataTestMethod]
+                [DataRow(2)]
+                public void [|TestMethod1|](int value)
+                {
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+
+    [TestMethod]
+    public async Task WhenAbstractTestClassHasDuplicateMethods_Diagnostic()
+    {
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public abstract class AbstractTestClass
+            {
+                [TestMethod]
+                public void TestMethod1()
+                {
+                }
+
+                [TestMethod]
+                public void [|TestMethod1|]()
+                {
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+
+    [TestMethod]
+    public async Task WhenPartialTestClassHasDuplicateMethodsAcrossParts_Diagnostic()
+    {
+        string code1 = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public partial class TestClass1
+            {
+                [TestMethod]
+                public void TestMethod1()
+                {
+                }
+            }
+            """;
+
+        string code2 = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            public partial class TestClass1
+            {
+                [TestMethod]
+                public void [|TestMethod1|]()
+                {
+                }
+            }
+            """;
+
+        await new VerifyCS.Test
+        {
+            TestState =
+            {
+                Sources = { code1, code2 },
+            },
+        }.RunAsync();
+    }
+
+    [TestMethod]
+    public async Task WhenTestClassHasThreeDuplicateMethods_DiagnosticForSecondAndThird()
+    {
+        string code = """
+            using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            [TestClass]
+            public class TestClass1
+            {
+                [TestMethod]
+                public void TestMethod1()
+                {
+                }
+
+                [TestMethod]
+                public void [|TestMethod1|]()
+                {
+                }
+
+                [TestMethod]
+                public void [|TestMethod1|]()
+                {
+                }
+            }
+            """;
+
+        await VerifyCS.VerifyAnalyzerAsync(code);
+    }
+}
