fde0cfd0fa75eee5e0eb722cd363bd2d442611e4
Youssef Victor <youssefvictor00@gmail.com>
2025-11-27 17:27:56 +0100

    Run integration tests with net10.0 (#6761)

--- test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/MSBuildTests.Test.cs
+++ test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/MSBuildTests.Test.cs
@@ -150,9 +150,9 @@ public class MSBuildTests_Test : AcceptanceTestBase<NopAssetFixture>
             failIfReturnValueIsNotZero: false,
             cancellationToken: TestContext.CancellationToken);
 
-        string outputFileLog = Directory.GetFiles(testAsset.TargetAssetPath, "MSBuild Tests_net9.0_x86.log", SearchOption.AllDirectories).Single();
-        Assert.IsTrue(File.Exists(outputFileLog), $"Expected file '{outputFileLog}'");
-        string logFileContent = File.ReadAllText(outputFileLog);
+        string[] outputLogFiles = Directory.GetFiles(testAsset.TargetAssetPath, $"MSBuild Tests_{TargetFrameworks.NetCurrent}_x86.log", SearchOption.AllDirectories);
+        Assert.ContainsSingle(outputLogFiles, $"Was expecting to find a single log file but found {outputLogFiles.Length}");
+        string logFileContent = File.ReadAllText(outputLogFiles[0]);
         Assert.IsTrue(Regex.IsMatch(logFileContent, ".*win-x86.*"), logFileContent);
 
         // This is the architecture part that's written by TerminalOutputDevice when there is no banner specified.
@@ -237,9 +237,9 @@ public class MSBuildTests_Test : AcceptanceTestBase<NopAssetFixture>
             failIfReturnValueIsNotZero: false,
             cancellationToken: TestContext.CancellationToken);
 
-        string outputFileLog = Directory.GetFiles(testAsset.TargetAssetPath, "MSBuild Tests_net9.0_x64.log", SearchOption.AllDirectories).Single();
-        Assert.IsTrue(File.Exists(outputFileLog), $"Expected file '{outputFileLog}'");
-        string logFileContent = File.ReadAllText(outputFileLog);
+        string[] outputLogFiles = Directory.GetFiles(testAsset.TargetAssetPath, $"MSBuild Tests_{TargetFrameworks.NetCurrent}_x64.log", SearchOption.AllDirectories);
+        Assert.ContainsSingle(outputLogFiles, $"Was expecting to find a single log file but found {outputLogFiles.Length}");
+        string logFileContent = File.ReadAllText(outputLogFiles[0]);
         // This is the architecture part that's written by TerminalOutputDevice when there is no banner specified.
         Assert.Contains($"[win-x64 - {TargetFrameworks.NetCurrent}]", logFileContent);
     }
