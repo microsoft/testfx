b1afac5ea572347b657eda76ef74735b02a0b683
Youssef Victor <youssefvictor00@gmail.com>
2026-01-09 10:27:11 +0100

    Fix formatting

--- samples/WasiPlayground/Program.cs
+++ samples/WasiPlayground/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-
 using Microsoft.Testing.Extensions;
 using Microsoft.Testing.Platform.Builder;
 using Microsoft.Testing.Platform.Capabilities.TestFramework;
