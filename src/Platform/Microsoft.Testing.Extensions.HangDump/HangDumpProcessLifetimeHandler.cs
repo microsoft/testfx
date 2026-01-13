fa02a897e1eadbe9560cf887f4d46307aa3afd02
Youssef1313 <youssefvictor00@gmail.com>
2026-01-13 12:56:24 +0100

    Allow none for --hangdump-type

--- src/Platform/Microsoft.Testing.Extensions.HangDump/HangDumpProcessLifetimeHandler.cs
+++ src/Platform/Microsoft.Testing.Extensions.HangDump/HangDumpProcessLifetimeHandler.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Microsoft.Diagnostics.NETCore.Client;
 using Microsoft.Testing.Extensions.Diagnostics.Helpers;
 using Microsoft.Testing.Extensions.Diagnostics.Resources;
 using Microsoft.Testing.Extensions.HangDump.Serializers;
@@ -354,12 +355,13 @@ internal sealed class HangDumpProcessLifetimeHandler : ITestHostProcessLifetimeH
 
 #if NETCOREAPP
         DiagnosticsClient diagnosticsClient = new(process.Id);
-        DumpType dumpType = _dumpType.ToLowerInvariant().Trim() switch
+        DumpType? dumpType = _dumpType.ToLowerInvariant().Trim() switch
         {
             "mini" => DumpType.Normal,
             "heap" => DumpType.WithHeap,
             "triage" => DumpType.Triage,
             "full" => DumpType.Full,
+            "none" => null,
             _ => throw ApplicationStateGuard.Unreachable(),
         };
 
@@ -372,7 +374,12 @@ internal sealed class HangDumpProcessLifetimeHandler : ITestHostProcessLifetimeH
 
         try
         {
-            diagnosticsClient.WriteDump(dumpType, finalDumpFileName, logDumpGeneration: false);
+            // Skip creating the dump if the option is set to none, and just kill the process.
+            if (dumpType.HasValue)
+            {
+                diagnosticsClient.WriteDump(dumpType.Value, finalDumpFileName, logDumpGeneration: false);
+                _dumpFiles.Add(finalDumpFileName);
+            }
         }
         catch (Exception e)
         {
@@ -381,17 +388,23 @@ internal sealed class HangDumpProcessLifetimeHandler : ITestHostProcessLifetimeH
         }
 
 #else
-        MiniDumpWriteDump.MiniDumpTypeOption miniDumpTypeOption = _dumpType.ToLowerInvariant().Trim() switch
+        MiniDumpWriteDump.MiniDumpTypeOption? miniDumpTypeOption = _dumpType.ToLowerInvariant().Trim() switch
         {
             "mini" => MiniDumpWriteDump.MiniDumpTypeOption.Mini,
             "heap" => MiniDumpWriteDump.MiniDumpTypeOption.Heap,
             "full" => MiniDumpWriteDump.MiniDumpTypeOption.Full,
+            "none" => null,
             _ => throw ApplicationStateGuard.Unreachable(),
         };
 
         try
         {
-            MiniDumpWriteDump.CollectDumpUsingMiniDumpWriteDump(process.Id, finalDumpFileName, miniDumpTypeOption);
+            // Skip creating the dump if the option is set to none, and just kill the process.
+            if (miniDumpTypeOption.HasValue)
+            {
+                MiniDumpWriteDump.CollectDumpUsingMiniDumpWriteDump(process.Id, finalDumpFileName, miniDumpTypeOption.Value);
+                _dumpFiles.Add(finalDumpFileName);
+            }
         }
         catch (Exception e)
         {
@@ -399,8 +412,6 @@ internal sealed class HangDumpProcessLifetimeHandler : ITestHostProcessLifetimeH
             await _outputDisplay.DisplayAsync(new ErrorMessageOutputDeviceData(string.Format(CultureInfo.InvariantCulture, ExtensionResources.ErrorWhileDumpingProcess, process.Id, process.Name, e)), cancellationToken).ConfigureAwait(false);
         }
 #endif
-
-        _dumpFiles.Add(finalDumpFileName);
     }
 
     private static void NotifyCrashDumpServiceIfEnabled()
