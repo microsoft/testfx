1b6b21ec32494133255ed6d96f0f5d4a413c8316
Youssef1313 <youssefvictor00@gmail.com>
2026-03-04 12:14:39 +0100

    Refactor UnitTest creation to separate method

--- src/Platform/Microsoft.Testing.Extensions.TrxReport/TrxReportEngine.cs
+++ src/Platform/Microsoft.Testing.Extensions.TrxReport/TrxReportEngine.cs
@@ -565,89 +565,7 @@ internal sealed partial class TrxReportEngine
             results.Add(unitTestResult);
 
             // TestDefinitions
-            var unitTest = new XElement(
-                "UnitTest",
-                new XAttribute("name", displayName),
-                new XAttribute("storage", testAppModule.ToLowerInvariant()),
-                new XAttribute("id", id));
-
-            TrxCategoriesProperty? trxCategories = testNode.Properties.SingleOrDefault<TrxCategoriesProperty>();
-            if (trxCategories?.Categories.Length > 0)
-            {
-                unitTest.Add(new XElement("TestCategory", trxCategories.Categories.Select(c => new XElement("TestCategoryItem", new XAttribute("TestCategory", c)))));
-            }
-
-            unitTest.Add(new XElement("Execution", new XAttribute("id", executionId)));
-
-            XElement? properties = null;
-            XElement? owners = null;
-            XElement? description = null;
-            foreach (TestMetadataProperty property in testNode.Properties.OfType<TestMetadataProperty>())
-            {
-                switch (property.Key)
-                {
-                    case "Owner":
-                        owners ??= new XElement("Owners", new XElement("Owner", new XAttribute("name", property.Value)));
-                        break;
-
-                    case "Priority":
-                        if (int.TryParse(property.Value, out _))
-                        {
-                            unitTest.SetAttributeValue("priority", property.Value);
-                        }
-
-                        break;
-
-                    case "Description":
-                        description ??= new XElement("Description", property.Value);
-                        break;
-
-                    default:
-                        // NOTE: VSTest doesn't produce Properties as of writing this.
-                        // It was historically fixed, but the fix wasn't correct and the fix was reverted and never revisited to be properly fixed.
-                        // Revert PR: https://github.com/microsoft/vstest/pull/15080
-                        // The original implementation (buggy) was setting "Key" and "Value" as attributes on "Property" element.
-                        // However, Visual Studio will validate the TRX file against vstst.xsd file in
-                        //  C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Xml\Schemas\vstst.xsd
-                        // In xsd, "Properties" element is defined as:
-                        // <xs:element name="Properties" minOccurs="0">
-                        //   <xs:complexType>
-                        //     <xs:sequence>
-                        //       <xs:element name="Property" minOccurs="0" maxOccurs="unbounded">
-                        //         <xs:complexType>
-                        //           <xs:sequence>
-                        //             <xs:element name="Key" />
-                        //             <xs:element name="Value" />
-                        //           </xs:sequence>
-                        //         </xs:complexType>
-                        //       </xs:element>
-                        //     </xs:sequence>
-                        //   </xs:complexType>
-                        // </xs:element>
-                        // So, Key and Value are **elements**, not attributes.
-                        // In MTP, we do the right thing and follow the XSD definition.
-                        properties ??= new XElement("Properties");
-                        properties.Add(new XElement(
-                            "Property",
-                            new XElement("Key", property.Key), new XElement("Value", property.Value)));
-                        break;
-                }
-            }
-
-            if (owners is not null)
-            {
-                unitTest.Add(owners);
-            }
-
-            if (description is not null)
-            {
-                unitTest.Add(description);
-            }
-
-            if (properties is not null)
-            {
-                unitTest.Add(properties);
-            }
+            XElement unitTest = CreateUnitTestElementForTestDefinition(displayName, testAppModule, id, testNode, executionId);
 
             var testMethod = new XElement(
                 "TestMethod",
@@ -686,6 +604,95 @@ internal sealed partial class TrxReportEngine
         testRun.Add(results);
 
         return new SummaryCounts(passed, failed, skipped, timedout);
+    }
+
+    private static XElement CreateUnitTestElementForTestDefinition(string displayName, string testAppModule, string id, TestNode testNode, string executionId)
+    {
+        var unitTest = new XElement(
+            "UnitTest",
+            new XAttribute("name", displayName),
+            new XAttribute("storage", testAppModule.ToLowerInvariant()),
+            new XAttribute("id", id));
+
+        TrxCategoriesProperty? trxCategories = testNode.Properties.SingleOrDefault<TrxCategoriesProperty>();
+        if (trxCategories?.Categories.Length > 0)
+        {
+            unitTest.Add(new XElement("TestCategory", trxCategories.Categories.Select(c => new XElement("TestCategoryItem", new XAttribute("TestCategory", c)))));
+        }
+
+        unitTest.Add(new XElement("Execution", new XAttribute("id", executionId)));
+
+        XElement? properties = null;
+        XElement? owners = null;
+        XElement? description = null;
+        foreach (TestMetadataProperty property in testNode.Properties.OfType<TestMetadataProperty>())
+        {
+            switch (property.Key)
+            {
+                case "Owner":
+                    owners ??= new XElement("Owners", new XElement("Owner", new XAttribute("name", property.Value)));
+                    break;
+
+                case "Priority":
+                    if (int.TryParse(property.Value, out _))
+                    {
+                        unitTest.SetAttributeValue("priority", property.Value);
+                    }
+
+                    break;
+
+                case "Description":
+                    description ??= new XElement("Description", property.Value);
+                    break;
+
+                default:
+                    // NOTE: VSTest doesn't produce Properties as of writing this.
+                    // It was historically fixed, but the fix wasn't correct and the fix was reverted and never revisited to be properly fixed.
+                    // Revert PR: https://github.com/microsoft/vstest/pull/15080
+                    // The original implementation (buggy) was setting "Key" and "Value" as attributes on "Property" element.
+                    // However, Visual Studio will validate the TRX file against vstst.xsd file in
+                    //  C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Xml\Schemas\vstst.xsd
+                    // In xsd, "Properties" element is defined as:
+                    // <xs:element name="Properties" minOccurs="0">
+                    //   <xs:complexType>
+                    //     <xs:sequence>
+                    //       <xs:element name="Property" minOccurs="0" maxOccurs="unbounded">
+                    //         <xs:complexType>
+                    //           <xs:sequence>
+                    //             <xs:element name="Key" />
+                    //             <xs:element name="Value" />
+                    //           </xs:sequence>
+                    //         </xs:complexType>
+                    //       </xs:element>
+                    //     </xs:sequence>
+                    //   </xs:complexType>
+                    // </xs:element>
+                    // So, Key and Value are **elements**, not attributes.
+                    // In MTP, we do the right thing and follow the XSD definition.
+                    properties ??= new XElement("Properties");
+                    properties.Add(new XElement(
+                        "Property",
+                        new XElement("Key", property.Key), new XElement("Value", property.Value)));
+                    break;
+            }
+        }
+
+        if (owners is not null)
+        {
+            unitTest.Add(owners);
+        }
+
+        if (description is not null)
+        {
+            unitTest.Add(description);
+        }
+
+        if (properties is not null)
+        {
+            unitTest.Add(properties);
+        }
+
+        return unitTest;
     }
 
     private static string AddTestSettings(XElement testRun, string testRunName)
