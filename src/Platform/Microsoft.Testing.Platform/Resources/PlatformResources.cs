fbe4f840c5c81340892d5149095d4d69b4788396
Youssef1313 <youssefvictor00@gmail.com>
2026-03-10 20:32:35 +0100

    Move InvalidTestApplicationBuilderTypeForAI from MTP to MTP.AI

--- src/Platform/Microsoft.Testing.Platform/Resources/PlatformResources.cs
+++ src/Platform/Microsoft.Testing.Platform/Resources/PlatformResources.cs
@@ -30,8 +30,6 @@ internal static partial class PlatformResources
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static string GetResourceString(string resourceKey) => ResourceManager.GetString(resourceKey, null)!;
 
-    internal static string @InvalidTestApplicationBuilderTypeForAI => GetResourceString("InvalidTestApplicationBuilderTypeForAI");
-
     internal static string @NoSerializerRegisteredWithIdErrorMessage => GetResourceString("NoSerializerRegisteredWithIdErrorMessage");
 
     internal static string @NoSerializerRegisteredWithTypeErrorMessage => GetResourceString("NoSerializerRegisteredWithTypeErrorMessage");
