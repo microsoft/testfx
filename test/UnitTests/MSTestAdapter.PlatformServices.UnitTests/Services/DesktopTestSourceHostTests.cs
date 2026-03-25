6eecf366eb7ad8190acc7b0bf8260e47f81afb06
Youssef Victor <youssefvictor00@gmail.com>
2026-03-25 10:01:50 +0100

    Update DesktopTestSourceHostTests.cs

--- test/UnitTests/MSTestAdapter.PlatformServices.UnitTests/Services/DesktopTestSourceHostTests.cs
+++ test/UnitTests/MSTestAdapter.PlatformServices.UnitTests/Services/DesktopTestSourceHostTests.cs
@@ -177,7 +177,6 @@ public class DesktopTestSourceHostTests : TestContainer
         }
     }
 
-
     public void NoAppDomainShouldGetCreatedWhenDisableAppDomainIsSetToTrue()
     {
         // Arrange
