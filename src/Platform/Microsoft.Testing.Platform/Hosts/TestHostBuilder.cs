17773f2f537402aa7e31f9fafc186e744696fb84
Youssef Victor <youssefvictor00@gmail.com>
2026-04-16 08:34:38 +0200

    Update src/Platform/Microsoft.Testing.Platform/Hosts/TestHostBuilder.cs

--- src/Platform/Microsoft.Testing.Platform/Hosts/TestHostBuilder.cs
+++ src/Platform/Microsoft.Testing.Platform/Hosts/TestHostBuilder.cs
@@ -618,7 +618,6 @@ internal sealed class TestHostBuilder(IFileSystem fileSystem, IRuntimeFeature ru
         await logger.LogDebugAsync($"Connecting to named pipe '{pipeName}'").ConfigureAwait(false);
         string? seconds = configuration[PlatformConfigurationConstants.PlatformTestHostControllersManagerNamedPipeClientConnectTimeoutSeconds];
 
-        // Default timeout is 30 seconds
         double timeoutSeconds = seconds is null ? TimeoutHelper.DefaultHangTimeoutSeconds : double.Parse(seconds, CultureInfo.InvariantCulture);
         await logger.LogDebugAsync($"Setting PlatformTestHostControllersManagerNamedPipeClientConnectTimeoutSeconds '{timeoutSeconds}'").ConfigureAwait(false);
         using CancellationTokenSource timeout = new(TimeSpan.FromSeconds(timeoutSeconds));
