c3efd05e44ded6b30b3ccecd4c1c79bbf8b00e48
Youssef1313 <youssefvictor00@gmail.com>
2026-04-16 07:55:27 +0200

    Cleanup TimeoutHelper and increase the timeout to 5 mins

--- src/Platform/Microsoft.Testing.Platform/Helpers/TimeoutHelper.cs
+++ src/Platform/Microsoft.Testing.Platform/Helpers/TimeoutHelper.cs
@@ -14,26 +14,13 @@ internal static class TimeoutHelper
         string? customAntiHangTimeout = Environment.GetEnvironmentVariable(EnvironmentVariableConstants.TESTINGPLATFORM_DEFAULT_HANG_TIMEOUT);
 #pragma warning restore RS0030 // Do not use banned APIs
 
-        // Took from Tpv2 experience, it's the timeout for the testhost.exe start.
-        DefaultHangTimeSpanTimeout = TimeSpan.FromSeconds(90);
-
-        if (customAntiHangTimeout is not null)
-        {
-            if (TimeSpanParser.TryParse(customAntiHangTimeout, out TimeSpan customHangTimeout))
-            {
-                DefaultHangTimeSpanTimeout = customHangTimeout;
-            }
-        }
+        DefaultHangTimeSpanTimeout =
+            TimeSpanParser.TryParse(customAntiHangTimeout, out TimeSpan customHangTimeout)
+            ? customHangTimeout
+            : TimeSpan.FromMinutes(5);
 
         DefaultHangTimeoutSeconds = (int)DefaultHangTimeSpanTimeout.TotalSeconds;
-        DefaultHangTimeoutMilliseconds = (int)DefaultHangTimeSpanTimeout.TotalMilliseconds;
     }
-
-    /// <summary>
-    /// Gets defaultAntiHangTimeout* values are used as timeout for every wait operation in the test platform.
-    /// Are not intended for any timeout logic, but only to avoid infinite waits in case of test platform hangs.
-    /// </summary>
-    public static int DefaultHangTimeoutMilliseconds { get; private set; }
 
     public static int DefaultHangTimeoutSeconds { get; private set; }
 
