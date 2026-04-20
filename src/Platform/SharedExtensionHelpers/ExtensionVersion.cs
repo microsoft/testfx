5d75ccdd1590067911eb569cb0cbcd236c0fda85
Amaury Levé <amauryleve@microsoft.com>
2026-04-20 21:11:22 +0200

    Address review: deduplicate BuildInfo.cs.template, remove ExtensionVersion wrapper
    - Move BuildInfo.cs.template to SharedExtensionHelpers/ as a single shared copy
    - Add BuildInfoTemplateFile property in Directory.Build.targets to allow
      overriding the template path (defaults to MSBuildProjectDirectory)
    - Extension csproj files now set BuildInfoTemplateFile to the shared template
    - Template now generates ExtensionVersion.DefaultSemVer directly, eliminating
      the unnecessary ExtensionBuildVersion -> ExtensionVersion const indirection
    - Remove #pragma warning disable for IDE0073; use correct file header directly
    - Remove per-extension BuildInfo.cs.template duplicates (7 files)
    - Remove SharedExtensionHelpers/ExtensionVersion.cs wrapper

--- src/Platform/SharedExtensionHelpers/ExtensionVersion.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-namespace Microsoft.Testing.Extensions;
-
-internal static class ExtensionVersion
-{
-    public const string DefaultSemVer = ExtensionBuildVersion.Version;
-}
