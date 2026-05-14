0c28d869683a1071586d46e98a9f0795e22a8ea7
Amaury Levé <amauryleve@microsoft.com>
2026-05-14 17:48:47 +0200

    Use full string match for AreEqual messages in scope tests

--- test/UnitTests/TestFramework.UnitTests/Assertions/AssertTests.ScopeTests.cs
+++ test/UnitTests/TestFramework.UnitTests/Assertions/AssertTests.ScopeTests.cs
@@ -152,7 +152,7 @@ public partial class AssertTests
 
             Assert.IsNotNull(value)
             """);
-        innerException.InnerExceptions[1].Message.Should().Contain("Assert.AreEqual failed.");
+        innerException.InnerExceptions[1].Message.Should().Be("Assert.AreEqual failed. Expected:<1>. Actual:<2>. 'expected' expression: '1', 'actual' expression: '2'.");
     }
 
     public void Scope_AssertIsInstanceOfType_IsSoftFailure()
@@ -175,7 +175,7 @@ public partial class AssertTests
 
         innerException.InnerExceptions.Should().HaveCount(2);
         innerException.InnerExceptions[0].Message.Should().Be("Assert.IsInstanceOfType failed. 'value' expression: 'value'. Expected type:<System.Int32>. Actual type:<System.String>.");
-        innerException.InnerExceptions[1].Message.Should().Contain("Assert.AreEqual failed.");
+        innerException.InnerExceptions[1].Message.Should().Be("Assert.AreEqual failed. Expected:<1>. Actual:<2>. 'expected' expression: '1', 'actual' expression: '2'.");
     }
 
     public void Scope_AssertIsExactInstanceOfType_IsSoftFailure()
@@ -198,7 +198,7 @@ public partial class AssertTests
 
         innerException.InnerExceptions.Should().HaveCount(2);
         innerException.InnerExceptions[0].Message.Should().Be("Assert.IsExactInstanceOfType failed. 'value' expression: 'value'. Expected exact type:<System.Object>. Actual type:<System.String>.");
-        innerException.InnerExceptions[1].Message.Should().Contain("Assert.AreEqual failed.");
+        innerException.InnerExceptions[1].Message.Should().Be("Assert.AreEqual failed. Expected:<1>. Actual:<2>. 'expected' expression: '1', 'actual' expression: '2'.");
     }
 
     public void Scope_AssertContainsSingle_IsSoftFailure()
@@ -221,6 +221,6 @@ public partial class AssertTests
 
         innerException.InnerExceptions.Should().HaveCount(2);
         innerException.InnerExceptions[0].Message.Should().Be("Assert.ContainsSingle failed. Expected collection to contain exactly one element but found 3 element(s). 'collection' expression: 'items'.");
-        innerException.InnerExceptions[1].Message.Should().Contain("Assert.AreEqual failed.");
+        innerException.InnerExceptions[1].Message.Should().Be("Assert.AreEqual failed. Expected:<1>. Actual:<2>. 'expected' expression: '1', 'actual' expression: '2'.");
     }
 }
