fbe1cc5cc546ea37f35d8e499fd70712f8383bfc
Amaury Levé <amaury.leve@gmail.com>
2026-05-28 14:12:56 +0200

    Apply suggestion from @Evangelink

--- test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/RetryWithMaxFailedTestsTests.cs
+++ test/IntegrationTests/Microsoft.Testing.Platform.Acceptance.IntegrationTests/RetryWithMaxFailedTestsTests.cs
@@ -1,7 +1,5 @@
-﻿#pragma warning disable IDE0073 // The file header does not match the required text
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under dual-license. See LICENSE.PLATFORMTOOLS.txt file in the project root for full license information.
-#pragma warning restore IDE0073 // The file header does not match the required text
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Microsoft.Testing.Platform.Acceptance.IntegrationTests;
 
